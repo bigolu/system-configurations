f1b4e3a11f2cc639aa3bb9a8ddbaa42c8d74ed26
Biggie Emmanuel <code@bigo.lu>
2024-09-14 21:13:49 -0400

    fix: remove reference to the fontconfig files I removed

--- flake-modules/home-manager/modules/fonts.nix
+++ flake-modules/home-manager/modules/fonts.nix
@@ -23,13 +23,6 @@ lib.mkMerge [
   })
   (lib.mkIf (isLinux && isGui) {
     fonts.fontconfig.enable = true;
-    repository.symlink.xdg.configFile = {
-      "fontconfig/fonts.conf".source = "fontconfig/local.conf";
-      # TODO: I should pull this file from the Nerdfont repository so it stays up to date, but nix can't do sparse
-      # checkouts and I don't want to download a ~3gb repo just for one file.
-      # issue: https://github.com/NixOS/nix/issues/5811
-      "fontconfig/conf.d/10-nerd-font-symbols.conf".source = "fontconfig/10-nerd-font-symbols.conf";
-    };
     # Wezterm can't read my fonts from Nix despite the showing up in fontconfig so I copy all the Nix fonts to
     # $XDG_DATA_HOME/fonts. Wezterm also can't read the font when it is a symlink to the Nix store so I
     # dereference the symbolic links. I don't preserve the mode of the original files since they are read
