650d0eac9dff818119fa6bd716141c46c513f585
Biggie Emmanuel <code@bigo.lu>
2024-09-20 10:31:11 -0400

    cleanup

--- scripts/glob.bash
+++ scripts/glob.bash
@@ -20,19 +20,19 @@ function has_match {
 }
 
 function filter {
-  readarray -d '' path_predicates < <(make_path_predicates "${patterns[@]}")
+  readarray -d '' path_predicates < <(make_path_predicates)
   # shellcheck disable=2185
-  find -files0-from - "${path_predicates[@]}" -print0
+  find -files0-from - "${path_predicates[@]}" -print0 2>/dev/null
 }
 
 function make_path_predicates {
   flags=()
-  for pattern in "$@"; do
+  for pattern in "${patterns[@]}"; do
     flags=("${flags[@]}" '-path' "$pattern" '-o')
   done
   # remove extra '-o'
   unset 'flags[-1]'
-  
+
   if [ -n "$invert" ]; then
     print_with_nul \! \( "${flags[@]}" \)
   else
@@ -43,16 +43,20 @@ function make_path_predicates {
 function parse_arguments {
   subcommand="$1"
   set -- "${@:2}"
-  
+
   invert=
   if [ "${1:-}" = '--invert' ]; then
     invert=1
     set -- "${@:2}"
   fi
 
-  declare -a patterns=( "$@" )
+  patterns=("$@")
   if [ ${#patterns[@]} -eq 0 ]; then
-    patterns=( '*' )
+    if [ -n "$invert" ]; then
+      patterns=('')
+    else
+      patterns=('*')
+    fi
   fi
 }
 
