43c50691b1bdd25f443aaba7cdd83e556ef44e06
Biggie Emmanuel <code@bigo.lu>
2024-09-26 08:41:13 -0400

    perf: I don't need the branch name for the remotes HEAD, just the ref. `git remote` was also taking about 3 seconds so removing it was a big speedup

--- scripts/get-files-that-differ-from-default-branch.bash
+++ scripts/get-files-that-differ-from-default-branch.bash
@@ -3,11 +3,6 @@ set -o nounset
 set -o pipefail
 
 remote="${1:-origin}"
-
-# source: https://stackoverflow.com/a/50056710
-default_branch_name="$(LC_ALL=C git remote show "$remote" | sed -n '/HEAD branch/s/.*: //p')"
-
-git diff -z --diff-filter=d --name-only "$remote/$default_branch_name" HEAD
-
+git diff-tree -z -r --diff-filter=d --name-only --no-commit-id "$remote/HEAD" HEAD
 # Untracked files
 git ls-files -z --others --exclude-standard
