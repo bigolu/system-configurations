8ce1702d9f7db267fb61b2a6cd50ca334cdbb361
Biggie Emmanuel <code@bigo.lu>
2024-10-01 07:49:05 -0400

    Rework renovate automerging N

--- scripts/ci/auto-merge.bash
+++ scripts/ci/auto-merge.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix
-#! nix shell --quiet local#nixpkgs.bash local#nixpkgs.gh --command bash
+#! nix shell --quiet local#nixpkgs.bash local#nixpkgs.gh local#nixpkgs.ripgrep local#nixpkgs.gitMinimal --command bash
 
 # shellcheck shell=bash
 
@@ -10,40 +10,77 @@ set -o pipefail
 # TODO: Add debug mode that just logs what it would do, triggered by lack of
 # github environment variable being set. This way I can test locally.
 
-while IFS= read -r pr_number; do
-  echo "Processing PR #$pr_number"
+function main {
+  readarray -t branches_with_pr \
+    < <(gh pr list --json headRefName --jq '.[].headRefName' --repo "$GITHUB_REPOSITORY")
+  readarray -t branches_to_automerge \
+    < <(git branch --remotes --format '%(refname:lstrip=3)' | rg '^renovate/branch-automerge/')
 
-  comment_body=
-  if [[ "$(gh pr checks "$pr_number" --json bucket --jq '.[].bucket' --repo "$GITHUB_REPOSITORY" --required)" =~ 'fail' ]]; then
-    # SYNC: NIX_BRANCH_PREFIX
-    if [[ "$(gh pr view "$pr_number" --json headRefName --jq '.headRefName' --repo "$GITHUB_REPOSITORY")" == renovate/flake-lock/* ]]; then
-      # Since I track nixpkgs-unstable some failures are expected. Just close
-      # the PR and hope it passes next time since the lock will get regenerated.
-      gh pr close "$pr_number" --repo "$GITHUB_REPOSITORY"
-      continue
-    else
-      echo 'Status checks failed'
-      comment_body='A status check failed so auto-merging has been disabled.'
+  branches_to_automerge_without_pr=()
+  for branch_to_automerge in "${branches_to_automerge[@]}"; do
+    if ! is_item_in "$branch_to_automerge" "${branches_with_pr[@]}"; then
+      branches_to_automerge_without_pr=("${branches_to_automerge_without_pr[@]}" "$branch_to_automerge")
     fi
-  elif [ "$(gh pr view "$pr_number" --json mergeable --jq '.mergeable' --repo "$GITHUB_REPOSITORY")" = CONFLICTING ]; then
-    echo 'PR is conflicted'
-    comment_body='This PR has a conflict with its base branch so auto-merging has been disabled.'
-  fi
+  done
 
-  if [ -n "$comment_body" ]; then
-    old_title="$(gh pr view "$pr_number" --json title --jq '.title' --repo "$GITHUB_REPOSITORY")"
-    gh pr edit "$pr_number" --repo "$GITHUB_REPOSITORY" \
-      --remove-label automerge \
-      --add-label automerge-failed \
-      --title "[automerge-failed] $old_title"
-    gh pr comment "$pr_number" --repo "$GITHUB_REPOSITORY" \
-      --body "$comment_body"
-    # Renovate uses the pull request title as its state so we shouldn't mess with it:
-    # https://docs.renovatebot.com/configuration-options/#prtitle
-    gh pr edit "$pr_number" --repo "$GITHUB_REPOSITORY" \
-      --title "$old_title"
-  else
-    gh pr update-branch "$pr_number" --repo "$GITHUB_REPOSITORY" --rebase
-    gh pr merge "$pr_number" --repo "$GITHUB_REPOSITORY" --auto --squash
-  fi
-done < <(gh pr list --json number --jq '.[].number' --repo "$GITHUB_REPOSITORY" --label automerge)
+  for branch_to_automerge_without_pr in "${branches_to_automerge_without_pr[@]}"; do
+    labels=()
+    if ! rg '^renovate/branch-automerge/fixup/' <<<"$branch_to_automerge_without_pr"; then
+      labels=(--label automerge)
+    fi
+
+    gh pr create --repo "$GITHUB_REPOSITORY" --head "$branch_to_automerge_without_pr" --base "$(git symbolic-ref --short HEAD)" --title "$IGNORE_MARKER $(git show -s --format=%B "$branch_to_automerge_without_pr")" --body 'A renovate automergeable update.' "${labels[@]}"
+  done
+
+  while IFS= read -r pr_number; do
+    echo "Processing PR #$pr_number"
+
+    comment_body=
+    if [[ "$(gh pr checks "$pr_number" --json bucket --jq '.[].bucket' --repo "$GITHUB_REPOSITORY" --required)" =~ 'fail' ]]; then
+      # SYNC: NIX_BRANCH_PREFIX
+      if [[ "$(gh pr view "$pr_number" --json headRefName --jq '.headRefName' --repo "$GITHUB_REPOSITORY")" == renovate/branch-automerge/flake-lock/* ]]; then
+        # Since I track nixpkgs-unstable some failures are expected. Just close
+        # the PR, delete the branch, and hope it passes next time since the lock
+        # will get regenerated.
+        gh pr close "$pr_number" --repo "$GITHUB_REPOSITORY" --delete-branch
+        continue
+      else
+        echo 'Status checks failed'
+        comment_body='A status check failed so auto-merging has been disabled.'
+      fi
+    elif [ "$(gh pr view "$pr_number" --json mergeable --jq '.mergeable' --repo "$GITHUB_REPOSITORY")" = CONFLICTING ]; then
+      echo 'PR is conflicted'
+      comment_body='This PR has a conflict with its base branch so auto-merging has been disabled.'
+    fi
+
+    if [ -n "$comment_body" ]; then
+      old_title="$(gh pr view "$pr_number" --json title --jq '.title' --repo "$GITHUB_REPOSITORY")"
+      gh pr edit "$pr_number" --repo "$GITHUB_REPOSITORY" \
+        --remove-label automerge \
+        --add-label automerge-failed \
+        --title "[automerge-failed] $old_title"
+      gh pr comment "$pr_number" --repo "$GITHUB_REPOSITORY" \
+        --body "$comment_body"
+    else
+      gh pr update-branch "$pr_number" --repo "$GITHUB_REPOSITORY" --rebase
+      gh pr merge "$pr_number" --repo "$GITHUB_REPOSITORY" --auto --squash
+    fi
+    # SYNC: AUTOMERGE_LABEL
+  done < <(gh pr list --json number --jq '.[].number' --repo "$GITHUB_REPOSITORY" --label automerge)
+}
+
+function is_item_in {
+  local target="$1"
+  shift
+
+  local item
+  for item in "$@"; do
+    if [[ "$item" == "$target" ]]; then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+main
