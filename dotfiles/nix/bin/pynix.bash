ff8aa0260622842cf4c42095f833aa918db9332d
Biggie Emmanuel <code@bigo.lu>
2024-10-06 14:33:24 -0400

    Fix pynix
    The python packages were not visible from python due to a change in how
    `nix shell` works vs. `nix-shell`[1].
    
    [1]: https://blog.ysndr.de/posts/guides/2021-12-01-nix-shells/#shell-hooks

--- dotfiles/nix/bin/pynix.bash
+++ dotfiles/nix/bin/pynix.bash
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 # Lets me starts a nix shell with python and the specified python packages.
 # Example: `pynix requests marshmallow`
 
-readarray -d '' packages < <(printf "nixpkgs#python3Packages.%s\0" "$@")
-nix shell "${packages[@]}"
+function main {
+  readarray -d '' packages < <(printf "p.%s\0" "$@")
+  nix shell --impure --expr "(import (builtins.getFlake \"nixpkgs\") {}).python3.withPackages (p: [$(join ' ' "${packages[@]}")])"
+}
+
+# source: https://stackoverflow.com/a/17841619
+function join {
+  local d=${1-} f=${2-}
+  if shift 2; then
+    printf %s "$f" "${@/#/$d}"
+  fi
+}
+
+main "$@"
