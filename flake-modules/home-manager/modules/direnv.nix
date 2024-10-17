39c2485732c9439252a6886918a514903821e5da
Biggie Emmanuel <code@bigo.lu>
2024-10-17 19:23:23 -0400

    Remove direnv config file
    It's already included in the package.

--- flake-modules/home-manager/modules/direnv.nix
+++ flake-modules/home-manager/modules/direnv.nix
@@ -7,15 +7,4 @@
   repository.symlink.xdg.configFile = {
     "direnv/direnv.toml".source = "direnv/direnv.toml";
   };
-
-  xdg.configFile = {
-    # TODO: I shouldn't have to do this. Either Nix for direnv should generate
-    # this, but not sure how that works generally. I can look at zoxide or
-    # doppler package definitions to see how they work. I'm thinking the CLI
-    # should be responsible for exporting automplete as part of the build, like
-    # types.
-    "fish/conf.d/direnv.fish".source = ''${pkgs.runCommand "direnv-config.fish" { }
-      "${pkgs.direnv}/bin/direnv hook fish > $out"
-    }'';
-  };
 }
