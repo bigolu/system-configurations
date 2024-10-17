6d2723ab521c069fd5c66ca663feecd505393119
Biggie Emmanuel <code@bigo.lu>
2024-10-17 19:19:11 -0400

    fish: Remove rust function

--- dotfiles/fish/conf.d/general.fish
+++ dotfiles/fish/conf.d/general.fish
@@ -197,23 +197,6 @@ function tunnel --description 'Connect my cloudflare tunnel to the specified por
         cloudflared tunnel run --url "http://localhost:$port"
 end
 
-function rust --description 'run the given rust source file' --wraps rustc
-    if test (count $argv) -eq 0
-        echo -s \
-            (set_color red) \
-            'ERROR: You must provide at least one argument, the source file to run' >/dev/stderr
-        return 1
-    end
-
-    set source_file $argv[-1]
-    set executable_name (basename $source_file .rs)
-    rustc $argv
-    and begin
-        ./$executable_name
-        rm $executable_name
-    end
-end
-
 function ls --wraps lsd
     lsd $argv
 end
