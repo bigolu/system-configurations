86fdcebdd7f7c8b61d6f22f3da155e3e1e929184
Biggie Emmanuel <code@bigo.lu>
2024-10-20 14:54:57 -0400

    Use flake-compat to get the host configurations instead of getFlake
    getFlake is really slow (~25 seconds) compared to flake-compat (~5 seconds)
    since getFlake copies the source into the store. There's an issue open for doing
    the copying lazily[1].
    
    [1]: https://github.com/NixOS/nix/issues/3121

--- scripts/get-host-configurations.bash
+++ scripts/get-host-configurations.bash
@@ -11,8 +11,8 @@ echo
 
 printf '\n%s\n' '- Home Manager'
 # shellcheck disable=2016
-nix eval --raw --impure --expr 'let x = builtins.getFlake (builtins.toString ./.); in (builtins.concatStringsSep "\n" (map (name: "\n  - ${name} / ${x.outputs.homeConfigurations.${name}.activationPackage.system}") (builtins.attrNames x.outputs.homeConfigurations))) + "\n"'
+nix eval --raw --impure --expr 'let x = import ./default.nix; in (builtins.concatStringsSep "\n" (map (name: "\n  - ${name} / ${x.outputs.homeConfigurations.${name}.activationPackage.system}") (builtins.attrNames x.outputs.homeConfigurations))) + "\n"'
 
 printf '\n%s\n' '- nix-darwin'
 # shellcheck disable=2016
-nix eval --raw --impure --expr 'let x = builtins.getFlake (builtins.toString ./.); in (builtins.concatStringsSep "\n" (map (name: "\n  - ${name} / ${x.outputs.darwinConfigurations.${name}.system.system}") (builtins.attrNames x.outputs.darwinConfigurations))) + "\n"'
+nix eval --raw --impure --expr 'let x = import ./default.nix; in (builtins.concatStringsSep "\n" (map (name: "\n  - ${name} / ${x.outputs.darwinConfigurations.${name}.system.system}") (builtins.attrNames x.outputs.darwinConfigurations))) + "\n"'
