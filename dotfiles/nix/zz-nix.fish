22ed379ebe969dce3352e3fcd147228d97d91643
Biggie Emmanuel <code@bigo.lu>
2024-10-21 16:19:37 -0400

    Filter out other systems from "nix flake show"

--- dotfiles/nix/zz-nix.fish
+++ dotfiles/nix/zz-nix.fish
@@ -17,3 +17,11 @@ end
 function nix-store-size
     numfmt --to=iec-i --suffix=B --format="%9.2f" (nix path-info --json --all | jq 'map(.narSize) | add')
 end
+
+# There's an open issue for omitting more information from `flake show`, but this
+# workaround was suggested in the meantime. https://github.com/NixOS/nix/issues/9011
+function nix-flake-show
+    # To avoid having nix's progress messages from (e.g. "evaluating x...")
+    # intersperse with ripgrep's output, I'm suppressing nix's progress messages.
+    nix flake show 2>/dev/null | rg -v omitted
+end
