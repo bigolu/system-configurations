9f4f501986bd31d4cbbedd78dd6cbfbfaf00c323
Biggie Emmanuel <code@bigo.lu>
2024-10-25 03:08:41 -0400

    fish: Shadow `nix flake show` instead of using `nix-flake-show`
    I never remember to use the dashed form.

--- dotfiles/nix/zz-nix.fish
+++ dotfiles/nix/zz-nix.fish
@@ -6,22 +6,24 @@ if not status is-interactive
 end
 
 function nix
-    # If I'm starting the repl, start it with my startup file.
+    # If I'm running the repl, use my startup file.
     if test (count $argv) -eq 1 -a "$argv[1]" = repl
         set xdg_config (test -n "$XDG_CONFIG_HOME" && echo $XDG_CONFIG_HOME || echo "$HOME/.config")
-        set --append argv --file "$xdg_config/nix/repl-startup.nix"
+        command nix $argv --file "$xdg_config/nix/repl-startup.nix"
+    else if test (count $argv) -eq 2 -a "$argv[1]" = flake -a "$argv[2]" = show
+        # There's an open issue for omitting more information from `flake show`, but
+        # this workaround was suggested in the meantime[1].
+        #
+        # To avoid interspersing nix's progress messages (e.g. "evaluating x...")
+        # with ripgrep's output, I'm suppressing the messages.
+        #
+        # [1]: https://github.com/NixOS/nix/issues/9011
+        command nix flake show 2>/dev/null | rg -v omitted
+    else
+        command nix $argv
     end
-    command nix $argv
 end
 
 function nix-store-size
     numfmt --to=iec-i --suffix=B --format="%9.2f" (nix path-info --json --all | jq 'map(.narSize) | add')
-end
-
-# There's an open issue for omitting more information from `flake show`, but this
-# workaround was suggested in the meantime. https://github.com/NixOS/nix/issues/9011
-function nix-flake-show
-    # To avoid having nix's progress messages from (e.g. "evaluating x...")
-    # intersperse with ripgrep's output, I'm suppressing nix's progress messages.
-    nix flake show 2>/dev/null | rg -v omitted
 end
