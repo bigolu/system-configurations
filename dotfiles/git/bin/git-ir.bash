aa1abb8381a1940960f79276600e44523403f1c5
Biggie Emmanuel <code@bigo.lu>
2024-10-26 11:18:27 -0400

    git: Use fzf to choose a commit to rebase from

---
+++ dotfiles/git/bin/git-ir.bash
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if (($# == 0)); then
+  choice="$(git log --oneline | fzf-zoom --prompt 'Choose a commit to rebase from: ' --preview 'git show --patch {1} | delta')"
+  hash="$(cut -d ' ' -f 1 <<<"$choice")"
+  git rebase --interactive "$hash^"
+elif ((${#1} <= 2)); then
+  git rebase --interactive "HEAD~$1"
+else
+  git rebase --interactive "$1^"
+fi
