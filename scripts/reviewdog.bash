44adf6f2446537fb01c23ea02ddbfa708640b00b
Biggie Emmanuel <code@bigo.lu>
2024-10-28 11:11:40 -0400

    ci: Fix

--- scripts/reviewdog.bash
+++ scripts/reviewdog.bash
@@ -10,14 +10,12 @@ set -o pipefail
 reporter=
 if [[ "${CI:-}" = 'true' ]]; then
   reporter='github-check'
-  fail_level='none'
 else
   reporter='local'
-  fail_level='any'
 fi
 
 reviewdog \
   "-reporter=$reporter" \
   -filter-mode=nofilter \
-  "-fail-level=$fail_level" \
+  -fail-level=any \
   -level=error "$@"
