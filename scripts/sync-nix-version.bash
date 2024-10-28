2c4b8d96b7f5767b4a25c54272589b2a5f6ead93
Biggie Emmanuel <code@bigo.lu>
2024-10-27 20:30:46 -0400

    nix: Get Nix version from flake
    This is run in CI on the branch that updates the flake.lock so the installed Nix
    version will be that of the previous flake.lock.

--- scripts/sync-nix-version.bash
+++ scripts/sync-nix-version.bash
@@ -7,9 +7,13 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-version="$(nix --version)"
-version_number="${version##* }"
-files=(.github/actions/setup/action.yml README.md)
-for file in "${files[@]}"; do
-  sed -i "s/\/nix-[0-9]*\.[0-9]*\.[0-9]*/\/nix-$version_number/g" "$file"
-done
+# shellcheck disable=2016
+version="$(
+  nix eval \
+    --raw --impure --expr \
+    '(import ./default.nix).outputs.legacyPackages.${builtins.currentSystem}.nixpkgs.nix.version'
+)"
+sed --regexp-extended --in-place \
+  "s/\/nix-[0-9]+(\.[0-9]+){0,2}/\/nix-$version/g" \
+  .github/actions/setup/action.yml \
+  README.md
