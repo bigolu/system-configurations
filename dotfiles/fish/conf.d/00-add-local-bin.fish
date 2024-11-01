f4fc8a9c201bacb832315c05bb4eee3b32a43f71
Biggie Emmanuel <code@bigo.lu>
2024-11-01 17:40:14 -0400

    fish: Add local bin to the PATH later in the startup sequence

--- dotfiles/fish/conf.d/00-add-local-bin.fish
+++ dotfiles/fish/conf.d/00-add-local-bin.fish
@@ -1,2 +1,9 @@
-# macOS doesn't set this
-fish_add_path --global --prepend --move "$HOME/.local/bin"
+if not status is-interactive
+    exit
+end
+
+function __add_local_bin --on-event fish_prompt
+    functions --erase (status current-function)
+    # macOS doesn't set this
+    fish_add_path --global --prepend --move "$HOME/.local/bin"
+end
