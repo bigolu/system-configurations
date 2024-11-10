91a07c299656d44c18a21aae16b18f46fe447814
Biggie <code@bigo.lu>
2024-11-10 11:31:45 -0500

    Handle line continuations in nix direnv hash generator

--- scripts/ci/set-nix-direnv-hash.bash
+++ scripts/ci/set-nix-direnv-hash.bash
@@ -8,27 +8,31 @@ set -o nounset
 set -o pipefail
 shopt -s nullglob
 
-# TODO: The following vs code bash debugger extension does not support bash_rematch:
-# https://github.com/rogalmic/vscode-bash-debug/issues/183
-# Also bash_rematch is more error prone since it has global state. I should use
-# something else.
+# WARNING: The following vs code bash debugger extension does not support
+# bash_rematch: https://github.com/rogalmic/vscode-bash-debug/issues/183
 
+nix_direnv_url_pattern='https://raw.githubusercontent.com/nix-community/nix-direnv/.*/direnvrc'
 original_envrc="$(<.envrc)"
-regex='.*source_url ['\''"](https://raw.githubusercontent.com/nix-community/nix-direnv/.*/direnvrc.*)['\''"] ['\''"][^[:space:]]+['\''"].*'
+regex=".*(${nix_direnv_url_pattern}).*"
 if ! [[ "$original_envrc" =~ $regex ]]; then
-  echo 'Error: Could not find the direnv dependency statement' >&2
+  echo 'Error: Could not find the nix-direnv URL' >&2
   exit 1
 fi
 nix_direnv_url="${BASH_REMATCH[1]}"
 
 new_nix_direnv_hash="$(direnv fetchurl "$nix_direnv_url")"
 
-new_envrc=
-replacement_regex='(.*source_url ['\''"]https://raw.githubusercontent.com/nix-community/nix-direnv/.*/direnvrc.*['\''"] ['\''"])[^[:space:]]+(['\''"].*)'
+# \\\\\n matches a backslash followed by a newline
+separator=$'( |\\\\\n)'
+quote=$'[\'"]'
+nix_direnv_hash=$'[^\'"]+'
+everything_before_nix_direnv_hash=".*source_url${separator}+${quote}${nix_direnv_url_pattern}${quote}${separator}+${quote}"
+everything_after_nix_direnv_hash="${quote}.*"
+replacement_regex="($everything_before_nix_direnv_hash)${nix_direnv_hash}($everything_after_nix_direnv_hash)"
 if ! [[ "$original_envrc" =~ $replacement_regex ]]; then
-  echo 'Error: Could not find the direnv url' >&2
+  echo 'Error: Could not find the nix-direnv hash' >&2
   exit 1
 fi
-new_envrc="${BASH_REMATCH[1]}${new_nix_direnv_hash}${BASH_REMATCH[2]}"
+new_envrc="${BASH_REMATCH[1]}${new_nix_direnv_hash}${BASH_REMATCH[4]}"
 
 echo "$new_envrc" >.envrc
