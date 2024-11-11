ab4769971239c4c39ab5e91f01d3ba8f1b1a29c9
Biggie <code@bigo.lu>
2024-11-11 11:37:25 -0500

    Refactor reviewdog script

--- scripts/run-check-with-reviewdog.bash
+++ scripts/run-check-with-reviewdog.bash
@@ -5,12 +5,12 @@
 #
 # [this_script] [reviewdog_flags]... -- [check_command]...
 #
-# If reviewdog_flags does not contain '-f {format}' or '-efm {format}', then it's
+# If reviewdog_flags does not contain an error format flag, -f or -efm, then it's
 # assumed that the check_command will modify files. For example, a linter would set
 # the error format, but a formatter wouldn't. In this case, the git diff, if any,
 # will be reported.
 #
-# If reviewdog_flags does not contain '-name {name}' then the first token in
+# If reviewdog_flags does not contain the name flag, -name, then the first token in
 # check_command will be used.
 
 # shellcheck shell=bash
@@ -21,49 +21,20 @@ set -o pipefail
 shopt -s nullglob
 
 function main {
-  reporter=
-  if [[ "${CI:-}" = 'true' ]]; then
-    # TODO: Due to a bug in GitHub Actions, the checks reported by reviewdog get
-    # associated with the wrong workflow[1]. This discussion seems to be tracking the
-    # issue[2]. In the meantime, I'll use the annotations reporter which was made
-    # specifically to work around this issue[3].
-    #
-    # [1]: https://github.com/reviewdog/reviewdog/issues/403
-    # [2]: https://github.com/orgs/community/discussions/24616
-    # [3]: https://github.com/reviewdog/reviewdog/pull/1623
-    reporter='github-pr-annotations'
-  else
-    reporter='local'
-  fi
   reviewdog_flags=(
-    "-reporter=$reporter"
     -filter-mode=nofilter
     -fail-level=any
     -level=error
   )
-  did_reach_end_of_reviewdog_flags=
-  did_set_reviewdog_name_flag=
-  did_set_reviewdog_error_format_flag=
   check_command=()
-  for argument in "$@"; do
-    if [[ "$did_reach_end_of_reviewdog_flags" ]]; then
-      check_command+=("$argument")
-    elif [[ "$argument" = '--' ]]; then
-      did_reach_end_of_reviewdog_flags=1
-    else
-      reviewdog_flags+=("$argument")
-      if [[ "$argument" =~ ^-name(=|$) ]]; then
-        did_set_reviewdog_name_flag=1
-      elif [[ "$argument" =~ ^-(f|efm)(=|$) ]]; then
-        did_set_reviewdog_error_format_flag=1
-      fi
-    fi
-  done
-  if [[ ! "$did_set_reviewdog_name_flag" ]]; then
-    reviewdog_flags+=("-name" "${check_command[0]}")
-  fi
 
-  if [[ "$did_set_reviewdog_error_format_flag" ]]; then
+  set_reviewdog_reporter
+  parse_arguments "$@"
+  run_check
+}
+
+function run_check {
+  if did_set_reviewdog_error_format_flag; then
     "${check_command[@]}" | reviewdog "${reviewdog_flags[@]}"
   else
     if [[ "${CI:-}" = 'true' ]]; then
@@ -83,6 +54,52 @@ function main {
       # commits.
       fail_if_files_change "${check_command[@]}"
     fi
+  fi
+}
+
+function did_set_reviewdog_error_format_flag {
+  for flag in "${reviewdog_flags[@]}"; do
+    if [[ "$flag" =~ ^-(f|efm)(=|$) ]]; then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+function set_reviewdog_reporter {
+  if [[ "${CI:-}" = 'true' ]]; then
+    # TODO: Due to a bug in GitHub Actions, the checks reported by reviewdog get
+    # associated with the wrong workflow[1]. This discussion seems to be tracking the
+    # issue[2]. In the meantime, I'll use the annotations reporter which was made
+    # specifically to work around this issue[3].
+    #
+    # [1]: https://github.com/reviewdog/reviewdog/issues/403
+    # [2]: https://github.com/orgs/community/discussions/24616
+    # [3]: https://github.com/reviewdog/reviewdog/pull/1623
+    reviewdog_flags+=(-reporter=github-pr-annotations)
+  fi
+}
+
+function parse_arguments {
+  did_reach_end_of_reviewdog_flags=
+  did_set_reviewdog_name_flag=
+
+  for argument in "$@"; do
+    if [[ "$did_reach_end_of_reviewdog_flags" ]]; then
+      check_command+=("$argument")
+    elif [[ "$argument" = '--' ]]; then
+      did_reach_end_of_reviewdog_flags=1
+    else
+      reviewdog_flags+=("$argument")
+      if [[ "$argument" =~ ^-name(=|$) ]]; then
+        did_set_reviewdog_name_flag=1
+      fi
+    fi
+  done
+
+  if [[ ! "$did_set_reviewdog_name_flag" ]]; then
+    reviewdog_flags+=("-name" "${check_command[0]}")
   fi
 }
 
