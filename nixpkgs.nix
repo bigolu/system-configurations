3a86197175ca9eb7cb5b00d0a4e2301a2f7d72c9
Biggie <code@bigo.lu>
2024-11-24 12:05:17 -0500

    Refactor

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,17 +1,11 @@
-# I add this file to the NIX_PATH as 'nixpkgs' so my nix-shell shebang scripts
-# can use the same version of nixpkgs as my flake, with my overlay applied.
+# I add this file to the NIX_PATH as 'nixpkgs' so my nix-shell shebang scripts can
+# use the same version of nixpkgs as my flake, with my overlay applied.
 originalArgs:
 let
   flake = import ./default.nix;
 
-  originalArgsWithFlakeOverlay =
-    let
-      originalOverlaysWithFlakeOverlay =
-        let
-          originalOverlays = originalArgs.overlays or [ ];
-        in
-        originalOverlays ++ [ flake.lib.overlay ];
-    in
-    originalArgs // { overlays = originalOverlaysWithFlakeOverlay; };
+  originalArgsWithFlakeOverlay = originalArgs // {
+    overlays = (originalArgs.overlays or [ ]) ++ [ flake.lib.overlay ];
+  };
 in
 import flake.inputs.nixpkgs originalArgsWithFlakeOverlay
