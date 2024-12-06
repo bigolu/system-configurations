6949cf73e19691e1ca4a5c248ca98fb44674222e
Biggie <code@bigo.lu>
2024-12-05 22:17:23 -0500

    neovim: Refactor statusline

--- dotfiles/neovim/lua/terminal/statusbars/statusline.lua
+++ dotfiles/neovim/lua/terminal/statusbars/statusline.lua
@@ -1,10 +1,53 @@
 -- vim:foldmethod=marker
 
-vim.o.laststatus = 3
-vim.o.statusline = "%!v:lua.StatusLine()"
-
 -- statusline helpers {{{
-local function get_mode_indicator()
+local function collect_to_list(acc, _index, item)
+  table.insert(acc, item)
+  return acc
+end
+
+local function make_statusline(...)
+  local right_border = "%#StatusLine# %#StatusLinePowerlineOuter#"
+  local right_border_length =
+    vim.api.nvim_eval_statusline(right_border, {}).width
+
+  local left_border = "%#StatusLinePowerlineOuter#"
+  local left_border_length = vim.api.nvim_eval_statusline(left_border, {}).width
+
+  local space_remaining = vim.o.columns
+    - (left_border_length + right_border_length)
+
+  local item_separator = "  "
+  local item_separator_length = #item_separator
+
+  local function has_space(index, item)
+    -- I need to remove aligners because they affect the width.
+    local length = vim.api.nvim_eval_statusline(item:gsub("%%=", ""), {}).width
+    space_remaining = space_remaining - length
+    if index > 1 then
+      space_remaining = space_remaining - item_separator_length
+    end
+    return space_remaining >= 0
+  end
+
+  -- iter() filters out nil values
+  local items =
+    vim.iter({ ... }):enumerate():filter(has_space):fold({}, collect_to_list)
+
+  return left_border .. table.concat(items, item_separator) .. right_border
+end
+-- }}}
+
+local function is_pattern_in_buffer(pattern)
+  -- PERF
+  if vim.fn.line("$") < 300 then
+    return vim.fn.search(pattern, "nw", 0, 50) > 0
+  else
+    return false
+  end
+end
+
+local function get_mode_and_left_items()
   local normal = "NORMAL"
   local operator_pending = "OPERATOR-PENDING"
   local visual = "VISUAL"
@@ -60,140 +103,13 @@ local function get_mode_indicator()
     ["!"] = shell,
     ["t"] = terminal,
   }
-  local mode = mode_map[vim.api.nvim_get_mode().mode]
-  if mode == nil then
-    mode = "?"
-  end
-
-  return "%#StatusLinePowerlineOuter#" .. "%#status_line_mode# " .. mode
-end
-
-local function collect_to_list(acc, _index, item)
-  table.insert(acc, item)
-  return acc
-end
-
-local function make_statusline(left_items, right_items)
-  local mode_indicator = get_mode_indicator()
-  local mode_indicator_length =
-    vim.api.nvim_eval_statusline(mode_indicator, {}).width
-
-  local item_separator = "  "
-  local item_separator_length = #item_separator
-
-  local showcmd = "%#StatusLineShowcmd#%S"
-  local statusline_separator = "%#StatusLineFill# %="
-    .. showcmd
-    .. "%#StatusLineFill#%= "
-  -- I need to remove the aligners from the statusline separator because they affect
-  -- the width.
-  local statusline_separator_length =
-    vim.api.nvim_eval_statusline(statusline_separator:gsub("%%=", ""), {}).width
-
-  local right_side_padding = "%#StatusLine# %#StatusLinePowerlineOuter#"
-  local right_side_padding_length =
-    vim.api.nvim_eval_statusline(right_side_padding, {}).width
-
-  local space_remaining = vim.o.columns
-    - (
-      mode_indicator_length
-      + statusline_separator_length
-      + right_side_padding_length
-    )
-  local function has_space(index, item)
-    local length = vim.api.nvim_eval_statusline(item, {}).width
-    space_remaining = space_remaining - length
-    if index > 1 then
-      space_remaining = space_remaining - item_separator_length
-    end
-    return space_remaining >= 0
-  end
-  local function filter_items(items)
-    return vim
-      -- iter() filters out nil values
-      .iter(items)
-      :enumerate()
-      :filter(has_space)
-      :fold({}, collect_to_list)
-  end
-
-  left_items = filter_items(left_items)
-  -- TODO: I'm not accounting for the length of these strings in space_remaining
-  local left_side = mode_indicator
-    .. (#left_items > 0 and " %#StatusLinePowerlineInner#%#StatusLinePowerlineOuter#" or "")
-    .. table.concat(left_items, "%#StatusLineSeparator#" .. item_separator)
-    .. (#left_items > 0 and "%#StatusLinePowerlineInner#" or " ")
-
-  right_items = filter_items(right_items)
-  local right_side = table.concat(right_items, item_separator)
-    .. right_side_padding
-
-  return left_side .. statusline_separator .. right_side
-end
-
-local function is_pattern_in_buffer(pattern)
-  -- PERF
-  if vim.fn.line("$") < 300 then
-    return vim.fn.search(pattern, "nw", 0, 50) > 0
-  else
-    return false
-  end
-end
--- }}}
-
-function StatusLine()
-  local basename = "%t"
-
-  local position = "%#StatusLine#" .. "%03l:%03c"
-
-  local fileformat = nil
-  if vim.o.fileformat == "mac" then
-    fileformat = " CR"
-  elseif vim.o.fileformat == "dos" then
-    fileformat = " CRLF"
-  end
-  if fileformat ~= nil then
-    fileformat = "%#StatusLine#" .. fileformat
-  end
-
-  local fileencoding = nil
-  if #vim.o.fileencoding > 0 and vim.o.fileencoding ~= "utf-8" then
-    fileencoding = "%#StatusLine#" .. string.upper(vim.o.fileencoding)
-  end
-
-  local filetype = nil
-  if string.len(vim.o.filetype) > 0 then
-    filetype = "%#StatusLine#" .. vim.o.filetype
-  end
+  local mode = "%#status_line_mode# "
+    .. (mode_map[vim.api.nvim_get_mode().mode] or "?")
 
   local readonly = nil
   if vim.o.readonly then
     local indicator = "󰍁 "
     readonly = "%#StatusLineStandoutText#" .. indicator
-  end
-
-  local reg_recording = nil
-  local recording_register = vim.fn.reg_recording()
-  if recording_register ~= "" then
-    reg_recording = "%#StatusLineRecordingIndicator# %#StatusLineNormal#REC@"
-      .. recording_register
-  end
-
-  local lsp_info = nil
-  local language_server_count_for_current_buffer =
-    -- TODO: A definition of get_clients in mini.nvim is being used by linters
-    -- instead of the one from the neovim runtime. Since it has a different signature
-    -- than the real one, linters think I'm calling it incorrectly.
-    ---@diagnostic disable-next-line: redundant-parameter
-    #vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
-  if language_server_count_for_current_buffer > 0 then
-    lsp_info = "%#StatusLine# " .. language_server_count_for_current_buffer
-  end
-
-  local maximized = nil
-  if IsMaximized then
-    local indicator = " "
-    maximized = "%#StatusLine#" .. indicator
   end
 
   local diagnostic_data = {
@@ -249,19 +165,91 @@ function StatusLine()
     mixed_line_endings = "%#StatusLineErrorText#[ mixed line-endings]"
   end
 
-  return make_statusline({
-    readonly,
-    diagnostics,
-    mixed_indentation_indicator,
-    mixed_line_endings,
-    reg_recording,
-  }, {
+  local reg_recording = nil
+  local recording_register = vim.fn.reg_recording()
+  if recording_register ~= "" then
+    reg_recording = "%#StatusLineRecordingIndicator# %#StatusLineNormal#REC@"
+      .. recording_register
+  end
+
+  -- iter() filters out nil values
+  local items = vim
+    .iter({
+      readonly,
+      diagnostics,
+      mixed_indentation_indicator,
+      mixed_line_endings,
+      reg_recording,
+    })
+    :totable()
+  local items_joined = (
+    #items > 0
+      and " %#StatusLinePowerlineInner#%#StatusLinePowerlineOuter#"
+    or ""
+  )
+    .. table.concat(items, "%#StatusLineSeparator#" .. "  ")
+    .. (#items > 0 and "%#StatusLinePowerlineInner#" or " ")
+
+  return mode .. items_joined
+end
+
+function StatusLine()
+  local showcmd = "%#StatusLineShowcmd#%S"
+  local aligner = "%#StatusLineFill#%="
+  local statusline_separator = aligner .. showcmd .. aligner
+
+  local maximized = nil
+  if IsMaximized then
+    maximized = "%#StatusLine# "
+  end
+
+  local lsp_info = nil
+  local language_server_count_for_current_buffer =
+    -- TODO: A definition of get_clients in mini.nvim is being used by linters
+    -- instead of the one from the neovim runtime. Since it has a different signature
+    -- than the real one, linters think I'm calling it incorrectly.
+    ---@diagnostic disable-next-line: redundant-parameter
+    #vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf() })
+  if language_server_count_for_current_buffer > 0 then
+    lsp_info = "%#StatusLine# " .. language_server_count_for_current_buffer
+  end
+
+  local filetype = nil
+  if string.len(vim.o.filetype) > 0 then
+    filetype = "%#StatusLine#" .. vim.o.filetype
+  end
+
+  local fileformat = nil
+  if vim.o.fileformat == "mac" then
+    fileformat = " CR"
+  elseif vim.o.fileformat == "dos" then
+    fileformat = " CRLF"
+  end
+  if fileformat ~= nil then
+    fileformat = "%#StatusLine#" .. fileformat
+  end
+
+  local fileencoding = nil
+  if #vim.o.fileencoding > 0 and vim.o.fileencoding ~= "utf-8" then
+    fileencoding = "%#StatusLine#" .. string.upper(vim.o.fileencoding)
+  end
+
+  local position = "%#StatusLine#" .. "%l:%c"
+
+  local basename = "%t"
+
+  return make_statusline(
+    get_mode_and_left_items(),
+    statusline_separator,
     maximized,
-    lsp_info,
+    position,
     filetype,
     fileformat,
     fileencoding,
-    position,
-    basename,
-  })
+    lsp_info,
+    basename
+  )
 end
+
+vim.o.laststatus = 3
+vim.o.statusline = "%!v:lua.StatusLine()"
