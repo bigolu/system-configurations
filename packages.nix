0ba8190bd08d8e78efb2640f7f6f23777cb22d16
Biggie <code@bigo.lu>
2024-12-13 21:40:55 -0500

    Refactor

--- packages.nix
+++ packages.nix
@@ -1,4 +1,6 @@
-# This file provides easy access to the package set used inside the flake, just
-# import this file. This way code outside the flake can use the same packages, for
-# consistency. For example, scripts with nix-shell shebangs.
+# This file provides easy access to the package set used inside the flake i.e. the
+# `pkgs` argument that is passed to each flake-parts module[1]. This way code outside
+# the flake, like nix-shell shebangs, can use the same packages.
+#
+# [1]: https://flake.parts/module-arguments.html?highlight=pkgs#pkgs
 (import ./default.nix).currentSystem._module.args.pkgs
