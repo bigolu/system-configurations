99af509d170ab5fb6fa25aca0c6c1a5d9a089423
Biggie <code@bigo.lu>
2024-12-22 12:28:55 -0500

    nix: Get neovim plugin list through a nix expression
    This way I don't need to check the list in or ensure it's up-to-date.

--- nix/flake-modules/home-configurations/default.nix
+++ nix/flake-modules/home-configurations/default.nix
@@ -28,7 +28,6 @@ let
       [
         "comma"
         "moreutils"
-        "ast-grep"
         "timg"
         "ripgrep-all"
         "lesspipe"
--- nix/overlays/private/plugins/vim-plugins.nix
+++ nix/overlays/private/plugins/vim-plugins.nix
@@ -1,15 +1,19 @@
-{ inputs, makePluginPackages, ... }:
+{
+  inputs,
+  makePluginPackages,
+  utils,
+}:
 final: prev:
 let
   inherit (inputs.nixpkgs) lib;
-  inherit (lib) splitString;
+  inherit (lib) splitString pipe init;
   inherit (builtins)
-    filter
     readFile
     hasAttr
     replaceStrings
     listToAttrs
     ;
+  inherit (utils) projectRoot;
 
   vimPluginsFromFlake =
     let
@@ -34,32 +38,48 @@ let
 
   myVimPlugins =
     let
-      pluginNames = filter (name: name != "") (
-        splitString "\n" (readFile "${inputs.self}/dotfiles/neovim/plugin-names.txt")
-      );
       replaceDotsWithDashes = replaceStrings [ "." ] [ "-" ];
     in
-    final.lib.recurseIntoAttrs (
-      listToAttrs (
-        map (
-          pluginName:
-          let
-            formattedPluginName = replaceDotsWithDashes pluginName;
-            package =
-              if hasAttr pluginName final.vimPlugins then
-                final.vimPlugins.${pluginName}
-              else if hasAttr formattedPluginName final.vimPlugins then
-                final.vimPlugins.${formattedPluginName}
-              else
-                abort "Failed to find vim plugin: ${pluginName}";
-          in
-          {
-            name = pluginName;
-            value = package;
-          }
-        ) pluginNames
+    pipe
+      (final.runCommand "neovim-plugin-names"
+        {
+          nativeBuildInputs = with final; [
+            ast-grep
+            jq
+            coreutils
+            gnused
+          ];
+        }
+        ''
+          # shellcheck disable=SC2016
+          # The dollar signs are for ast-grep
+          ast-grep --lang lua --pattern 'Plug($ARG $$$)' --json=compact ${
+            projectRoot + /dotfiles/neovim/lua
+          } \
+            | jq --raw-output '.[].metaVariables.single.ARG.text' \
+            | cut -d'/' -f2 \
+            | sed 's/.$//' \
+            | sort --ignore-case --dictionary-order --unique \
+            > $out
+        ''
       )
-    );
+      [
+        readFile
+        (splitString "\n")
+        # The file ends in a newline so the last line will be empty
+        init
+        (map (
+          pluginName:
+          final.vimPlugins.${pluginName} or final.vimPlugins.${replaceDotsWithDashes pluginName}
+          or (abort "Failed to find vim plugin: ${pluginName}")
+        ))
+        (map (plugin: {
+          name = plugin.pname;
+          value = plugin;
+        }))
+        listToAttrs
+        final.lib.recurseIntoAttrs
+      ];
 in
 {
   inherit myVimPlugins;
