815112538887544c35077c272ce20ddd93334754
Biggie <code@bigo.lu>
2024-12-23 13:03:22 -0500

    ci: Run `direnv allow` before `direnv exec`

--- scripts/ci/run-in-direnv.sh
+++ scripts/ci/run-in-direnv.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env sh
 
-nix shell \
-  --impure --expr 'import ./nix/packages.nix' \
-  bash direnv \
-  --command env DEV_SHELL='ci-default' direnv exec . "$@"
+main() {
+  run_in_nix_shell_containing_direnv \
+    direnv allow .
+  run_in_nix_shell_containing_direnv \
+    env DEV_SHELL='ci-default' direnv exec . "$@"
+}
+
+run_in_nix_shell_containing_direnv() {
+  nix shell \
+    --impure --expr 'import ./nix/packages.nix' \
+    bash direnv \
+    --command "$@"
+}
+
+main "$@"
