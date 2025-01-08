b8237390c51071a7c62cb87679276060b02b0435
Biggie <code@bigo.lu>
2025-01-08 14:55:19 -0500

    home-manager: Install fish plugins with home.packages instead of using programs.fish.plugins
    The plugins' configuration files are already placed in <nix-profile>/share/fish by
    `nixpkgs.fishPlugins.buildFishPlugin` and since <nix-profile>/share is in
    XDG_DATA_DIRS, fish will read them. Also, home-manager wasn't loading the config
    files if they didn't have an init.fish file.

--- nix/flake-modules/home-configurations/modules/fish.nix
+++ nix/flake-modules/home-configurations/modules/fish.nix
@@ -4,35 +4,22 @@
 }:
 {
   # Using this so Home Manager can include its generated completion scripts
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      # This way, if I reload my fish shell with `exec fish` the config files get
-      # read again. The default behaviour is for config files to only be sourced
-      # once.
-      set --unexport __HM_SESS_VARS_SOURCED
-      set --unexport __fish_home_manager_config_sourced
-    '';
-    plugins =
-      let
-        packages = with pkgs.fishPlugins; [
-          autopair-fish
-          fish-async-prompt
-          done
-          # Using this to get shell completion for programs added to the path through
-          # nix+direnv. Issue to upstream into direnv:
-          # https://github.com/direnv/direnv/issues/443
-          fish-completion-sync
-        ];
-      in
-      map (package: {
-        name = package.pname;
-        src = package;
-      }) packages;
-  };
+  programs.fish.enable = true;
 
-  # For ssh.fish
-  home.packages = with pkgs; [ ncurses ];
+  home.packages =
+    (with pkgs; [
+      # For ssh.fish
+      ncurses
+    ])
+    ++ (with pkgs.fishPlugins; [
+      autopair-fish
+      fish-async-prompt
+      done
+      # Using this to get shell completion for programs added to the path through
+      # nix+direnv. Issue to upstream into direnv:
+      # https://github.com/direnv/direnv/issues/443
+      fish-completion-sync
+    ]);
 
   repository = {
     symlink.xdg.configFile = {
