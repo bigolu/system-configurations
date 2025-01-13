f63082921d5432e80b09323d9d98e202d0f342cb
Biggie <code@bigo.lu>
2025-01-13 12:11:35 -0500

    Use `fd` instead of a glob

--- scripts/lefthook/sync-direnv.bash
+++ scripts/lefthook/sync-direnv.bash
@@ -1,7 +1,7 @@
 #! /usr/bin/env cached-nix-shell
 #! nix-shell --keep FLAKE_PACKAGE_SET_FILE
 #! nix-shell -i nix-shell-interpreter
-#! nix-shell --packages "with (import (builtins.getEnv \"FLAKE_PACKAGE_SET_FILE\")); [nix-shell-interpreter coreutils nix-output-monitor]"
+#! nix-shell --packages "with (import (builtins.getEnv \"FLAKE_PACKAGE_SET_FILE\")); [nix-shell-interpreter coreutils fd nix-output-monitor]"
 
 set -o errexit
 set -o nounset
@@ -22,16 +22,7 @@ function main {
 # Prints the store path to the dev shell that is currently cached by nix-direnv.
 # Prints nothing if nix-direnv has not cached a dev shell.
 function get_dev_shell_store_path {
-  flake_profile=''
-  # This glob will match two files that are created by nix-direnv:
-  # flake-profile-<hash> and flake-profile-<hash>.rc. The one that doesn't end in .rc
-  # will be a symlink to the dev shell store path.
-  for flake_profile_candidate in ".direnv/flake-profile-"*; do
-    if [[ $flake_profile_candidate != *.rc ]]; then
-      flake_profile="$flake_profile_candidate"
-      break
-    fi
-  done
+  flake_profile="$(fd --type symlink --exclude '*.rc' 'flake-profile-.*' .direnv)"
 
   if [[ -n $flake_profile ]]; then
     # Prints the target of the symlink i.e. the dev shell store path
