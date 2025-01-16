180b9a682e8c1e0031625f97ca605aeba1d046fa
Biggie <code@bigo.lu>
2025-01-15 19:18:23 -0500

    Move nix expression from a bash string to its own file

---
+++ nix/flake-modules/private.nix
@@ -0,0 +1,36 @@
+{ lib, self, ... }:
+let
+  inherit (builtins) concatStringsSep attrNames;
+  inherit (lib) hasPrefix;
+in
+{
+  flake.private.systemConfigurationsAsMarkdown =
+    let
+      homeManagerConfigNames = builtins.filter (name: !hasPrefix "portable-home" name) (
+        attrNames self.homeConfigurations
+      );
+      homeManagerPlatformFetcher = name: self.homeConfigurations.${name}.activationPackage.system;
+
+      nixDarwinConfigNames = builtins.attrNames self.darwinConfigurations;
+      nixDarwinPlatformFetcher = name: self.darwinConfigurations.${name}.system.system;
+
+      makeListItems =
+        platformFetcher: configNames:
+        let
+          makeListItem = name: "  - ${name} / ${platformFetcher name}";
+        in
+        concatStringsSep "\n" (map makeListItem configNames);
+    in
+    ''
+
+
+      - Home Manager
+
+      ${makeListItems homeManagerPlatformFetcher homeManagerConfigNames}
+
+      - nix-darwin
+
+      ${makeListItems nixDarwinPlatformFetcher nixDarwinConfigNames}
+
+    '';
+}
--- scripts/code-generation/add-system-configurations-to-readme.bash
+++ scripts/code-generation/add-system-configurations-to-readme.bash
@@ -10,7 +10,7 @@ shopt -s nullglob
 
 function main {
   configs="$(
-    get_configs
+    nix eval --raw .#private.systemConfigurationsAsMarkdown
     # Add a character to the end of the output to preserve trailing newlines.
     printf x
   )"
@@ -19,42 +19,6 @@ function main {
   perl -0777 -w -i -pe \
     "s{(<!-- START_CONFIGURATIONS -->).*(<!-- END_CONFIGURATIONS -->)}{\$1$configs\$2}igs" \
     README.md
-}
-
-function get_configs {
-  nix eval --raw --impure --expr "
-    let
-      flake = import ./default.nix;
-
-      makeListItems = platformFetcher: configNames: let
-        makeListItem = name:
-          \"  - \${name} / \${platformFetcher name}\";
-      in
-        builtins.concatStringsSep \"\n\" (map makeListItem configNames);
-
-      homeManagerConfigNames = builtins.filter
-        (name: !flake.inputs.nixpkgs.lib.hasPrefix \"portable-home\" name)
-        (builtins.attrNames flake.outputs.homeConfigurations);
-      homeManagerPlatformFetcher = name:
-        flake.outputs.homeConfigurations.\${name}.activationPackage.system;
-
-      nixDarwinConfigNames = builtins.attrNames flake.outputs.darwinConfigurations;
-      nixDarwinPlatformFetcher = name:
-        flake.outputs.darwinConfigurations.\${name}.system.system;
-    in
-      ''
-
-
-        - Home Manager
-
-        \${makeListItems homeManagerPlatformFetcher homeManagerConfigNames}
-
-        - nix-darwin
-
-        \${makeListItems nixDarwinPlatformFetcher nixDarwinConfigNames}
-
-      ''
-  "
 }
 
 main
