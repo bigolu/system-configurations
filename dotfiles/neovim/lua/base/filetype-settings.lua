cdd39f907a0818583f1322315c5dfcfc2e5bc826
Biggie <code@bigo.lu>
2025-01-19 11:04:17 -0500

    neovim: Set filetype to systemd for files ending in .service or .target

--- dotfiles/neovim/lua/base/filetype-settings.lua
+++ dotfiles/neovim/lua/base/filetype-settings.lua
@@ -1,6 +1,7 @@
 -- vim:foldmethod=marker
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  nested = true,
   pattern = ".envrc",
   callback = function()
     vim.opt_local.filetype = "sh"
@@ -8,9 +9,21 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 })
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  nested = true,
   pattern = "*/git/config",
   callback = function()
     vim.opt_local.filetype = "gitconfig"
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  nested = true,
+  pattern = "*.{service,target}",
+  callback = function()
+    -- TODO: Why doesn't this work without `defer_fn`?
+    vim.defer_fn(function()
+      vim.opt_local.filetype = "systemd"
+    end, 0)
   end,
 })
 
