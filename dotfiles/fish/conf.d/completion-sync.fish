ecf8abbc0f905e802e89093a64ac2d10e057171a
Biggie <code@bigo.lu>
2025-02-06 16:28:00 -0500

    fish-completion-sync: Handle more edge cases

--- dotfiles/fish/conf.d/completion-sync.fish
+++ dotfiles/fish/conf.d/completion-sync.fish
@@ -2,16 +2,16 @@ if not status is-interactive
     exit
 end
 
-set -x COMPLETION_SYNC_DEBUG 1
+set -x COMPLETE_FISH_DEBUG 1
 function _complete_fish_debug --argument-names message
-    if test -n "$COMPLETION_SYNC_DEBUG"
+    if test -n "$COMPLETE_FISH_DEBUG"
         echo "[completion-sync] $message" >&2
     end
 end
 
 function _complete_fish_remove_managed_files_from_complete_path
     set -l removed_paths
-    for addition_file in $COMPLETE_FISH_PATH
+    for addition_file in $_complete_fish_files
         if set -l index (contains --index -- (path dirname $addition_file) $fish_complete_path)
             set --append removed_paths $fish_complete_path[$index]
             set --erase fish_complete_path[$index]
@@ -20,8 +20,6 @@ function _complete_fish_remove_managed_files_from_complete_path
     if test (count $removed_paths) -gt 0
         _complete_fish_debug 'Paths removed from fish_complete_path:'\n"$(string join \n $removed_paths)"
     end
-
-    set --erase COMPLETE_FISH_PATH
 end
 
 function _complete_fish_add
@@ -42,32 +40,70 @@ function _complete_fish_add
             return
         end
 
-        # Fish will treat variables with 'PATH' suffix like arrays, but when
-        # exported the elements will be joined by ':'.
-        #
-        # This needs to be exported for
-        # `_complete_fish_remove_managed_files_from_complete_path` to work.
-        if set --query --export COMPLETE_FISH_PATH
-            set --append COMPLETE_FISH_PATH $file
+        if set --query --global _complete_fish_files
+            set --append _complete_fish_files $file
         else
-            set --global --export COMPLETE_FISH_PATH $file
+            set --global _complete_fish_files $file
         end
 
         set -l added_entries_string "$(string join --no-empty \n $added_entries)"
         # The autocomplete entries for `ruff` exceeded the max length of an
         # environment variable so a global variable should be used.
-        if set --query --global COMPLETE_FISH_ENTRIES
-            set --append COMPLETE_FISH_ENTRIES $added_entries_string
+        if set --query --global _complete_fish_entries
+            set --append _complete_fish_entries $added_entries_string
         else
-            set --global COMPLETE_FISH_ENTRIES $added_entries_string
+            set --global _complete_fish_entries $added_entries_string
         end
     end
 end
 
-function _complete_fish_remove
-    for file_index in (seq (count $COMPLETE_FISH_PATH))
-        set -l added_entries (string split --no-empty \n "$COMPLETE_FISH_ENTRIES[$file_index]")
-        set -l file $COMPLETE_FISH_PATH[$file_index]
+function _complete_fish_post_load
+    _complete_fish_debug 'In post load'
+
+    set -l xdg_files
+    for dir in $XDG_DATA_DIRS
+        # This was in XDG before direnv was loaded so ignore it
+        if contains $dir $COMPLETE_FISH_OLD_XDG_PATH
+            continue
+        end
+
+        set -l fish_dir $dir'/fish/vendor_completions.d'
+        if not test -d $fish_dir
+            continue
+        end
+        set --append xdg_files $fish_dir/*
+    end
+    if test (count $xdg_files) -eq 0
+        return
+    end
+
+    _complete_fish_debug 'Adding completions for these files:'\n"$(string join \n $xdg_files)"
+    _complete_fish_add $xdg_files
+
+    # On startup, Fish will add XDG_DATA_DIRS to fish_complete_path so we'll
+    # remove the ones we're managing.
+    #
+    # Cases where we need to do this:
+    #   - A sub shell is started in a direnv environment
+    #   - `exec` is used in a direnv environment
+    #
+    # TODO: I should only be doing this if we're in one of the cases above, but
+    # I think it's safe to just always do it.
+    _complete_fish_remove_managed_files_from_complete_path
+end
+
+function _complete_fish_post_unload
+    _complete_fish_debug 'In post unload'
+
+    if test (count $_complete_fish_files) -eq 0
+        return
+    end
+
+    _complete_fish_debug 'Removing completions for these files:'\n"$(string join \n $_complete_fish_files)"
+
+    for file_index in (seq (count $_complete_fish_files))
+        set -l added_entries (string split --no-empty \n "$_complete_fish_entries[$file_index]")
+        set -l file $_complete_fish_files[$file_index]
         set -l command (path change-extension '' (path basename $file))
         set -l current_entries (complete $command)
         for entry in $added_entries
@@ -78,88 +114,120 @@ function _complete_fish_remove
         complete --erase $command
         printf %s\n $current_entries | source
     end
-    set COMPLETE_FISH_PATH
-    set COMPLETE_FISH_ENTRIES
+    set _complete_fish_files
+    set _complete_fish_entries
 end
 
-function _complete_fish_post_load
-    _complete_fish_debug 'In post load'
+function _complete_fish_pre_load
+    _complete_fish_debug 'In pre load'
 
-    # On startup, Fish will add XDG_DATA_DIRS to fish_complete_path so we'll
-    # remove the ones we're managing.
+    # This variable holds the value of XDG_DATA_DIRS before the direnv environment is
+    # loaded. It's exported to account for the following edge cases:
+    #   - A sub shell is started in a direnv environment: The XDG_DATA_DIRS in the
+    #     sub shell will contain everything that direnv adds to it.
+    #   - `exec` is used in a direnv environment: The XDG_DATA_DIRS in the new
+    #     process will contain everything that direnv adds to it.
+    #   - The user moves from one direnv environment directly to another direnv
+    #     environment: XDG_DATA_DIRS will contain everything the old direnv
+    #     environment added to it. This is a limitation of how I call the pre_load
+    #     hook. There is a comment where _complete_fish_pre_load is called with
+    #     details on the limitation.
     #
-    # Cases where we need to do this:
-    #   - A sub shell is started in a direnv environment
-    #   - `exec` is used in a direnv environment
-    #
-    # I'm not sure how to detect all of these cases so I'll just always do it.
-    #
-    # If we ever change our minds and decide to leave them, we need to
-    # remove the code further down that ignores files that are already in
-    # fish_complete_path.
-    _complete_fish_remove_managed_files_from_complete_path
-
-    set -l xdg_files
-    for dir in $XDG_DATA_DIRS
-        set dir $dir'/fish/vendor_completions.d'
-
-        if not test -d $dir
-            continue
-        end
-
-        # We only want to add completions for directories added to XDG
-        # by direnv. I'm not sure how to do that without a 'pre-load'
-        # hook. Instead, we'll ignore directories that are already in
-        # `fish_complete_path`. Fish adds those on startup.
-        if contains $dir $fish_complete_path
-            continue
-        end
-
-        set --append xdg_files $dir/*
+    # It needs to end in 'PATH' so fish can treat it like an array, but join it with
+    # ':' when exported.
+    if not set --query --global --export COMPLETE_FISH_OLD_XDG_PATH
+        set --global --export COMPLETE_FISH_OLD_XDG_PATH $XDG_DATA_DIRS
     end
-    if test (count $xdg_files) -eq 0
+end
+
+function _complete_fish_nearest_envrc
+    set -l current_directory (pwd)
+    while true
+        if test -e $current_directory/.envrc
+            echo $current_directory/.envrc
+            return
+        end
+
+        set -l parent_directory $current_directory/..
+        # This will happen when we hit the root directory e.g. '/'
+        if test $current_directory -ef $parent_directory
+            return
+        end
+        set current_directory $parent_directory
+    end
+end
+
+function _complete_fish_is_moving_directly_to_new_direnv
+    if not set --query DIRENV_DIR
+        echo false
         return
     end
 
-    _complete_fish_debug 'Adding completions for these files:'\n"$(string join \n $xdg_files)"
-    _complete_fish_add $xdg_files
-end
-
-function _complete_fish_pre_unload
-    _complete_fish_debug 'In pre unload'
-
-    if test (count $COMPLETE_FISH_PATH) -eq 0
+    set -l nearest_envrc "$(_complete_fish_nearest_envrc)"
+    if test -z "$nearest_envrc"
+        echo false
         return
     end
 
-    _complete_fish_debug 'Removing completions for these files:'\n"$(string join \n $COMPLETE_FISH_PATH)"
-    _complete_fish_remove
-end
-
-function _complete_fish_main
-    if not set --query --global COMPLETE_FISH_DIRENV_LOADED
-        and set --query DIRENV_DIR
-
-        set --global COMPLETE_FISH_DIRENV_LOADED true
-        _complete_fish_post_load
-    else if set --query --global COMPLETE_FISH_DIRENV_LOADED
-        and not set --query DIRENV_DIR
-
-        set --erase COMPLETE_FISH_DIRENV_LOADED
-        _complete_fish_pre_unload
+    # DIRENV_DIR starts with a '-', the `string sub` removes it
+    set -l direnv_dir (string sub --start 2 -- $DIRENV_DIR)
+    if test (path resolve (path dirname $nearest_envrc)) != (path resolve $direnv_dir)
+        echo true
+        return
     end
+
+    echo false
 end
 
-# Run after direnv's prompt hook
+# Replace direnv's prompt hook with one that will call out pre_load, post_load,
+# and post_unload hooks.
 function _complete_fish_register --on-event fish_prompt
     functions --erase (status current-function)
 
     functions --copy __direnv_export_eval __direnv_export_eval_backup
     function __direnv_export_eval --on-event fish_prompt
+        set -l is_moving_directly_to_new_direnv (_complete_fish_is_moving_directly_to_new_direnv)
+
+        # See if pre_load should run
+        set -l nearest_envrc "$(_complete_fish_nearest_envrc)"
+        if test -n "$nearest_envrc"
+            if not set --query DIRENV_DIR
+                _complete_fish_pre_load
+                set --global _complete_fish_pre_load_done true
+            else if not set --query --global _complete_fish_pre_load_done
+                # This accounts for `exec` and sub shells
+                _complete_fish_pre_load
+                set --global _complete_fish_pre_load_done true
+            else if test $is_moving_directly_to_new_direnv = true
+                # TODO: I want to call this after the old direnv is unloaded and
+                # before the new direnv is loaded, but direnv does them both in its
+                # single hook.
+                _complete_fish_pre_load
+                set --global _complete_fish_pre_load_done true
+            end
+        end
+
         __direnv_export_eval_backup
-        _complete_fish_main
+
+        if not set --query --global _complete_fish_direnv_loaded
+            and set --query DIRENV_DIR
+
+            set --global _complete_fish_direnv_loaded true
+            _complete_fish_post_load
+        else if set --query --global _complete_fish_direnv_loaded
+            and not set --query DIRENV_DIR
+
+            set --erase _complete_fish_direnv_loaded
+            _complete_fish_post_unload
+        else if test $is_moving_directly_to_new_direnv = true
+            # TODO: I want to call this after the old direnv is unloaded and
+            # before the new direnv is loaded, but direnv does them both in its
+            # single hook.
+            _complete_fish_post_unload
+
+            _complete_fish_post_load
+        end
     end
 
-    # In case direnv's hook hasn't run yet, run it now
     __direnv_export_eval
 end
