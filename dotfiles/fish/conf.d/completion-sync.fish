2865ce5b3fa7b58e35545c032fbf62c0fb367253
Biggie <code@bigo.lu>
2025-02-07 13:02:49 -0500

    completion-sync: Implement pre_unload and add more cases to consider

--- dotfiles/fish/conf.d/completion-sync.fish
+++ dotfiles/fish/conf.d/completion-sync.fish
@@ -10,67 +10,6 @@ end
 # How it works:
 # TODO
 
-function _complete_fish_debug --argument-names message
-    if test -n "$COMPLETE_FISH_DEBUG"
-        echo "[completion-sync] $message" >&2
-    end
-end
-
-function _complete_fish_remove_managed_files_from_complete_path
-    set -l removed_paths
-    for addition_file in $_complete_fish_files
-        if set -l index (contains --index -- (path dirname $addition_file) $fish_complete_path)
-            set --append removed_paths $fish_complete_path[$index]
-            set --erase fish_complete_path[$index]
-        end
-    end
-    if test (count $removed_paths) -gt 0
-        _complete_fish_debug 'Paths removed from fish_complete_path:'\n"$(string join \n $removed_paths)"
-    end
-end
-
-function _complete_fish_add
-    for file in $argv
-        set -l command (path change-extension '' (path basename $file))
-
-        set -l old_entries (complete $command)
-        source $file
-        set -l new_entries (complete $command)
-        set -l added_entries
-        for new_entry in $new_entries
-            if not contains $new_entry $old_entries
-                set --append added_entries $new_entry
-            end
-        end
-        if test (count $added_entries) -eq 0
-            _complete_fish_debug "This file didn't add any completion entries, ignoring: $file"
-            return
-        end
-
-        if set --query --global _complete_fish_files
-            set --append _complete_fish_files $file
-        else
-            set --global _complete_fish_files $file
-        end
-
-        set -l added_entries_string "$(string join --no-empty \n $added_entries)"
-        # The autocomplete entries for `ruff` exceeded the max length of an
-        # environment variable so a global variable should be used.
-        if set --query --global _complete_fish_entries
-            set --append _complete_fish_entries $added_entries_string
-        else
-            set --global _complete_fish_entries $added_entries_string
-        end
-    end
-end
-
-function _complete_fish_convert_xdg_to_path_variable
-    # TODO: On Linux, fish treats XDG_DATA_DIRS like a "path variable", meaning
-    # its value is split on ":". However, on macOS it's a single element. I
-    # think they should do the same thing on either platform.
-    set --path XDG_DATA_DIRS "$XDG_DATA_DIRS"
-end
-
 function _complete_fish_post_load
     _complete_fish_debug 'In post load'
 
@@ -160,6 +99,195 @@ function _complete_fish_pre_load
     end
 end
 
+function _complete_fish_pre_unload
+    # This isn't needed for completion sync, but I'm including it just to complete
+    # the illustration of how shell hooks could be implemented in direnv.
+    :
+end
+
+function _complete_fish_debug --argument-names message
+    if test -n "$COMPLETE_FISH_DEBUG"
+        echo "[completion-sync] $message" >&2
+    end
+end
+
+function _complete_fish_remove_managed_files_from_complete_path
+    set -l removed_paths
+    for addition_file in $_complete_fish_files
+        if set -l index (contains --index -- (path dirname $addition_file) $fish_complete_path)
+            set --append removed_paths $fish_complete_path[$index]
+            set --erase fish_complete_path[$index]
+        end
+    end
+    if test (count $removed_paths) -gt 0
+        _complete_fish_debug 'Paths removed from fish_complete_path:'\n"$(string join \n $removed_paths)"
+    end
+end
+
+function _complete_fish_add
+    for file in $argv
+        set -l command (path change-extension '' (path basename $file))
+
+        set -l old_entries (complete $command)
+        source $file
+        set -l new_entries (complete $command)
+        set -l added_entries
+        for new_entry in $new_entries
+            if not contains $new_entry $old_entries
+                set --append added_entries $new_entry
+            end
+        end
+        if test (count $added_entries) -eq 0
+            _complete_fish_debug "This file didn't add any completion entries, ignoring: $file"
+            return
+        end
+
+        if set --query --global _complete_fish_files
+            set --append _complete_fish_files $file
+        else
+            set --global _complete_fish_files $file
+        end
+
+        set -l added_entries_string "$(string join --no-empty \n $added_entries)"
+        # The autocomplete entries for `ruff` exceeded the max length of an
+        # environment variable so a global variable should be used.
+        if set --query --global _complete_fish_entries
+            set --append _complete_fish_entries $added_entries_string
+        else
+            set --global _complete_fish_entries $added_entries_string
+        end
+    end
+end
+
+function _complete_fish_convert_xdg_to_path_variable
+    # TODO: On Linux, fish treats XDG_DATA_DIRS like a "path variable", meaning
+    # its value is split on ":". However, on macOS it's a single element. I
+    # think they should do the same thing on either platform.
+    set --path XDG_DATA_DIRS "$XDG_DATA_DIRS"
+end
+
+# direnv hook wrapper
+# ------------------------------------------------------------------------------
+# This wrapper is responsible for calling the pre and post hooks. There are four
+# hooks:
+#   - pre_load: This is useful for recording what an environment variable contained
+#     before loading direnv.
+#   - pre_unload: You can use this to undo the shell-specific changes.
+#   - post_load: This is where you should apply the shell-specific changes.
+#   - post_unload: You can use this to undo the shell-specific changes.
+#
+# This next section will describe the actions that lead to each hook being run.
+#   Actions:
+#     - cd: Change directory
+#     - exec: Running `exec fish`
+#     - sub_shell Running `fish`
+#   Terminology:
+#     - direnv: A directory that itself, or one of its ancestors, contains a .envrc.
+#     - non_direnv: A directory that itself, or one of its ancestors, does not
+#       contain a .envrc.
+#   When hooks are called:
+#     1. cd from non_direnv to allowed direnv: pre_load runs before direnv loads.
+#        post_load runs after direnv loads.
+#     2. cd from allowed direnv to non_direnv: pre_unload runs before direnv is
+#        unloaded. post_unload runs after direnv is unloaded.
+#     3. cd from allowed direnv1 to allowed direnv2: pre_unload runs before direnv1 is unloaded.
+#        post_unload runs after direnv1 is unloaded. pre_load runs before direnv2 is
+#        loaded. post_load runs after direnv2 is loaded.
+#        LIMITATIONS: pre_load should be called after direnv1 is unloaded, but it's
+#        called while direnv1 is still loaded. post_unload should be called before
+#        direnv2 is loaded, but it's called afterwards.
+#     4. exec while inside an allowed direnv: pre_load runs before direnv is loaded. post_load
+#        runs after direnv is loaded.
+#        LIMITATIONS: pre_load should be called before direnv is loaded, but since
+#        exec is used, the new process will inherit the environment variables set by
+#        direnv in the old process.
+#     5. sub_shell while inside an allowed direnv: pre_load runs before direnv is loaded.
+#        post_load runs after direnv is loaded.
+#        LIMITATIONS: pre_load should be called before direnv is loaded, but the
+#        child process will inherit the environment variables set by direnv in the
+#        parent process.
+#     6. One of the watched files changes in an allowed direnv and direnv reloads:
+#        pre_unload runs before the old direnv is unloaded. post_unload runs after
+#        the old direnv is unloaded. pre_load runs before the new direnv is loaded.
+#        post_load runs after the new direnv is loaded.
+#        LIMITATIONS: This case is currently not being handled.
+#     7. User runs `direnv block` within an allowed direnv: pre_unload runs before
+#        direnv is unloaded. post_unload runs after direnv is unloaded.
+#        LIMITATIONS: This case is currently not being handled.
+#     8. User runs `direnv allow` within a blocked direnv: pre_load runs before
+#        direnv is loaded. post_load runs after direnv is loaded.
+#        LIMITATIONS: This case is currently not being handled.
+#     9. cd from allowed direnv to blocked direnv: pre_unload runs before allowed
+#        direnv is unloaded. post_unload runs after allowed direnv is unloaded.
+#        LIMITATIONS: This currently has the same behavior as case #3, but it
+#        shouldn't.
+
+# Replace direnv's prompt hook with one that will call our pre and post hooks.
+#
+# TODO: For this to work correctly, this file must be loaded before direnv's config
+# runs. This way, the function below will run before `__direnv_export_eval` and we
+# can wrap it before it gets a chance to run. This works on my machine since this
+# file starts with a 'c' and direnv's config is in a file named 'direnv.fish'. I
+# should find a way to guarantee that this runs at the right time.
+function _complete_fish_register_direnv_hook_wrapper --on-event fish_prompt
+    functions --erase (status current-function)
+
+    functions --copy __direnv_export_eval __direnv_export_eval_backup
+    function __direnv_export_eval --on-event fish_prompt
+        set -l is_moving_from_one_direnv_directly_to_another \
+            (_complete_fish_is_moving_from_one_direnv_directly_to_another)
+
+        # Determine which pre hooks should run
+        set -l nearest_envrc "$(_complete_fish_nearest_envrc)"
+        if test -n "$nearest_envrc"
+            if not set --query DIRENV_DIR
+                # We get here in case #1
+                _complete_fish_pre_load
+            else
+                if not set --query --global _complete_fish_direnv_loaded
+                    # We get here in cases #4 and #5. #4 and #5 get here because exec
+                    # or sub_shell would erase global variables, but not environment
+                    # variables.
+                    _complete_fish_pre_load
+                else if test $is_moving_from_one_direnv_directly_to_another = true
+                    # We get here in case #3
+                    _complete_fish_pre_load
+                    _complete_fish_pre_unload
+                end
+            end
+        else
+            if set --query DIRENV_DIR
+                # We get here in case #2
+                _complete_fish_pre_unload
+            end
+        end
+
+        __direnv_export_eval_backup
+
+        # Determine which post hooks should run
+        if not set --query --global _complete_fish_direnv_loaded
+            and set --query DIRENV_DIR
+
+            # We get here in cases #1, #4, and #5. #4 and #5 get here because exec or
+            # sub_shell would erase global variables, but not environment variables.
+            set --global _complete_fish_direnv_loaded true
+            _complete_fish_post_load
+        else if set --query --global _complete_fish_direnv_loaded
+            and not set --query DIRENV_DIR
+
+            # We get here in case #2
+            set --erase _complete_fish_direnv_loaded
+            _complete_fish_post_unload
+        else if test $is_moving_from_one_direnv_directly_to_another = true
+            # We get here in case #3
+            _complete_fish_post_unload
+            _complete_fish_post_load
+        end
+    end
+
+    __direnv_export_eval
+end
+
 function _complete_fish_nearest_envrc
     set -l current_directory (pwd)
     while true
@@ -177,7 +305,7 @@ function _complete_fish_nearest_envrc
     end
 end
 
-function _complete_fish_is_moving_directly_to_new_direnv
+function _complete_fish_is_moving_from_one_direnv_directly_to_another
     if not set --query DIRENV_DIR
         echo false
         return
@@ -197,98 +325,4 @@ function _complete_fish_is_moving_directly_to_new_direnv
     end
 
     echo false
-end
-
-# direnv hook wrapper
-# ------------------------------------------------------------------------------
-# This wrapper is responsible for calling the pre and post hooks. There are four
-# hooks:
-#   - pre_load: This is useful for recording what an environment variable contained
-#     before loading direnv.
-#   - pre_unload (TODO): You can use this to undo the shell-specific changes.
-#   - post_load: This is where you should apply the shell-specific changes.
-#   - post_unload: You can use this to undo the shell-specific changes.
-#
-# This next section will describe the actions that lead to each hook being run.
-#   Actions:
-#     - cd: Change directory
-#     - exec: Running `exec fish`
-#     - sub_shell Running `fish`
-#   Terminology:
-#     - direnv: A directory that itself, or one of its ancestors, contains a .envrc.
-#     - non_direnv: A directory that itself, or one of its ancestors, does not
-#       contain a .envrc.
-#   When hooks are called:
-#     - cd from non_direnv to direnv: pre_load runs before direnv loads. post_load
-#       runs after direnv loads.
-#     - cd from direnv to non_direnv: pre_unload runs before direnv is unloaded.
-#       post_unload runs after direnv is unloaded.
-#     - cd from direnv1 to direnv2: pre_unload runs before direnv1 is unloaded.
-#       post_unload runs after direnv1 is unloaded. pre_load runs before direnv2 is
-#       loaded. post_load runs after direnv2 is loaded.
-#     - exec while inside a direnv: pre_load runs before direnv is loaded. post_load
-#       runs after direnv is loaded.
-#     - sub_shell while inside a direnv: pre_load runs before direnv is loaded.
-#       post_load runs after direnv is loaded.
-
-# Replace direnv's prompt hook with one that will call our pre and post hooks.
-#
-# TODO: For this to work correctly, this file must be loaded before direnv's config
-# runs. This way, the function below will run before `__direnv_export_eval` and we
-# can wrap it before it gets a chance to run. This works on my machine since this
-# file starts with a 'c' and direnv's config is in a file named 'direnv.fish'. I
-# should find a way to guarantee that this runs at the right time.
-function _complete_fish_register_direnv_hook_wrapper --on-event fish_prompt
-    functions --erase (status current-function)
-
-    functions --copy __direnv_export_eval __direnv_export_eval_backup
-    function __direnv_export_eval --on-event fish_prompt
-        set -l is_moving_directly_to_new_direnv (_complete_fish_is_moving_directly_to_new_direnv)
-
-        # Determine which pre hooks should run
-        set -l nearest_envrc "$(_complete_fish_nearest_envrc)"
-        if test -n "$nearest_envrc"
-            if not set --query DIRENV_DIR
-                _complete_fish_pre_load
-            else
-                if not set --query --global _complete_fish_direnv_loaded
-                    # If DIRENV_DIR is set, but _complete_fish_direnv_loaded
-                    # isn't, then the user either called `exec fish` or started
-                    # a sub shell. Doing either of those would erase global
-                    # variables, like _complete_fish_direnv_loaded, but not
-                    # DIRENV_DIR.
-                    _complete_fish_pre_load
-                else if test $is_moving_directly_to_new_direnv = true
-                    # TODO: I want to call this after the old direnv is unloaded and
-                    # before the new direnv is loaded, but direnv does them both in its
-                    # single hook.
-                    _complete_fish_pre_load
-                end
-            end
-        end
-
-        __direnv_export_eval_backup
-
-        # Determine which post hooks should run
-        if not set --query --global _complete_fish_direnv_loaded
-            and set --query DIRENV_DIR
-
-            set --global _complete_fish_direnv_loaded true
-            _complete_fish_post_load
-        else if set --query --global _complete_fish_direnv_loaded
-            and not set --query DIRENV_DIR
-
-            set --erase _complete_fish_direnv_loaded
-            _complete_fish_post_unload
-        else if test $is_moving_directly_to_new_direnv = true
-            # TODO: I want to call this after the old direnv is unloaded and
-            # before the new direnv is loaded, but direnv does them both in its
-            # single hook.
-            _complete_fish_post_unload
-
-            _complete_fish_post_load
-        end
-    end
-
-    __direnv_export_eval
 end
