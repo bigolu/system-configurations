8e3ebe13449b189604b2b43a5828fc63098959d4
Biggie <code@bigo.lu>
2025-02-08 09:21:47 -0500

    fish: Tweak prompt

--- dotfiles/fish/conf.d/prompt.fish
+++ dotfiles/fish/conf.d/prompt.fish
@@ -34,7 +34,8 @@ function fish_prompt --description 'Print the prompt'
     # transient prompt
     if set --query TRANSIENT
         set --erase TRANSIENT
-        printf $separator\n(set_color --reverse brblack)' '(path basename (prompt_pwd))'  '(date +"%T")' '(set_color normal; set_color brblack)''$_color_normal' '
+        set -l spacer '  '
+        printf $separator\n(set_color --reverse brblack)' '(path basename (prompt_pwd))$spacer(date +"%T")$spacer(set_color normal; set_color --reverse brblack)''$_color_normal
         return
     else if set --query TRANSIENT_EMPTY
         set --erase TRANSIENT_EMPTY
