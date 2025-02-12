48f87eaec25f9e917793d66511e1dab37da7a28e
Biggie <code@bigo.lu>
2025-02-12 16:29:39 -0500

    Add missing dependencies

--- lefthook/scripts/sync-direnv.bash
+++ lefthook/scripts/sync-direnv.bash
@@ -1,7 +1,7 @@
 #! /usr/bin/env cached-nix-shell
 #! nix-shell --keep FLAKE_PACKAGE_SET_FILE
 #! nix-shell -i nix-shell-interpreter
-#! nix-shell --packages "with (import (builtins.getEnv \"FLAKE_PACKAGE_SET_FILE\")); [nix-shell-interpreter coreutils fd nvd]"
+#! nix-shell --packages "with (import (builtins.getEnv \"FLAKE_PACKAGE_SET_FILE\")); [nix-shell-interpreter coreutils fd nvd nix-output-monitor]"
 
 # This script reloads the direnv environment. Since I've disabled nix-direnv's auto
 # reload, I'll reload it here as well.
--- mise/tasks/nix-darwin.bash
+++ mise/tasks/nix-darwin.bash
@@ -1,7 +1,7 @@
 #! /usr/bin/env cached-nix-shell
 #! nix-shell --keep FLAKE_PACKAGE_SET_FILE
 #! nix-shell -i nix-shell-interpreter
-#! nix-shell --packages "with (import (builtins.getEnv \"FLAKE_PACKAGE_SET_FILE\")); [nix-shell-interpreter curl coreutils darwin-rebuild]"
+#! nix-shell --packages "with (import (builtins.getEnv \"FLAKE_PACKAGE_SET_FILE\")); [nix-shell-interpreter curl darwin-rebuild nix-output-monitor]"
 #MISE hide=true
 #USAGE arg "<configuration>"
 
