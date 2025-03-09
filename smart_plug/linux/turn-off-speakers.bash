8822cc3539b0e2a13ef91ffdcc6652892606b00e
Biggie <code@bigo.lu>
2025-03-09 19:00:47 -0400

    Comment

--- smart_plug/linux/turn-off-speakers.bash
+++ smart_plug/linux/turn-off-speakers.bash
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-# Only stop the speakers if the system is about to shut down or sleep.
+# Stop the speakers if the system is about to shut down or sleep.
+#
+# Apparently, NetworkManager's shutdown is not handled by systemd when the
+# system sleeps[1]. Instead, it shuts itself down after receiving a dbus
+# signal from logind. To work around this, I register this script to run before
+# NetworkManager turns off any network interface.
+#
+# [1]: https://unix.stackexchange.com/a/687849
 if
   [[ $(systemctl is-system-running || true) == 'stopping' ]] ||
     grep -q 'suspend' <<<"$(journalctl --since '10 seconds ago' || true)"
