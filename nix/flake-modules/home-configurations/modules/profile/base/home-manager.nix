a51366f4c2a38e68c4b72adcbb07d9c5cdc22fd5
Biggie <code@bigo.lu>
2025-03-14 15:57:40 -0400

    Refactor

--- nix/flake-modules/home-configurations/modules/profile/base/home-manager.nix
+++ nix/flake-modules/home-configurations/modules/profile/base/home-manager.nix
@@ -249,6 +249,35 @@ let
   };
 in
 mkMerge [
+  {
+    # The `man` in nixpkgs is only intended to be used for NixOS, it doesn't work
+    # properly on other OS's so I'm disabling it.
+    #
+    # home-manager issue: https://github.com/nix-community/home-manager/issues/432
+    programs.man.enable = false;
+
+    home = {
+      # Since I'm not using the nixpkgs man, I have any packages I install their man
+      # outputs so my system's `man` can find them.
+      extraOutputsToInstall = [ "man" ];
+
+      # This value determines the Home Manager release that your
+      # configuration is compatible with. This helps avoid breakage
+      # when a new Home Manager release introduces backwards
+      # incompatible changes.
+      #
+      # You can update Home Manager without changing this value. See
+      # the Home Manager release notes for a list of state version
+      # changes in each release.
+      stateVersion = "23.11";
+
+      packages = [
+        system-config-apply
+        system-config-pull
+      ];
+    };
+  }
+
   # These are all things that don't need to be done when home manager is being run as
   # a submodule inside of another system manager, like nix-darwin. They don't need to
   # be done because the outer system manager will do them.
@@ -282,35 +311,6 @@ mkMerge [
     # [1]: https://github.com/nix-community/home-manager/issues/2033#issuecomment-1698406098
     news.display = "silent";
   })
-
-  {
-    # The `man` in nixpkgs is only intended to be used for NixOS, it doesn't work
-    # properly on other OS's so I'm disabling it.
-    #
-    # home-manager issue: https://github.com/nix-community/home-manager/issues/432
-    programs.man.enable = false;
-
-    home = {
-      # Since I'm not using the nixpkgs man, I have any packages I install their man
-      # outputs so my system's `man` can find them.
-      extraOutputsToInstall = [ "man" ];
-
-      # This value determines the Home Manager release that your
-      # configuration is compatible with. This helps avoid breakage
-      # when a new Home Manager release introduces backwards
-      # incompatible changes.
-      #
-      # You can update Home Manager without changing this value. See
-      # the Home Manager release notes for a list of state version
-      # changes in each release.
-      stateVersion = "23.11";
-
-      packages = [
-        system-config-apply
-        system-config-pull
-      ];
-    };
-  }
 
   (mkIf isLinux {
     systemd.user = {
