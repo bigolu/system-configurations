aeeb6b450d793ad86cd746229d6f340a0dd7dcc7
Biggie <code@bigo.lu>
2025-04-06 09:20:09 -0400

    default-shells: Refactor interactive config + Check for `ZED_TERM`

--- dotfiles/default-shells/bashrc.bash
+++ dotfiles/default-shells/bashrc.bash
@@ -1,24 +1,23 @@
-# I only want this file (.bashrc) to be loaded when Bash is in interactive
+# I only want this file to be loaded when Bash is in interactive
 # mode. There are two problems that I encountered when trying to do this. Below,
 # I'll list the problems and my workarounds.
-#   - .bashrc does not get loaded when the shell is in interactive _login_
-#     mode: To workaround this, I added code to .bash_profile to source this file if
+#   1. .bashrc does not get loaded when the shell is in interactive login
+#     mode: To work around this, I added code to .bash_profile to source this file if
 #     the shell is in interactive mode. This covers the interactive login mode case
 #     since .bash_profile is always loaded when shell is started in login mode.
-#   - There are cases where .bashrc gets sourced even if the shell isn't in
-#     interactive mode: To workaround this, I have conditionals below that will
-#     return early if this shell isn't actually in interactive mode.
+#   2. There are cases where .bashrc gets sourced even if the shell isn't in
+#     interactive mode: To work around this, the conditional below will
+#     exit this script early if the shell wasn't launched with the `-i` flag. The
+#     cases are:
+#       - Bash will source .bashrc whenever Bash is run by a remote shell daemon like
+#         ssh[1].
+#       - The default .profile on Pop!_OS sources .bashrc even if the shell isn't
+#         interactive. I'm assuming this is done because Bash won't source .bashrc if
+#         the shell is both interactive and login[2]. Even if that's the case, I
+#         think a check for interactive mode should be added.
 #
 # The rules for how Bash decides what file to load can be found here:
 # https://www.gnu.org/software/bash/manual/html_node/Bash-Startup-Files.html
-
-# Sometimes, .bashrc is sourced even if the shell isn't in interactive mode:
-#   - bash will source .bashrc whenever bash is run by a remote shell daemon like
-#     ssh[1].
-#   - The default .profile on Pop!_OS sources .bashrc even if the shell isn't
-#     interactive. I'm assuming this is done because bash won't source .bashrc if
-#     the shell is both interactive and login[2]. Even if that's the case, I
-#     think a check for interactive mode should be added.
 #
 # [1]: https://unix.stackexchange.com/questions/257571/why-does-bashrc-check-whether-the-current-shell-is-interactive
 # [2]: https://stackoverflow.com/a/415444
@@ -26,27 +25,26 @@ if [[ $- != *i* ]]; then
   return
 fi
 
-# As part of VS Code's "shell resolution"[1], it starts the default shell in
-# interactive-login mode. Though they shouldn't be launching it in interactive
-# mode if it isn't actually being used interactively. Apparently they do this
-# because they think most users would want the configuration in their .bashrc to
-# be applied to VS Code[2].
+# There are a few cases where Bash get launched interactively even though it won't be
+# used interactively. This is a problem because I call `exec` in this script which
+# usually breaks the program launching Bash. To work around this, I exit this script
+# early if I detect any of these cases. The cases are:
 #
-# `exec`ing into fish breaks their shell resolution so I first check to see if
-# the shell was started for shell resolution using the environment variable that
-# VS Code sets to indicate this[3].
+# - VS Code's "shell resolution"[1], starts the default shell in interactive-login
+# mode. Though they shouldn't be launching it in interactive mode if it isn't
+# actually being used interactively. Apparently they do this because they think most
+# users would want the configuration in their .bashrc to be applied to VS Code[2].
+# `exec`ing into fish breaks their shell resolution so I first check to see if the
+# shell was started for shell resolution using the environment variable that VS Code
+# sets to indicate this[3].
+#
+# - Zed will launch the shell in interactive mode to resolve the environment. It sets
+# `ZED_TERM` when resolving the environment so I check for that.
 #
 # [1]: https://code.visualstudio.com/docs/supporting/FAQ#_resolving-shell-environment-fails
 # [2]: https://github.com/microsoft/vscode/issues/177126#issuecomment-1630889619
 # [3]: https://github.com/microsoft/vscode/issues/163186
-if [[ -n ${VSCODE_RESOLVING_ENVIRONMENT+set} ]]; then
-  return
-fi
-
-# Zed will launch the shell in interactive mode to resolve the environment. It fails
-# when I call `exec fish` further down. I'm not sure of a way to detect when Zed is
-# doing this so I'll just check if stdin isn't connected to a tty.
-if [[ ! -t 1 ]]; then
+if [[ -n ${VSCODE_RESOLVING_ENVIRONMENT+set} ]] || [[ -n ${ZED_TERM+set} ]]; then
   return
 fi
 
--- dotfiles/default-shells/zshrc.zsh
+++ dotfiles/default-shells/zshrc.zsh
@@ -1,30 +1,38 @@
-# As part of VS Code's "shell resolution"[1], it starts the default shell in
-# interactive-login mode. Though they shouldn't be launching it in interactive
-# mode if it isn't actually being used interactively. Apparently they do this
-# because they think most users would want the configuration in their .bashrc to
-# be applied to VS Code[2].
+# There are a few cases where Zsh get launched interactively even though it won't be
+# used interactively. This is a problem because I call `exec` in this script which
+# usually breaks the program launching Zsh. To work around this, I exit this script
+# early if I detect any of these cases. The cases are:
 #
-# `exec`ing into fish breaks their shell resolution so I first check to see if
-# the shell was started for shell resolution using the environment variable that
-# VS Code sets to indicate this[3].
+# - VS Code's "shell resolution"[1], starts the default shell in interactive-login
+# mode. Though they shouldn't be launching it in interactive mode if it isn't
+# actually being used interactively. Apparently they do this because they think most
+# users would want the configuration in their .bashrc to be applied to VS Code[2].
+# `exec`ing into fish breaks their shell resolution so I first check to see if the
+# shell was started for shell resolution using the environment variable that VS Code
+# sets to indicate this[3].
 #
-# When trying to load the environment for one of my hammerspoon spoons, I launch
+# - When trying to load the environment for one of my hammerspoon spoons, I launch
 # the shell in interactive mode, even though it isn't actually being used
-# interactively. Like VS Code, I set an environment variable so I can detect
-# when I'm doing this. My reason for using interactive mode can be found in
+# interactively. Like VS Code, I set an environment variable so I can detect when I'm
+# doing this. My reason for using interactive mode can be found in
 # Speakers.spoon/init.lua.
 #
-# Zed will launch the shell in interactive mode to resolve the environment. It fails
-# when I call `exec fish` further down. I'm not sure of a way to detect when Zed is
-# doing this so I'll just check if stdin is connected to a tty.
+# - Zed will launch the shell in interactive mode to resolve the environment. It sets
+# `ZED_TERM` when resolving the environment so I check for that.
 #
 # [1]: https://code.visualstudio.com/docs/supporting/FAQ#_resolving-shell-environment-fails
 # [2]: https://github.com/microsoft/vscode/issues/177126#issuecomment-1630889619
 # [3]: https://github.com/microsoft/vscode/issues/163186
-if ! (( ${+VSCODE_RESOLVING_ENVIRONMENT} )) && ! (( ${+HAMMERSPOON_RESOLVING_ENVIRONMENT} )) && [ -t 1 ]; then
-  # If the current shell isn't fish, exec into fish. My reason for doing this is in
-  # README.md
-  if [ "$(basename "$SHELL")" != 'fish' ] && (( $+commands[fish] )); then
-    SHELL="$(command -v fish)" exec fish
-  fi
+if
+  (( ${+VSCODE_RESOLVING_ENVIRONMENT} )) \
+    || (( ${+HAMMERSPOON_RESOLVING_ENVIRONMENT} )) \
+    || (( ${+ZED_TERM} ))
+then
+  return
+fi
+
+# If the current shell isn't fish, exec into fish. My reason for doing this is in
+# README.md
+if [ "$(basename "$SHELL")" != 'fish' ] && (( $+commands[fish] )); then
+  SHELL="$(command -v fish)" exec fish
 fi
