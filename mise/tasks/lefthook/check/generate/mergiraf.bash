7ec43ededed2127f18489cf9c59414835a650782
Biggie <code@bigo.lu>
2025-04-13 10:51:10 -0400

    mise: Add script boilerplate

--- mise/tasks/lefthook/check/generate/mergiraf.bash
+++ mise/tasks/lefthook/check/generate/mergiraf.bash
@@ -4,4 +4,10 @@
 #! nix-shell --packages "with (import (builtins.getEnv \"FLAKE_PACKAGE_SET_FILE\")); [nix-shell-interpreter mergiraf]"
 #MISE hide=true
 
+set -o errexit
+set -o nounset
+set -o pipefail
+shopt -s nullglob
+shopt -s inherit_errexit
+
 mergiraf languages --gitattributes >dotfiles/git/attributes
