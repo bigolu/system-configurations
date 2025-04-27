3090f75fd61b782bc6e3f13bf6ee74fb83fac0b7
Biggie <code@bigo.lu>
2025-04-27 08:27:44 -0400

    Update link

--- nix/flake/modules/home-configurations/modules/base/terminal/default.nix
+++ nix/flake/modules/home-configurations/modules/base/terminal/default.nix
@@ -87,7 +87,7 @@ in
     # The programs store files according to the XDG Base Directory spec[1] on Linux
     # and the Standard Directories guidelines[2] on macOS:
     #
-    # [1]: https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+    # [1]: https://specifications.freedesktop.org/basedir-spec/latest/
     # [2]: https://developer.apple.com/library/archive/documentation/FileManagement/Conceptual/FileSystemProgrammingGuide/FileSystemOverview/FileSystemOverview.html#//apple_ref/doc/uid/TP40010672-CH2-SW6
     home.file = {
       "${if isLinux then ".config" else "Library/Application Support"}/tealdeer/config.toml".source =
