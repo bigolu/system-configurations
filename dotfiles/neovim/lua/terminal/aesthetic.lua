6a135ed69a08dcd6cebab370e060c92cf7887865
Biggie <code@bigo.lu>
2025-04-27 12:38:06 -0400

    neovim: Cursor tweak

--- dotfiles/neovim/lua/terminal/aesthetic.lua
+++ dotfiles/neovim/lua/terminal/aesthetic.lua
@@ -17,15 +17,15 @@ vim.api.nvim_create_autocmd({ "OptionSet" }, {
 -- cursor
 -- I don't think I'll need to reset/restore the cursor once this issue is resolved:
 -- https://github.com/neovim/neovim/issues/4396
+local blink = "blinkwait0-blinkon150-blinkoff150"
+local bar = "ver25"
 local function set_cursor()
   -- Block cursor in normal mode, thin line in insert mode, and underline in replace
   -- mode
-  vim.o.guicursor =
-    "n-v:block-blinkon0,i-c-ci-ve:ver25-blinkwait0-blinkon200-blinkoff200,r-cr-o:hor20-blinkwait0-blinkon200-blinkoff200"
+  vim.o.guicursor = string.format("n-v:block-%s,i-c-ci-ve:%s-%s,r-cr-o:hor20-%s", blink, bar, blink, blink)
 end
 local function reset_cursor()
-  -- Reset terminal cursor to blinking bar.
-  vim.o.guicursor = "a:ver25-blinkwait0-blinkon200-blinkoff200"
+  vim.o.guicursor = string.format("a:%s-%s", bar, blink)
 end
 set_cursor()
 vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
