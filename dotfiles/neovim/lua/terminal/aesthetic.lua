f645b5ad76659efa84b8a2ea943f6dfb8d510c75
Biggie <code@bigo.lu>
2025-05-16 16:05:42 -0400

    Tweak

--- dotfiles/neovim/lua/terminal/aesthetic.lua
+++ dotfiles/neovim/lua/terminal/aesthetic.lua
@@ -3,7 +3,7 @@ vim.o.breakat = " ^I"
 vim.o.cursorline = true
 vim.o.cursorlineopt = "number"
 vim.o.wrap = true
-vim.o.listchars = "tab:¬-,space:·"
+vim.o.listchars = "tab: ,space:·"
 vim.opt.fillchars:append("eob: ,lastline:>")
 
 vim.cmd.colorscheme("bigolu")
