ca57bd7b92034675999293a6b337bb9172f370a0
Biggie <code@bigo.lu>
2025-05-18 22:11:43 -0400

    ipython: Remove theme

--- dotfiles/python/ipython/ipython_config.py
+++ dotfiles/python/ipython/ipython_config.py
@@ -7,4 +7,3 @@ c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.shortcuts = [
     {"command": "IPython:shortcuts.open_input_in_editor", "new_keys": ["c-e"]}
 ]
-c.TerminalInteractiveShell.highlighting_style = "bw"
