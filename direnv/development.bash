98c18fd4e66b72210846e020fda9ba76ac62ea17
Biggie <code@bigo.lu>
2025-06-06 23:01:58 -0400

    direnv: Comment

--- direnv/development.bash
+++ direnv/development.bash
@@ -1,6 +1,8 @@
 # For development, create the file <project_root>/.envrc and add the following
 # line:
 #   source direnv/development.bash
+# Or just run the following command in Bash, or a shell with similar syntax:
+#   echo 'source direnv/development.bash' >../.envrc
 #
 # .envrc shouldn't be committed to version control so users can make changes to it
 # without accidentally committing them. Here are some examples of changes they could
