77a0c7ec81ae4536861bdba95072f132f63e3507
Biggie <code@bigo.lu>
2025-06-09 15:24:26 -0400

    lefthook: Support user-specified auto-sync skips

---
+++ mise/tasks/lefthook/auto-sync-user-skip.bash
@@ -0,0 +1,36 @@
+#! Though we don't use shebangs, cached-nix-shell expects the first line to be one so we put this on the first line instead.
+#! nix-shell --keep NIX_PACKAGES
+#! nix-shell -i nix-shell-interpreter
+#! nix-shell --packages "with (import (builtins.getEnv \"NIX_PACKAGES\")); [nix-shell-interpreter gitMinimal]"
+#MISE hide=true
+
+set -o errexit
+set -o nounset
+set -o pipefail
+shopt -s nullglob
+shopt -s inherit_errexit
+
+hook_name="$1"
+
+current_branch="$(git rev-parse --abbrev-ref HEAD)"
+git config --get-all "auto-sync.$hook_name.branch" |
+  {
+    readarray -t branches
+    for branch in "${branches[@]}"; do
+      if [[ $branch == "$current_branch" ]]; then
+        exit 0
+      fi
+    done
+  }
+
+git config --get-all "auto-sync.$hook_name.shell" |
+  {
+    readarray -t shell_commands
+    for shell_command in "${shell_commands[@]}"; do
+      if eval "$shell_command"; then
+        exit 0
+      fi
+    done
+  }
+
+exit 1
