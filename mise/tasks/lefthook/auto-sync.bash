8baccce619161c61b2ea68f993ecd93dc4032353
Biggie <code@bigo.lu>
2025-06-12 16:17:25 -0400

    mise: Remove unused dependency

--- mise/tasks/lefthook/auto-sync.bash
+++ mise/tasks/lefthook/auto-sync.bash
@@ -1,7 +1,7 @@
 #! Though we don't use shebangs, cached-nix-shell expects the first line to be one so we put this on the first line instead.
 #! nix-shell --keep NIX_PACKAGES
 #! nix-shell -i nix-shell-interpreter
-#! nix-shell --packages "with (import (builtins.getEnv \"NIX_PACKAGES\")); [nix-shell-interpreter git coreutils gnused]"
+#! nix-shell --packages "with (import (builtins.getEnv \"NIX_PACKAGES\")); [nix-shell-interpreter git coreutils]"
 #MISE hide=true
 
 set -o errexit
