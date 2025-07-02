d81671b363e5a139c01d283ba3028e107d90e6ac
Biggie <code@bigo.lu>
2025-07-02 15:47:59 -0400

    nix: Refactor

--- nix/public-overlay/mk-shell-wrapper.nix
+++ nix/public-overlay/mk-shell-wrapper.nix
@@ -25,34 +25,54 @@ let
   uniqueShellHooksFunc =
     args@{
       inputsFrom ? [ ],
+      shellHook ? "",
       ...
     }:
     let
-      # This key will be added to the shell to keep track of the inputsFrom, in case
-      # we need to merge this shell with another shell.
-      uniqueInputsFromKey = "_inputsFrom";
-
-      uniqueInputsFrom = pipe inputsFrom [
-        (catAttrs uniqueInputsFromKey)
+      uniquePropagatedShells = pipe inputsFrom [
+        (catAttrs "propagatedShells")
         concatLists
         unique
       ];
 
-      joinedShellHooks = pipe uniqueInputsFrom [
+      joinedShellHooks = pipe uniquePropagatedShells [
         (catAttrs "shellHook")
-        (shellHooks: shellHooks ++ optionals (args ? "shellHook") [ args.shellHook ])
+        (shellHooks: shellHooks ++ [ shellHook ])
         (filter (hook: hook != ""))
         (concatStringsSep "\n")
       ];
 
+      uniquePropagatedShellsWithoutShellHooks = map (
+        shell: removeAttrs shell [ "shellHook" ]
+      ) uniquePropagatedShells;
+
       shellWithoutInputsFrom = mkShellNoCC (removeAttrs args [ "inputsFrom" ]);
-      uniqueInputsFromWithoutShellHooks = map (shell: removeAttrs shell [ "shellHook" ]) uniqueInputsFrom;
     in
     args
     // {
+      # We have to join the shellHooks ourselves since we store the individual
+      # shellHooks in propagatedShells.
       shellHook = joinedShellHooks;
-      inputsFrom = uniqueInputsFromWithoutShellHooks;
-      "passthru".${uniqueInputsFromKey} = uniqueInputsFrom ++ [ shellWithoutInputsFrom ];
+      # Since we've already joined the shellHooks, we'll remove them from inputsFrom
+      # so they don't get joined again.
+      inputsFrom = uniquePropagatedShellsWithoutShellHooks;
+      # We store all the shells included in inputsFrom, recursively, so we can keep
+      # track of the individual shellHooks. We need to do this since mkShell combines
+      # all the shellHooks from the shells in inputsFrom with the shellHook of the
+      # shell being created.
+      #
+      # Even though the goal is only to deduplicate shellHooks, we keep track of the
+      # entire shells so we can deduplicate the hooks based on the shell derivation
+      # they belong to and not their contents. This way, if two different shell
+      # derivations happen to have the same shellHook, the shellHook will still be
+      # included twice.
+      passthru.propagatedShells =
+        uniquePropagatedShells
+        # We include the shell being created so we can retain its original shellHook,
+        # before we joined it with the shellHooks from the propagatedShells in
+        # inputsFrom. We remove its inputsFrom since those shells are already
+        # included in their own propagatedShells, due to what we're doing here.
+        ++ [ shellWithoutInputsFrom ];
     };
 
   # Prevent nested nix shells from executing this shell's hook[1][2].
