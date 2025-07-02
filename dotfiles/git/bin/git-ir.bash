f8579a91251a26ad3c4fad8ab09ec0277be3b2a1
Biggie <code@bigo.lu>
2025-07-02 14:19:04 -0400

    git-ir: Comment

--- dotfiles/git/bin/git-ir.bash
+++ dotfiles/git/bin/git-ir.bash
@@ -15,11 +15,15 @@ check_command="$(
 
 start_commit=''
 if (($# == 0)); then
-  # i.e. rebase all the commits I haven't pushed yet
+  # Rebase all the commits I haven't pushed yet
   start_commit="$(git merge-base '@{push}' HEAD)"
 elif ((${#1} <= 2)); then
+  # I assume that if the argument is 2 characters or less, it's a number specifying
+  # how many commits from HEAD we want to rebase.
   start_commit="HEAD~$1"
 else
+  # The argument is a commit-ish specifying the first commit to be included in the
+  # rebase.
   start_commit="$1^"
 fi
 
