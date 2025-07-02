995699b5334c7438735a8b49d955b4e33cd43c00
Biggie <code@bigo.lu>
2025-07-02 14:08:59 -0400

    git-ir: Refactor

--- dotfiles/git/bin/git-ir.bash
+++ dotfiles/git/bin/git-ir.bash
@@ -8,21 +8,15 @@ shopt -s inherit_errexit
 
 # To set the check command run: git config bigolu.check-command 'the command'
 check_command="$(
-  # If a config option isn't set, git exits with a non-zero code so the `||`
-  # both stops the statement from failing and provides a default value.
-  git config --get 'bigolu.check-command' ||
-    echo 'false'
+  # If a config option isn't set, git exits with a non-zero code so the `echo` both
+  # stops the statement from failing and provides a default value.
+  git config --get 'bigolu.check-command' || echo 'false'
 )"
 
 start_commit=''
 if (($# == 0)); then
-  current_branch="$(git symbolic-ref -q HEAD)"
-  tracking_branch="$(git for-each-ref --format='%(upstream:short)' "$current_branch")"
-  if [[ -z $tracking_branch ]]; then
-    echo 'Error: No tracking branch' >&2
-    exit 1
-  fi
-  start_commit="$(git merge-base "$tracking_branch" HEAD)"
+  # i.e. rebase all the commits I haven't pushed yet
+  start_commit="$(git merge-base '@{push}' HEAD)"
 elif ((${#1} <= 2)); then
   start_commit="HEAD~$1"
 else
