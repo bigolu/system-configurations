cb367bca359315b689879003c731dfdd74a9940b
Biggie <code@bigo.lu>
2025-07-08 09:02:33 -0400

    direnv: Comment

--- direnv/config/ci.bash
+++ direnv/config/ci.bash
@@ -1,1 +1,5 @@
+# Environment Variables
+#   NIX_DEV_SHELL (optional):
+#     The name of the flake dev shell to load.
+
 NIX_DEV_SHELL="${NIX_DEV_SHELL:-ci-essentials}" source direnv/config/base.bash
--- direnv/config/development.bash
+++ direnv/config/development.bash
@@ -1,28 +1,5 @@
-# For development, create the file <project_root>/.envrc and add the following
-# line:
-#   source direnv/config/development.bash
-# Or just run the following command, from the root of the project, in Bash or a
-# shell with similar syntax:
-#   echo 'source direnv/config/development.bash' >.envrc
-#
-# .envrc shouldn't be committed to version control so users can make changes to it
-# without accidentally committing them. Here are some examples of changes they could
-# make:
-#   - Users may want to apply configuration specific to themselves. For example,
-#     authentication tokens or shell aliases.
-#   - Users can set environment variables that influence the behavior of the project
-#     without accidentally committing those changes. For example, build flags.
-#   - Users can add their configuration before the project's. This way, if there are
-#     any environment variables that affect the behavior of the project's config,
-#     users have a place to set them.
-#   - Users can add their configuration after the project's. This way, they can
-#     overwrite anything that doesn't work for them.
-#   - If the project doesn't already provide a way to get its dependencies, users can
-#     add configuration to get those dependencies using package managers like nix for
-#     example.
-#
-# More reasons for not committing .envrc can be found in this GitHub issue[1].
-#
-# [1]: https://github.com/NixOS/nixpkgs/pull/325793#issuecomment-2219538799
+# Environment Variables
+#   NIX_DEV_SHELL (optional):
+#     The name of the flake dev shell to load.
 
 NIX_DEV_SHELL="${NIX_DEV_SHELL:-development}" source direnv/config/base.bash
