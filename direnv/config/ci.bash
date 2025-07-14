15346321d2dd65df164ee923f811f56cad0a885d
Biggie <code@bigo.lu>
2025-07-13 23:52:24 -0400

    nix-direnv-wrapper: Make GC roots for npins

--- direnv/config/ci.bash
+++ direnv/config/ci.bash
@@ -2,4 +2,6 @@
 #   NIX_DEV_SHELL (optional):
 #     The name of the flake dev shell to load.
 
-NIX_DEV_SHELL="${NIX_DEV_SHELL:-ci-essentials}" source direnv/config/base.bash
+NIX_DEV_SHELL="${NIX_DEV_SHELL:-ci-essentials}" \
+  NIX_DIRENV_DISABLE_NPINS=true \
+  source direnv/config/base.bash
