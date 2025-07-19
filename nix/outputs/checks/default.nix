3bc079556fd11fd3fe22c9be2862c2b89237d5aa
Biggie <code@bigo.lu>
2025-07-19 12:58:28 -0400

    nix: Refactor

--- nix/outputs/checks/default.nix
+++ nix/outputs/checks/default.nix
@@ -8,57 +8,26 @@ let
   inherit (builtins) getAttr;
   inherit (lib)
     mapAttrs
-    mapAttrs'
-    nameValuePair
-    pipe
     mergeAttrsList
     getExe
     ;
 
-  prefixAttrNames = prefix: mapAttrs' (name: nameValuePair "${prefix}-${name}");
-
-  darwinChecks = pipe outputs.darwinConfigurations [
-    (mapAttrs (_name: getAttr "system"))
-    (prefixAttrNames "darwin")
-  ];
-
-  devShellChecks = pipe outputs.devShells [
-    (prefixAttrNames "dev-shell")
-  ];
-
-  homeChecks = pipe outputs.homeConfigurations [
-    (mapAttrs (_name: getAttr "activationPackage"))
-    (prefixAttrNames "home")
-  ];
-
-  packageChecks = pipe outputs.packages [
-    (prefixAttrNames "package")
-  ];
-
-  bundlerChecks =
+  rootlessBundlerCheck =
     let
-      bundlerPrefix = "bundler";
-
       rootlessBundlerName = "rootless";
       rootlessBundler = outputs.bundlers.${rootlessBundlerName};
-      # TODO: To ensure the bundle isn't accessing the nix store I should use
-      # something like chroot.
-      rootlessBundlerCheck = nixpkgs.runCommand "check-${bundlerPrefix}-${rootlessBundlerName}" { } ''
-        [[ $(${rootlessBundler nixpkgs.hello}) == $(${getExe nixpkgs.hello}) ]]
-
-        # Nix only considers the command to be successful if something is written
-        # to $out.
-        echo success > $out
-      '';
     in
-    prefixAttrNames bundlerPrefix {
-      ${rootlessBundlerName} = rootlessBundlerCheck;
-    };
+    nixpkgs.runCommand "check-bundler-${rootlessBundlerName}" { } ''
+      [[ $(${rootlessBundler nixpkgs.hello}) == $(${getExe nixpkgs.hello}) ]]
+
+      # Nix only considers the command to be successful if something is written
+      # to $out.
+      echo success > $out
+    '';
 in
 mergeAttrsList [
-  darwinChecks
-  devShellChecks
-  homeChecks
-  packageChecks
-  bundlerChecks
+  { inherit (outputs) packages devShells; }
+  { homeConfigurations = mapAttrs (_name: getAttr "activationPackage") outputs.homeConfigurations; }
+  { darwinConfigurations = mapAttrs (_name: getAttr "system") outputs.darwinConfigurations; }
+  { bundlers.rootless = rootlessBundlerCheck; }
 ]
