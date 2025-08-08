5071a70b99cc28491ba4a06c605071c06a903ef7
Biggie <code@bigo.lu>
2025-08-08 13:31:42 -0400

    Tweak

--- dotfiles/neovim/lua/base/motions-and-textobjects.lua
+++ dotfiles/neovim/lua/base/motions-and-textobjects.lua
@@ -72,12 +72,10 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- andymass/vim-matchup
---
--- Don't display off-screen matches in my statusline or a popup window
 vim.g.matchup_matchparen_offscreen = {}
--- These two highlight the surroundings that the cursor is currently contained within
-vim.g.matchup_matchparen_deferred = 1
-vim.g.matchup_matchparen_hi_surround_always = 1
+vim.g.matchup_treesitter_disable_virtual_text = true
+vim.g.matchup_matchparen_deferred = true
+vim.g.matchup_matchparen_hi_surround_always = true
 vim.keymap.set({ "n", "x" }, ";", "%", { remap = true })
 vim.keymap.set({ "n", "x" }, "g;", "g%", { remap = true })
 vim.keymap.set({ "n", "x" }, "];", "]%", { remap = true })
