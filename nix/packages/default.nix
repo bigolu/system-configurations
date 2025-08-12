0434295c8f2969c04afab9041e48562943605b21
Biggie <code@bigo.lu>
2025-08-12 13:44:36 -0400

    Revert "Remove mkShellWrapper"
    This reverts commit 3cbeab3d40cfdfbbe01309ddd4233ca75edab249.

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -84,6 +84,8 @@ recursiveUpdateList [
       interpreter = bash-script;
     };
 
+    mkShellNoCC = outputs.packages.mkShellWrapper;
+
     resolveNixShellShebang = outputs.packages.resolveNixShellShebang.override { inherit pkgs; };
 
     neovim =
