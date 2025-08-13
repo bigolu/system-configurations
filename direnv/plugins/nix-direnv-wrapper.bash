c7f628a74acd528f5a60530c48a4b61e5e1639d3
Biggie <code@bigo.lu>
2025-08-13 14:40:37 -0400

    nix-direnv-wrapper: Remove nix config loader

--- direnv/plugins/nix-direnv-wrapper.bash
+++ direnv/plugins/nix-direnv-wrapper.bash
@@ -1,5 +1,4 @@
 # A wrapper for nix-direnv that provides the following additional features:
-#   - Automatically load a nix config file
 #   - Show a diff of the dev shell when it changes
 
 # renovate: nix-direnv
@@ -36,8 +35,6 @@ function _ndw_backup {
 function _ndw_wrapper {
   local -r original_function_name="$1"
   local -ra args=("${@:2}")
-
-  _ndw_load_nix_config_file
 
   local profile_prefix
   if [[ $original_function_name == 'use_nix' ]]; then
@@ -81,44 +78,6 @@ function _ndw_get_dev_shell_store_path {
       realpath "$profile"
     fi
   fi
-}
-
-# TODO: This wouldn't be necessary if nix supported project/directory-specific config
-# files[1]. The `nixConfig` field on a flake isn't a good alternative because it only
-# supports flakes so it doesn't apply to nix shebang scripts for example.
-#
-# https://github.com/NixOS/nix/issues/10258
-function _ndw_load_nix_config_file {
-  local config_file
-  config_file="$(_ndw_find_nix_config_file)"
-  if [[ -n $config_file ]]; then
-    _ndw_add_line_to_nix_config "include $config_file"
-  fi
-}
-
-function _ndw_find_nix_config_file {
-  if [[ -n ${NIX_DIRENV_NIX_CONF:-} ]]; then
-    echo "$NIX_DIRENV_NIX_CONF"
-    return 0
-  fi
-
-  local absolute_path
-  if absolute_path="$(find_up 'nix/nix.conf')"; then
-    echo "$absolute_path"
-  fi
-}
-
-function _ndw_add_line_to_nix_config {
-  local -r line="$1"
-
-  local new_config
-  if [[ -z ${NIX_CONFIG:-} ]]; then
-    new_config="$line"
-  else
-    new_config="$NIX_CONFIG"$'\n'"$line"
-  fi
-
-  export NIX_CONFIG="$new_config"
 }
 
 _ndw_create_wrappers
