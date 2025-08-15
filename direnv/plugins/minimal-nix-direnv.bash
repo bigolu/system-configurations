6d78fb8e0fed83d77d450e5f2ce15ac1dd522916
Biggie <code@bigo.lu>
2025-08-14 23:20:23 -0400

    Comment

--- direnv/plugins/minimal-nix-direnv.bash
+++ direnv/plugins/minimal-nix-direnv.bash
@@ -1,15 +1,15 @@
+# This plugin loads a nix environment, caches it, and invalidates the cache whenever
+# a watched file is modified. If you want to invalidate manually, you can use my
+# direnv-manual-reload plugin.
+#
 # Differences from nix-direnv:
 #   - No GC root creation: The various nix dev shell implementations already provide
 #     a way to set up the environment e.g. `shellHook` for nix's devShell or
-#     `startup.*` for numtide's devshell. Therefore, nix should handle all of its
-#     environment management so it can be less coupled with direnv. To help with
-#     this, I wrote a nix utility that handles GC roots so I wouldn't need direnv to
-#     do that.
+#     `startup.*` for numtide's devshell. Therefore, nix should be able to handle all
+#     of its environment management so it can be less dependent on direnv. To help
+#     with this, I wrote a nix utility that handles GC roots.
 #   - Fast: This intentionally doesn't offer much configuration or features to make
 #     it as fast as possible.
-#
-# Essentially this plugin just loads the environment and reloads it whenever a
-# watched file is modified.
 
 # This is numtide's devshell, not nix's devShell
 function use_devshell {
