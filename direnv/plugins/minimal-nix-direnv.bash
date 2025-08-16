60bd6c7a91b2289dac1cfe9f68fa9d95a2f2d264
Biggie <code@bigo.lu>
2025-08-15 20:50:38 -0400

    minimal-nix-direnv: Use a trap to recover from errors

--- direnv/plugins/minimal-nix-direnv.bash
+++ direnv/plugins/minimal-nix-direnv.bash
@@ -1,31 +1,21 @@
 # This plugin caches and loads a nix environment. The cache is invalidated whenever a
-# watched file is modified.
+# watched file is modified. If it fails to load a new environment, it'll fall back to
+# the last cached one.
 #
 # Differences from nix-direnv:
 #   - Much less features. Some notable ones are:
-#     - No GC root creation: The various nix dev shell implementations already
-#       provide a way to set up the environment e.g. `shellHook` for nix's devShell
-#       or `startup.*` for numtide's devshell. Therefore, nix should be able to
-#       handle all of its environment management itself so it can be less dependent
-#       on direnv. To help with this, I wrote a nix utility that handles GC roots.
+#     - No GC root creation: Nix dev shell implementations already provide a way to
+#       set up the environment e.g. `shellHook` for nix's devShell or `startup.*` for
+#       numtide's devshell. Therefore, nix should be able to handle all of its
+#       environment management itself so it can be less dependent on direnv. To help
+#       with this, I wrote a nix utility that handles GC roots.
 #     - No manual reload: If you want to reload manually, you can use my
 #       direnv-manual-reload plugin. In most of the `.envrc` files that I've seen,
-#       the only thing done is loading a nix environment so providing a dedicated
-#       command to reload nix would be redundant.
-#   - A little faster. It felt like there was less of a pause when entering a
-#     directory so I made a rough benchmark to confirm:
-#       Command:
-#         `hyperfine -m 50 --shell=none 'direnv exec . true'`
-#       .envrc contents:
-#         source <path_to_plugin>
-#         use nix `devshell --file . devShell.development`/`-A devShells.development`
-#       Results:
-#         minimal-nix-direnv - 192 ms
-#         nix-direnv - 596 ms
-#
-#       I also ran the same command with an empty .envrc and got 98 ms. This means
-#       nix-direnv added 498 ms of overhead and minimal-nix-direnv added 94 ms which
-#       makes minimal-nix-direnv 5.2x faster.
+#       the only thing done is loading a nix environment so a dedicated command to
+#       reload nix would be redundant.
+#   - It also felt like there was less of a pause when entering a directory so it may
+#     be a bit faster. I did a rough benchmark with `hyperfine` and this plugin was
+#     faster by about half a second.
 
 function use_nix {
   # The name of the dev shell implementation. See the case statement below for valid
@@ -34,7 +24,8 @@ function use_nix {
   # These will be appended to `nix build` to get the devshell package.
   local -ra args=("${@:2}")
 
-  local -r cached_env_script="${direnv_layout_dir:-.direnv}/dev-shell-env.bash"
+  # Intentionally global so it can be accessed from a trap
+  cached_env_script="${direnv_layout_dir:-.direnv}/dev-shell-env.bash"
 
   local should_update=false
   if [[ ! -e $cached_env_script ]]; then
@@ -56,9 +47,23 @@ function use_nix {
     done
   fi
 
-  if [[ $should_update == 'true' ]]; then
-    local new_env_script=''
+  if [[ $should_update != 'true' ]]; then
+    # shellcheck disable=1090
+    source "$cached_env_script"
+  else
+    local original_trap
+    original_trap="$(_mnd_get_exit_trap)"
+    # direnv already sets an exit trap so we'll prepend our command to it instead
+    # of overwriting it.
+    _mnd_prepend_to_exit_trap "$(
+      printf \
+        'touch %q; _mnd_log_error %q; source %q' \
+        "$cached_env_script" \
+        "Something went wrong, loading the last dev shell" \
+        "$cached_env_script"
+    )"
 
+    local new_env_script
     # Nix may add a standard format for dev shell packages[1]. If this is done, then
     # this plugin won't need separate handlers for each dev shell implementation
     # since the script for loading the environment will always be in
@@ -69,30 +74,51 @@ function use_nix {
       # numtide/devshell
       'devshell')
         local package
-        if package="$(nix build --no-link --print-out-paths "${args[@]}")"; then
-          new_env_script="$package/env.bash"
-        fi
+        package="$(nix build --no-link --print-out-paths "${args[@]}")"
+        new_env_script="$package/env.bash"
         ;;
       *)
-        log_error "Unknown dev shell type: $type"
+        _mnd_log_error "Unknown dev shell type: $type"
+        return 1
         ;;
     esac
 
-    if [[ -n $new_env_script ]]; then
-      local -r cached_env_script_directory="${cached_env_script%/*}"
-      if [[ ! -d $cached_env_script_directory ]]; then
-        mkdir -p "$cached_env_script_directory"
-      fi
-      echo "$(<"$new_env_script")" >"$cached_env_script"
-    else
-      if [[ -e $cached_env_script ]]; then
-        log_error 'Something went wrong, loading the last dev shell'
-      else
-        return 1
-      fi
-    fi
-  fi
+    # shellcheck disable=1090
+    source "$new_env_script"
 
-  # shellcheck disable=1090
-  source "$cached_env_script"
+    local -r cached_env_script_directory="${cached_env_script%/*}"
+    if [[ ! -d $cached_env_script_directory ]]; then
+      mkdir -p "$cached_env_script_directory"
+    fi
+    echo "$(<"$new_env_script")" >"$cached_env_script"
+
+    trap -- "$original_trap" EXIT
+  fi
+}
+
+function _mnd_log_error {
+  log_error "minimal-nix-direnv: $1"
+}
+
+function _mnd_prepend_to_exit_trap {
+  local -r command="$1"
+
+  local current_trap
+  current_trap="$(_mnd_get_exit_trap)"
+
+  trap -- "$command"$'\n'"$current_trap" EXIT
+}
+
+function _mnd_get_exit_trap {
+  # `trap -p EXIT` will print 'trap -- <current_trap> EXIT'. The output seems to be
+  # formatted the way the %q directive for printf formats variables[1]. Because of
+  # this, we can use eval to tokenize it.
+  #
+  # [1]: https://www.gnu.org/software/coreutils/manual/html_node/printf-invocation.html#printf-invocation
+  local trap_output
+  trap_output="$(trap -p EXIT)"
+  eval "local -ra trap_output_tokens=($trap_output)"
+  # shellcheck disable=2154
+  # I declare `trap_output_tokens` in the eval statement above
+  echo "${trap_output_tokens[2]}"
 }
--- nix/outputs/devShells/modules/lefthook/hooks/check/lua-language-server.nix
+++ nix/outputs/devShells/modules/lefthook/hooks/check/lua-language-server.nix
@@ -1,33 +1,35 @@
 { pkgs, ... }:
 {
-  devshell.packages = with pkgs; [
-    lua-language-server
-  ];
+  devshell = {
+    packages = with pkgs; [
+      lua-language-server
+    ];
 
-  devshell.startup.lua.text = ''
-    function symlink_if_target_changed {
-      local -r target="$1"
-      local -r symlink_path="$2"
+    startup.lua.text = ''
+      function symlink_if_target_changed {
+        local -r target="$1"
+        local -r symlink_path="$2"
 
-      if [[ ! $target -ef $symlink_path ]]; then
-        ln --force --no-dereference --symbolic "$target" "$symlink_path"
+        if [[ ! $target -ef $symlink_path ]]; then
+          ln --force --no-dereference --symbolic "$target" "$symlink_path"
+        fi
+      }
+
+      prefix="$DEV_SHELL_STATE/lua-libraries"
+      if [[ ! -d $prefix ]]; then
+        mkdir -p "$prefix"
       fi
-    }
 
-    prefix="$DEV_SHELL_STATE/lua-libraries"
-    if [[ ! -d $prefix ]]; then
-      mkdir -p "$prefix"
-    fi
-
-    symlink_if_target_changed \
-      ${pkgs.myVimPluginPack}/pack/bigolu/start "$prefix/neovim-plugins"
-    symlink_if_target_changed \
-      ${pkgs.neovim}/share/nvim/runtime "$prefix/neovim-runtime"
-
-    hammerspoon_annotations="$HOME/.hammerspoon/Spoons/EmmyLua.spoon/annotations"
-    if [[ -e $hammerspoon_annotations ]]; then
       symlink_if_target_changed \
-        "$hammerspoon_annotations" "$prefix/hammerspoon-annotations"
-    fi
-  '';
+        ${pkgs.myVimPluginPack}/pack/bigolu/start "$prefix/neovim-plugins"
+      symlink_if_target_changed \
+        ${pkgs.neovim}/share/nvim/runtime "$prefix/neovim-runtime"
+
+      hammerspoon_annotations="$HOME/.hammerspoon/Spoons/EmmyLua.spoon/annotations"
+      if [[ -e $hammerspoon_annotations ]]; then
+        symlink_if_target_changed \
+          "$hammerspoon_annotations" "$prefix/hammerspoon-annotations"
+      fi
+    '';
+  };
 }
