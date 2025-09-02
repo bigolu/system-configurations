3b0fc0fab3199c572c54da0812b1b1990dfe628e
Biggie <code@bigo.lu>
2025-09-02 17:53:21 -0400

    Tweak

--- dotfiles/git/bin/git-http-to-ssh.bash
+++ dotfiles/git/bin/git-http-to-ssh.bash
@@ -6,18 +6,11 @@ set -o pipefail
 shopt -s nullglob
 shopt -s inherit_errexit
 
-# See if the remote url ends in '.git'
-if git config get remote.origin.url | grep -P '\.git$' >/dev/null; then
-  new_url="$(
-    git config get remote.origin.url |
-      sed --regexp-extended 's#(http.*://)([^/]+)/(.+)$#git@\2:\3#g'
-  )"
-else
-  new_url="$(
-    git config get remote.origin.url |
-      sed --regexp-extended 's#(http.*://)([^/]+)/(.+)$#git@\2:\3.git#g'
-  )"
-fi
+new_url="$(
+  git remote get-url origin |
+    # Ensure it ends in `.git`
+    perl -pe 's|(http.*://)([^/]+)/(.+?)(\.git)?$|git@\2:\3.git|'
+)"
 
 echo "Does this new url look fine? (y/n): $new_url"
 read -r response
