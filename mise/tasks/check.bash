44e97d4dc0e122a4ecf75245b10c6d13c51f20a6
Biggie <code@bigo.lu>
2025-09-17 18:01:42 -0400

    feat(checks): Add option to run on commits that aren't in upstream

--- mise/tasks/check.bash
+++ mise/tasks/check.bash
@@ -2,21 +2,21 @@
 #! nix-shell -i nix-shell-interpreter
 #! nix-shell --packages nix-shell-interpreter git-branchless
 #MISE description="Run jobs to find/fix issues"
-#USAGE long_about "Run jobs to find/fix issues with the code. If no flags are provided, it will behave as if `--files unpushed` was provided. The list of jobs is in `lefthook.yaml`."
+#USAGE long_about "Run jobs to find/fix issues with the code. If no flags are provided, it will behave as if `--files uncommitted` was provided. The list of jobs is in `lefthook.yaml`."
 #USAGE
 #USAGE arg "[jobs]" var=#true help="Jobs to run. If none are passed then all of them will be run"
 #USAGE complete "jobs" run=#"""
 #USAGE   fish -c 'complete --do-complete "lefthook run check --jobs "'
 #USAGE """#
 #USAGE
-#USAGE flag "-f --files <files>" help="Check the files specified" long_help="Check the files specified. `files` can be a commit range with the format `<start>..<end>` e.g. `17f0a477..HEAD`. This will check all the files changed in all the commits within that range (the range excludes the start commit). You can also provide a single commit, e.g. `HEAD`, if you only want to check the files within one. Use the special value `uncommitted` to check any files that haven't been committed including untracked files, `unpushed` to check the files of any commits that haven't been pushed, and `all` to check all tracked/untracked files."
+#USAGE flag "-f --files <files>" help="Check the files specified" long_help="Check the files specified. `files` can be a commit range with the format `<start>..<end>` e.g. `17f0a477..HEAD`. This will check all the files changed in all the commits within that range (the range excludes the start commit). You can also provide a single commit, e.g. `HEAD`, if you only want to check the files within one. Use the special value `uncommitted` to check any files that haven't been committed including untracked files, `unpushed` to check the files of any commits that haven't been pushed, `not-in-upstream` to check the files of any commits that are not in `upstream/HEAD` (there must be a remote named `upstream` for this to work), and `all` to check all tracked/untracked files."
 #USAGE complete "files" run=#"""
-#USAGE   printf '%s\n' uncommitted unpushed all
+#USAGE   printf '%s\n' uncommitted unpushed not-in-upstream all
 #USAGE """#
 #USAGE
-#USAGE flag "-c --commits <commits>" help="Check the files/messages of the commits specified" long_help="Check the files and commit message of each of the commits specified. `commits` can be a commit range with the format `<start>..<end>` e.g. `17f0a477..HEAD`. This will check the files and commit message of each commit within that range (the range excludes the start commit). You can also provide a single commit, e.g. `HEAD`, if you only want to check one. Use the special value `unpushed` to check any commits that haven't been pushed. Commits will be checked individually to ensure checks pass at each commit."
+#USAGE flag "-c --commits <commits>" help="Check the files/messages of the commits specified" long_help="Check the files and commit message of each of the commits specified. `commits` can be a commit range with the format `<start>..<end>` e.g. `17f0a477..HEAD`. This will check the files and commit message of each commit within that range (the range excludes the start commit). You can also provide a single commit, e.g. `HEAD`, if you only want to check one. Use the special value `unpushed` to check any commits that haven't been pushed or `not-in-upstream` to check any commits that are not in `upstream/HEAD` (there must be a remote named `upstream` for this to work). Commits will be checked individually to ensure checks pass at each commit."
 #USAGE complete "commits" run=#"""
-#USAGE   printf '%s\n' unpushed HEAD
+#USAGE   printf '%s\n' unpushed not-in-upstream HEAD
 #USAGE """#
 
 set -o errexit
@@ -26,7 +26,7 @@ shopt -s nullglob
 shopt -s inherit_errexit
 
 if [[ -z ${usage_commits:-} && -z ${usage_files:-} ]]; then
-  usage_files='unpushed'
+  usage_files='uncommitted'
 fi
 
 if [[ -n ${usage_commits:-} ]]; then
@@ -36,6 +36,15 @@ if [[ -n ${usage_commits:-} ]]; then
   case "$usage_commits" in
     'unpushed')
       start='^@{push}'
+      end='HEAD'
+      ;;
+    'not-in-upstream')
+      ref='upstream/HEAD'
+      if ! git rev-parse --verify --quiet "$ref" >/dev/null; then
+        echo "Error: ref '$ref' does not exist" >&2
+        exit 1
+      fi
+      start="^$ref"
       end='HEAD'
       ;;
     *'..'*)
