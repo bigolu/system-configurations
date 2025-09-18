0c69865adf0283d24bedfc555abbe725ed17d450
Biggie <code@bigo.lu>
2025-09-18 19:47:59 -0400

    feat(checks): Save a ref for HEAD before starting interactive rebase

--- mise/tasks/check.bash
+++ mise/tasks/check.bash
@@ -10,10 +10,10 @@
 #USAGE flag "-f --files <files>" help="Check the files specified" long_help="Check the files specified. `files` can be a commit range with the format `<start>..<end>` e.g. `17f0a477..HEAD`. This will check all the files changed in all the commits within that range (the range excludes the start commit). You can also provide a single commit, e.g. `HEAD`, if you only want to check the files within one. Use the special value `uncommitted` to check any files that haven't been committed including untracked files, `unpushed` to check the files of any commits that haven't been pushed, `not-in-upstream` to check the files of any commits that are not in `upstream/HEAD` (there must be a remote named `upstream` for this to work), `head` to run on all files in the current commit (i.e. HEAD), and `all` to check all tracked/untracked files."
 #USAGE complete "files" run=#" printf '%s\n' uncommitted unpushed not-in-upstream head all "#
 #USAGE
-#USAGE flag "-r --rebase <start>" help="Check commits using an interactive rebase" long_help="An interactive rebase will be started from the commit `start`. An `exec` command will be added after every commit which checks the files and message for that commit. Use the special value `unpushed` to rebase any commits that haven't been pushed or `not-in-upstream` to rebase any commits that are not in `upstream/HEAD` (requires a remote named `upstream`). If `--files` is also specified, the files specified by `--files` will be checked per commit instead of the files in the commit."
+#USAGE flag "-r --rebase <start>" help="Check commits using an interactive rebase" long_help="An interactive rebase will be started from the commit `start`. An `exec` command will be added after every commit which checks the files and message for that commit. Use the special value `unpushed` to rebase any commits that haven't been pushed or `not-in-upstream` to rebase any commits that are not in `upstream/HEAD` (requires a remote named `upstream`). If `--files` is also used, the files specified by `--files` will be checked per commit instead of the files in the commit. If you make a mistake and want to go back to where you were before the rebase, run `git reset --hard refs/project/ir-backup`."
 #USAGE complete "start" run=#" printf '%s\n' unpushed not-in-upstream "#
 #USAGE
-#USAGE flag "-c --commits <commits>" help="Check the files/messages of the commits specified" long_help="Check the files and commit message of each of the commits specified. `commits` can be a commit range with the format `<start>..<end>` e.g. `17f0a477..HEAD`. This will check the files and commit message of each commit within that range. You can also provide a single commit, e.g. `HEAD`, if you only want to check one. Use the special value `unpushed` to check any commits that haven't been pushed or `not-in-upstream` to check any commits that are not in `upstream/HEAD` (there must be a remote named `upstream` for this to work). Commits will be checked individually to ensure checks pass at each commit. If `--files` is also specified, the files specified by `--files` will be checked per commit instead of the files in the commit. The working tree must be clean to run this, meaning there are no uncommitted changes."
+#USAGE flag "-c --commits <commits>" help="Check the files/messages of the commits specified" long_help="Check the files and commit message of each of the commits specified. `commits` can be a commit range with the format `<start>..<end>` e.g. `17f0a477..HEAD`. This will check the files and commit message of each commit within that range. You can also provide a single commit, e.g. `HEAD`, if you only want to check one. Use the special value `unpushed` to check any commits that haven't been pushed or `not-in-upstream` to check any commits that are not in `upstream/HEAD` (there must be a remote named `upstream` for this to work). Commits will be checked individually to ensure checks pass at each commit. If `--files` is also used, the files specified by `--files` will be checked per commit instead of the files in the commit. The working tree must be clean to run this, meaning there are no uncommitted changes."
 #USAGE complete "commits" run=#" printf '%s\n' unpushed not-in-upstream head "#
 
 set -o errexit
@@ -45,6 +45,10 @@ if [[ -n ${usage_rebase:-} ]]; then
       start="$usage_rebase^"
       ;;
   esac
+
+  # Save a reference to the commit we were on before the rebase started, in case we
+  # want to go back.
+  git update-ref refs/project/ir-backup HEAD
 
   git rebase --interactive --exec "$lefthook_check_commit_command" "$start"
 elif [[ -n ${usage_commits:-} ]]; then
