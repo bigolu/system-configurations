6b2a715b54570db8c8cff3de527370109ab592c7
Biggie <code@bigo.lu>
2025-09-22 09:45:29 -0400

    checks: Tweak

--- mise/tasks/check.bash
+++ mise/tasks/check.bash
@@ -8,13 +8,13 @@
 #USAGE complete "jobs" run=#" fish -c 'complete --do-complete "lefthook run check --jobs "' "#
 #USAGE
 #USAGE flag "-f --files <files>" help="Check the files specified" long_help="Check the files specified. `files` can be a revision range with the format `<start>..<end>`. This will check all the files changed in all the commits within that range (`<start>` is not included in the range). You can also provide a single revision, e.g. `HEAD`, to only check the files within the commit referenced by that revision. You can also use the format `not-in-<revision>` to check any commits that are ancestors of `HEAD`, but not ancestors of `<revision>`. This is useful for checking commits that haven't been merged to a remote e.g. `not-in-upstream`. Use the special value `uncommitted` to check any files that haven't been committed including untracked files, `not-pushed` to check the files of any commits that haven't been pushed, `head` to run on all files in the current commit (i.e. HEAD), and `all` to check all tracked/untracked files. See [git's documentation for specifying a revision](https://git-scm.com/docs/git-rev-parse#_specifying_revisions)."
-#USAGE complete "files" run=#" git remote | while IFS= read -r remote; do echo "not-in-$remote"; done; printf '%s\n' uncommitted not-pushed head all "#
+#USAGE complete "files" run=#" git remote | while IFS= read -r remote; do echo "not-in-$remote"; done; printf '%s\n' uncommitted not-pushed HEAD all "#
 #USAGE
 #USAGE flag "-r --rebase <start>" help="Check commits using an interactive rebase" long_help="An interactive rebase will be started from the commit referenced by the revision in `start`. An `exec` command will be added after every commit which checks the files and message for that commit. You can use the format `not-in-<revision>` to rebase any commits that are ancestors of `HEAD`, but not ancestors of `<revision>`. This is useful for checking commits that haven't been merged to a remote e.g. `not-in-upstream`. Use the special value `not-pushed` to rebase any commits that haven't been pushed. If `--files` is also used, the files specified by `--files` will be checked per commit instead of the files in the commit. If you make a mistake and want to go back to where you were before the rebase, run `git reset --hard refs/project/ir-backup`. See [git's documentation for specifying a revision](https://git-scm.com/docs/git-rev-parse#_specifying_revisions)."
 #USAGE complete "start" run=#" git remote | while IFS= read -r remote; do echo "not-in-$remote"; done; printf '%s\n' not-pushed "#
 #USAGE
 #USAGE flag "-c --commits <commits>" help="Check the files/messages of the commits specified" long_help="Check the files and commit message of each of the commits specified. `commits` can be a revision range with the format `<start>..<end>`. This will check the files and commit message of each commit within that range (`<start>` is not included in the range). `commits` can also have the format `not-in-<revision>` to check any commits that are ancestors of `HEAD`, but not ancestors of `<revision>`. This is useful for checking commits that haven't been merged to a remote e.g. `not-in-upstream`. You can also provide a single commit, e.g. `HEAD`, if you only want to check one. Use the special value `not-pushed` to check any commits that haven't been pushed. Commits will be checked individually to ensure checks pass at each commit. If `--files` is also used, the files specified by `--files` will be checked per commit instead of the files in the commit. See [git's documentation for specifying a revision](https://git-scm.com/docs/git-rev-parse#_specifying_revisions)."
-#USAGE complete "commits" run=#" git remote | while IFS= read -r remote; do echo "not-in-$remote"; done; printf '%s\n' not-pushed head "#
+#USAGE complete "commits" run=#" git remote | while IFS= read -r remote; do echo "not-in-$remote"; done; printf '%s\n' not-pushed HEAD "#
 
 set -o errexit
 set -o nounset
@@ -38,10 +38,10 @@ if [[ -n ${usage_rebase:-} ]]; then
         echo "error: Revision '$revision' does not exist" >&2
         exit 1
       fi
-      start="$(git merge-base "$revision" 'HEAD')"
+      start="$(git merge-base "$revision" HEAD)"
       ;;
     *)
-      start="$usage_rebase^"
+      start="$usage_rebase"
       ;;
   esac
 
@@ -81,9 +81,6 @@ elif [[ -n ${usage_commits:-} ]]; then
         exit 1
       fi
       range="$(git merge-base "$revision" 'HEAD').."
-      ;;
-    'head')
-      range='HEAD^!'
       ;;
     *'..'*)
       range="$usage_commits"
