ef3b316ddd83219e45572c339adf5895c4e8bb6d
Biggie <code@bigo.lu>
2025-09-23 10:03:43 -0400

    secrets: Use temporary directory for `doppler` mount
    The mount isn't accessed after the call to `doppler` so there's no need to
    store it in `$DEV_SHELL_STATE`.

--- mise/tasks/get-secrets.bash
+++ mise/tasks/get-secrets.bash
@@ -1,6 +1,6 @@
 #! Though we don't use shebangs, cached-nix-shell expects the first line to be one so we put this on the first line instead.
 #! nix-shell -i nix-shell-interpreter
-#! nix-shell --packages nix-shell-interpreter
+#! nix-shell --packages nix-shell-interpreter coreutils
 
 set -o errexit
 set -o nounset
@@ -8,11 +8,13 @@ set -o pipefail
 shopt -s nullglob
 shopt -s inherit_errexit
 
+temp="$(mktemp --directory)"
+
 # GITHUB_TOKEN is for lychee
 #
 # shellcheck disable=2016
 doppler run \
-  --mount "${DEV_SHELL_STATE:?}/doppler-mount" \
+  --mount "$temp/doppler-mount" \
   --mount-format 'env' \
   --only-secrets GH_TOKEN \
   --only-secrets GITHUB_TOKEN \
