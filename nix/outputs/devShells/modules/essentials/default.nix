62d2433d34dec320d8673b7b45905831200d0adf
Biggie <code@bigo.lu>
2025-10-11 18:51:25 -0400

    Tweak

--- nix/outputs/devShells/modules/essentials/default.nix
+++ nix/outputs/devShells/modules/essentials/default.nix
@@ -47,6 +47,31 @@ in
     '';
 
     startup = {
+      # HACK: This should run before other startup scripts, but there's no way to
+      # control the order. I noticed that they're sorted so I'm prefixing it with
+      # 'AAA' in hopes that it gets put first.
+      AAAstateDirectory.text =
+        let
+          inherit (pkgs) coreutils;
+          mkdir = getExe' coreutils "mkdir";
+        in
+        ''
+          export DEV_SHELL_STATE="''${DEV_SHELL_STATE:-''${PRJ_ROOT:?}/.dev-shell-state}"
+          if [[ ! -e $DEV_SHELL_STATE ]]; then
+            ${mkdir} --parents "$DEV_SHELL_STATE"
+            echo '*' >"$DEV_SHELL_STATE/.gitignore"
+          fi
+        '';
+
+      secrets.text = ''
+        if [[ -e $PRJ_ROOT/.env ]]; then
+          # Export any variables that are modified/created
+          set -a
+          source "$PRJ_ROOT/.env"
+          set +a
+        fi
+      '';
+
       gcRoot.text =
         (pkgs.gcRoot {
           script.rootPath.eval = "$DEV_SHELL_STATE/gc-roots/${name}";
@@ -91,31 +116,6 @@ in
             );
           };
         }).script;
-
-      # HACK: This should run before other startup scripts, but there's no way to
-      # control the order. I noticed that they're sorted so I'm prefixing it with
-      # 'AAA' in hopes that it gets put first.
-      AAAstateDirectory.text =
-        let
-          inherit (pkgs) coreutils;
-          mkdir = getExe' coreutils "mkdir";
-        in
-        ''
-          export DEV_SHELL_STATE="''${DEV_SHELL_STATE:-''${PRJ_ROOT:?}/.dev-shell-state}"
-          if [[ ! -e $DEV_SHELL_STATE ]]; then
-            ${mkdir} --parents "$DEV_SHELL_STATE"
-            echo '*' >"$DEV_SHELL_STATE/.gitignore"
-          fi
-        '';
-
-      secrets.text = ''
-        if [[ -e $PRJ_ROOT/.env ]]; then
-          # Export any variables that are modified/created
-          set -a
-          source "$PRJ_ROOT/.env"
-          set +a
-        fi
-      '';
     };
   };
 }
