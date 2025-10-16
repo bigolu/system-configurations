c17fcfc06fdea590822145dd30453e326d435f78
Biggie <code@bigo.lu>
2025-10-16 16:16:03 -0400

    Refactor

--- nix/utils/home-modules/common/terminal/neovim.nix
+++ nix/utils/home-modules/common/terminal/neovim.nix
@@ -3,19 +3,13 @@
   ...
 }:
 let
-  inherit (pkgs) runCommand myVimPluginPack neovim;
-
-  # Put the pack under share/ so neovim can automatically find it
-  vimPack = runCommand "my-vim-pack" { } ''
-    pack_path="$out/share/nvim"
-    mkdir -p "$pack_path"
-    ln --symbolic ${myVimPluginPack} "$pack_path/site"
-  '';
+  inherit (pkgs) linkFarm myVimPluginPack neovim;
 in
 {
   home.packages = [
     neovim
-    vimPack
+    # Put the pack under share/ so neovim can automatically find it
+    (linkFarm "my-vim-pack" { "share/nvim/site" = myVimPluginPack; })
   ];
 
   repository.xdg.configFile."nvim".source = "neovim";
