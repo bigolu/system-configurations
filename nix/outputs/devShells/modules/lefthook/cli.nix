fc78db5cfda910a83ed0436d1db5fc64e35885a0
Biggie <code@bigo.lu>
2025-10-23 20:21:02 -0400

    Tweak

--- nix/outputs/devShells/modules/lefthook/cli.nix
+++ nix/outputs/devShells/modules/lefthook/cli.nix
@@ -1,9 +1,24 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  inherit (lib) getExe';
+  inherit (pkgs) coreutils;
+  touch = getExe' coreutils "touch";
+in
 {
-  devshell.packages = with pkgs; [
-    lefthook
-    # TODO: Lefthook won't run unless git is present so maybe nixpkgs should make it
-    # a dependency.
-    git
-  ];
+  devshell = {
+    packages = with pkgs; [
+      lefthook
+      # TODO: Lefthook won't run unless git is present so maybe nixpkgs should make it
+      # a dependency.
+      git
+    ];
+
+    startup.lefthook.text = ''
+      # We only need to do this once since lefthook reinstalls hooks automatically.
+      if [[ ! -e "$DEV_SHELL_STATE/lefthook-installed" ]]; then
+        lefthook install --force
+        ${touch} "$DEV_SHELL_STATE/lefthook-installed"
+      fi
+    '';
+  };
 }
