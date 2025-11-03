fd795639ee02ca30a685b00f99c72f5c8109bc3c
Biggie <code@bigo.lu>
2025-11-03 16:26:20 -0500

    Fix

--- mise/tasks/ci/lychee.bash
+++ mise/tasks/ci/lychee.bash
@@ -1,6 +1,6 @@
 #! Though we don't use shebangs, cached-nix-shell expects the first line to be one so we put this on the first line instead.
 #! nix-shell -i nix-shell-interpreter
-#! nix-shell --packages nix-shell-interpreter gh lychee coreutils
+#! nix-shell --packages nix-shell-interpreter gh lychee coreutils git
 #MISE hide=true
 
 set -o errexit
