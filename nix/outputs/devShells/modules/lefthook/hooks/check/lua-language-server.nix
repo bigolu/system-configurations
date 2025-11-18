09b66bb35533b71248e180d357195d5dde0f270a
Biggie <code@bigo.lu>
2025-11-18 12:50:40 -0500

    lua-language-server: Use environment variables for library paths

--- nix/outputs/devShells/modules/lefthook/hooks/check/lua-language-server.nix
+++ nix/outputs/devShells/modules/lefthook/hooks/check/lua-language-server.nix
@@ -1,42 +1,25 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 let
-  inherit (lib) getExe';
-  inherit (pkgs) coreutils;
-
-  ln = getExe' coreutils "ln";
-  mkdir = getExe' coreutils "mkdir";
+  inherit (pkgs) linkFarm;
 in
 {
   devshell = {
     packages = with pkgs; [
       lua-language-server
     ];
-
-    startup.lua.text = ''
-      function symlink_if_target_changed {
-        local -r target="$1"
-        local -r symlink_path="$2"
-
-        if [[ ! $target -ef $symlink_path ]]; then
-          ${ln} --force --no-dereference --symbolic "$target" "$symlink_path"
-        fi
-      }
-
-      prefix="$PRJ_DATA_DIR/lua-libraries"
-      if [[ ! -d $prefix ]]; then
-        ${mkdir} -p "$prefix"
-      fi
-
-      symlink_if_target_changed \
-        ${pkgs.myVimPluginPack}/pack/bigolu/start "$prefix/neovim-plugins"
-      symlink_if_target_changed \
-        ${pkgs.neovim}/share/nvim/runtime "$prefix/neovim-runtime"
-
-      hammerspoon_annotations="$HOME/.hammerspoon/Spoons/EmmyLua.spoon/annotations"
-      if [[ -e $hammerspoon_annotations ]]; then
-        symlink_if_target_changed \
-          "$hammerspoon_annotations" "$prefix/hammerspoon-annotations"
-      fi
-    '';
   };
+
+  env = [
+    {
+      name = "LUA_LIBRARY_NIX";
+      value = linkFarm "lua-libraries" {
+        neovim-runtime = "${pkgs.neovim}/share/nvim/runtime";
+        neovim-plugins = "${pkgs.myVimPluginPack}/pack/bigolu/start";
+      };
+    }
+    {
+      name = "LUA_LIBRARY_HAMMERSPOON";
+      eval = "$HOME/.hammerspoon/Spoons/EmmyLua.spoon/annotations";
+    }
+  ];
 }
