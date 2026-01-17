18ebc5cb0bc259795ea6c7f01c8c8cef2eef2576
Biggie <code@bigo.lu>
2026-01-16 19:34:51 -0500

    direnv: Remove outdated comment

--- direnv-recommended.bash
+++ direnv-recommended.bash
@@ -1,6 +1,4 @@
 source direnv/plugins/direnv-manual-reload.bash
 direnv_manual_reload
 source direnv/plugins/minimal-nix-direnv.bash
-# This has to be after `direnv_manual_reload`. See the source code for
-# `direnv_manual_reload` to learn why.
 use_nix numtide_dev_shell --file . devShells.development
