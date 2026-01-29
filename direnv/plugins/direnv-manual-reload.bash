606114d1e7eb3488e39a171e3bb8f74cbb342683
Biggie <code@bigo.lu>
2026-01-29 14:57:46 -0500

    direnv-manual-reload: Wrap `direnv`

--- direnv/plugins/direnv-manual-reload.bash
+++ direnv/plugins/direnv-manual-reload.bash
@@ -1,8 +1,5 @@
 # Disable direnv's automatic reloading. It will still automatically load when you
-# first enter the directory, but it will not reload after that. If you want to reload
-# the environment, you can instead use the command `direnv-reload` which will be
-# added to the PATH. If you use this script, `direnv reload` will no longer work, you
-# have to use `direnv-reload`.
+# first enter the directory, but it will not reload after that.
 #
 # Usage:
 #   Add the following two lines to the top of your envrc:
@@ -35,15 +32,15 @@
 #     up the new environment as well. If you use this script, then the only watched
 #     file will be the one created by the script. Since you never edit that file, you
 #     won't trigger any reloads. Instead, you can reload both your terminal and
-#     editor's direnv environment by running `direnv-reload`.
+#     editor's direnv environment by running `direnv reload`.
 #
 # How it works:
 #   - direnv automatically reloads whenever it detects a change in the modification
 #     time of any of the files on its watch list. To stop it from doing so, this
 #     script removes all files from the watch list except the following ones:
 #       - reload file: To give users a way to manually reload direnv, a new file is
-#         created and put on the watch list, `.direnv/reload`. A command named
-#         `direnv-reload` is put on the PATH which will change the modification time
+#         created and put on the watch list, `.direnv/reload`. A direnv wrapper
+#         is put on the PATH which will change the modification time
 #         of that file, causing direnv to reload.
 #       - allow/deny files: We keep those files on the watch list so
 #         `direnv allow/block` still triggers a reload.
@@ -52,32 +49,38 @@
 # [2]: https://github.com/nix-community/nix-direnv?tab=readme-ov-file#tracked-files
 # [3]: https://github.com/direnv/direnv-vscode
 function direnv_manual_reload {
-  local layout_dir
-  layout_dir="$(_dmr_make_layout_dir)"
+  local directory
+  directory="$(_dmr_make_directory)"
 
   local reload_file
-  reload_file="$(_dmr_make_reload_file "$layout_dir")"
+  reload_file="$(_dmr_make_reload_file "$directory")"
 
   _dmr_set_watch_list "$reload_file"
   _dmr_disable_file_watching
-  _dmr_add_reload_program_to_path "$layout_dir" "$reload_file"
+  _dmr_add_direnv_wrapper_to_path "$directory" "$reload_file"
+  # If the user calls `direnv_manual_reload` in their config and then removes
+  # it, the original `direnv reload` behavior should be restored. To do this, we
+  # set an environment variable when `direnv_manual_reload` is called and the
+  # direnv wrapper will only use the manual-reload behavior when this variable
+  # is set.
+  export DIRENV_MANUAL_RELOAD=true
 }
 
-function _dmr_make_layout_dir {
-  local layout_dir
-  layout_dir="$(direnv_layout_dir)"
+function _dmr_make_directory {
+  local directory
+  directory="$(direnv_layout_dir)/manual-reload"
 
-  if [[ ! -e $layout_dir ]]; then
-    mkdir -p "$layout_dir"
+  if [[ ! -e $directory ]]; then
+    mkdir -p "$directory"
   fi
 
-  echo "$layout_dir"
+  echo "$directory"
 }
 
 function _dmr_make_reload_file {
-  local -r layout_dir="$1"
+  local -r directory="$1"
 
-  local -r reload_file="$layout_dir/reload"
+  local -r reload_file="$directory/reload"
   if [[ ! -e $reload_file ]]; then
     # Create a file without using an external command
     true >"$reload_file"
@@ -129,40 +132,73 @@ function _dmr_disable_file_watching {
   }
 }
 
-function _dmr_add_reload_program_to_path {
-  local -r layout_dir="$1"
+function _dmr_add_direnv_wrapper_to_path {
+  local -r directory="$1"
   local -r reload_file="$2"
 
-  local -r direnv_bin="$layout_dir/bin"
-  if [[ ! -e $direnv_bin ]]; then
-    mkdir "$direnv_bin"
+  local direnv_path
+  direnv_path="$(type -P direnv)"
+
+  # We can't use `$(direnv_layout_dir)/bin` because other plugins use that and
+  # if they add that directory to the path before us, then when we try to get
+  # the path to direnv above, we may get the path to our wrapper.
+  local bin_directory
+  bin_directory="$directory/bin"
+  if [[ ! -e $bin_directory ]]; then
+    mkdir -p "$bin_directory"
   fi
   # Ensure we don't add the same directory to the PATH twice.
-  PATH_rm "$direnv_bin"
-  PATH_add "$direnv_bin"
+  #
+  # This shouldn't be done in `_dmr_make_bin_directory` since it gets run in a
+  # subshell.
+  PATH_rm "$bin_directory"
+  PATH_add "$bin_directory"
 
-  local reload_program
-  reload_program="$direnv_bin/direnv-reload"
+  local -r wrapper_prefix='direnv-wrapper-'
+  local -r direnv_wrapper="$directory/${wrapper_prefix}${direnv_path//\//-}"
+  if [[ ! -e $direnv_wrapper ]]; then
+    # Remove an outdated wrapper.
+    #
+    # `-f` avoids an error if it doesn't exist and avoids a race condition
+    # between multiple instances of direnv e.g. a direnv editor extension and
+    # the terminal.
+    rm -f "$directory/$wrapper_prefix"*
+  fi
 
-  local bash_path
-  bash_path="$(type -P bash)"
-
-  local reload_file_escaped
-  reload_file_escaped="$(printf '%q' "$reload_file")"
+  local -r reload_file_escaped="$(printf '%q' "$reload_file")"
+  local -r direnv_path_escaped="$(printf '%q' "$direnv_path")"
+  local -r reload_program_content="#!/usr/bin/env bash
+    if ((\$# == 1)) && [[ \$1 == 'reload' ]] && [[ \$DIRENV_MANUAL_RELOAD == 'true' ]]; then
+      touch $reload_file_escaped
+    else
+      $direnv_path_escaped \"\$@\"
+    fi
+  "
 
   # TODO(perf): To avoid always remaking this file, we could add the version of this
   # script to the file name and make a symlink to it without the version. This way,
   # we could do a `-e` check to avoid creating the file again. This isn't done
   # because this script currently isn't versioned.
-  #
-  # Alternatively, direnv could set a variable before a script gets sourced with
-  # `source_url` that contains the file hash.
-  cat >"$reload_program" <<EOF
-#!$bash_path
-touch $reload_file_escaped
-EOF
+  _dmr_atomic_make_file "$reload_program_content" "$direnv_wrapper"
 
-  if [[ ! -x $reload_program ]]; then
-    chmod +x "$reload_program"
+  if [[ ! -x $direnv_wrapper ]]; then
+    chmod +x "$direnv_wrapper"
   fi
+
+  # We use `-f` to avoid a race condition between multiple instances of direnv e.g.
+  # a direnv editor extension and the terminal.
+  ln -fs "$direnv_wrapper" "$bin_directory/direnv"
+}
+
+function _dmr_atomic_make_file {
+  local -r content="$1"
+  local -r path="$2"
+
+  local temp
+  temp="$(mktemp)"
+
+  echo "$content" >"$temp"
+  # We use `-f` to avoid a race condition between multiple instances of direnv e.g. a
+  # direnv editor extension and the terminal.
+  mv -f "$temp" "$path"
 }
