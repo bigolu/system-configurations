b335877b4e95ff28d75b3561c32689190317cab1
Biggie <code@bigo.lu>
2026-01-31 06:34:50 -0500

    Tweak

--- nix/outputs/devShells/default.nix
+++ nix/outputs/devShells/default.nix
@@ -47,7 +47,7 @@ makeShells
         packages = with pkgs; [ npins ];
         startup.repl-overlay.text = ''
           # TODO
-          export LEFTHOOK_EXCLUDE='4-nix-outputs,system'
+          export LEFTHOOK_EXCLUDE='4-nix-outputs,system,fish'
 
           export NIX_CONFIG="
             ''${NIX_CONFIG:-}
