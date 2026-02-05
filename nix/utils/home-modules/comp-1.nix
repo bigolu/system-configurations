a35f7aceeecdeb38223ff9eeb89286c6baa0f2bc
Biggie <code@bigo.lu>
2026-02-04 20:07:40 -0500

    home-manager: Remove Nvidia settings
    The Nvidia driver includes them now

--- nix/utils/home-modules/comp-1.nix
+++ nix/utils/home-modules/comp-1.nix
@@ -16,20 +16,6 @@
 
     activation = {
       nvidiaSuspensionFix = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        # Whenever I resume from suspension on Pop!_OS, I get a black screen and
-        # then I have to switch to another tty to reboot. Apparently, the issue
-        # is caused by Nvidia. This fix was suggested on the issue tracker[1].
-        # More details on the fix can be found here[2].
-        #
-        # [1]: https://github.com/pop-os/pop/issues/2605#issuecomment-2526281526
-        # [2]: https://wiki.archlinux.org/title/NVIDIA/Tips_and_tricks#Preserve_video_memory_after_suspend
-        settings_file='/etc/modprobe.d/bigolu-nvidia-suspension-fix.conf'
-        if [[ ! -e $settings_file ]]; then
-          setting='options nvidia NVreg_PreserveVideoMemoryAllocations=1 NVreg_TemporaryFilePath=/var/tmp'
-          echo "$setting" | sudo tee "$settings_file"
-          sudo update-initramfs -u -k all
-        fi
-
         # When I try to suspend, the screen just flashes black and the system
         # doesn't suspend. This fixes it [1].
         #
