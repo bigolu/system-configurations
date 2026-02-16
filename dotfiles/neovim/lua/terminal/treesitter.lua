93beadeb1939c884447266ca4e74b92d352d42f4
Biggie <code@bigo.lu>
2026-02-16 17:50:55 -0500

    vscode: Switch to another generic LSP client

--- dotfiles/neovim/lua/terminal/treesitter.lua
+++ dotfiles/neovim/lua/terminal/treesitter.lua
@@ -6,13 +6,20 @@ local function is_current_buffer_too_big_to_highlight()
 end
 
 local filetypes_with_regex_highlights = vim.fn.getcompletion("", "syntax") or {}
+
 local filetypes_with_treesitter_parser = require("nvim-treesitter.parsers")
+
+local function fix(filetype)
+  return ({
+    gitrebase = "git_rebase",
+  })[filetype] or filetype
+end
 
 vim.cmd.syntax("manual")
 vim.api.nvim_create_autocmd("FileType", {
   callback = function()
     if not is_current_buffer_too_big_to_highlight() then
-      if filetypes_with_treesitter_parser[vim.bo.filetype] ~= nil then
+      if filetypes_with_treesitter_parser[fix(vim.bo.filetype)] ~= nil then
         vim.treesitter.start()
       elseif vim.tbl_contains(filetypes_with_regex_highlights, vim.bo.filetype) then
         vim.bo.syntax = "ON"
