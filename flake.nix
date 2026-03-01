03550910c6d3bc48bde197501449c7a2babdaa7e
Biggie <code@bigo.lu>
2026-03-01 03:53:58 -0500

    Fix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-    flake-compat.url = "git+https://git.lix.systems/lix-project/flake-compat";
+    flake-compat.url = "https://git.lix.systems/lix-project/flake-compat/archive/main.tar.gz";
 
     gomod2nix = {
       url = "github:nix-community/gomod2nix";
