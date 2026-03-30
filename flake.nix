b6f4ff443cf2716e5dcd4877761d95de4305354d
Biggie <code@bigo.lu>
2026-03-29 21:45:18 -0400

    Use flake-utils

--- flake.nix
+++ flake.nix
@@ -6,7 +6,10 @@
 
     gomod2nix = {
       url = "github:nix-community/gomod2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
     };
 
     gitignore = {
@@ -57,6 +60,8 @@
       url = "github:Mic92/nix-fast-build";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   # I only define these outputs so I can use:
@@ -64,46 +69,7 @@
   #   - `nix run <this_repo>`
   outputs =
     inputs:
-    let
-      # Everything below was taken from https://github.com/numtide/flake-utils
-
-      systems = [
-        "aarch64-linux"
-        "aarch64-darwin"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-
-      # Builds a map from <attr>=value to <attr>.<system>=value for each system.
-      eachSystem = eachSystemOp (
-        # Merge outputs for each system.
-        f: attrs: system:
-        let
-          ret = f system;
-        in
-        builtins.foldl' (
-          attrs: key:
-          attrs
-          // {
-            ${key} = (attrs.${key} or { }) // {
-              ${system} = ret.${key};
-            };
-          }
-        ) attrs (builtins.attrNames ret)
-      ) systems;
-
-      # Applies a merge operation across systems.
-      eachSystemOp =
-        op: systems: f:
-        builtins.foldl' (op f) { } (
-          if !builtins ? currentSystem || builtins.elem builtins.currentSystem systems then
-            systems
-          else
-            # Add the current system if the --impure flag is used.
-            systems ++ [ builtins.currentSystem ]
-        );
-    in
-    eachSystem (
+    inputs.flake-utils.lib.eachDefaultSystem (
       system:
       let
         outputs = import ./. {
