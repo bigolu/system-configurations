795e9ac233c18d7c4a5f19b84623220d99300cea
Biggie <code@bigo.lu>
2026-04-09 17:47:03 -0400

    Fix

--- nix/outputs/packages/init-config/default.nix
+++ nix/outputs/packages/init-config/default.nix
@@ -8,40 +8,54 @@ nixpkgs.callPackage (
     direnv,
     perl,
     coreutils,
+    writeShellApplication,
   }:
   let
     inherit (lib) getExe getExe';
-  in
-  resholve.mkDerivation {
-    pname = name;
-    version = "0.0.1";
-    src = ./init-config.bash;
-    meta.mainProgram = name;
-    dontUnpack = true;
-    installPhase = ''
-      install -D $src $out/bin/${name}
-    '';
-    solutions.default = {
-      scripts = [ "bin/${name}" ];
-      interpreter = "${bash}/bin/bash";
-      inputs = [
-        git
-        direnv
-        perl
-        coreutils
-      ];
-      execer = [
-        "cannot:${getExe git}"
-        "cannot:${getExe direnv}"
-        "cannot:${getExe' perl "shasum"}"
-      ];
-      keep = {
-        "/bin/bash" = true;
+
+    script = resholve.mkDerivation {
+      pname = name;
+      version = "0.0.1";
+      src = ./init-config.bash;
+      meta.mainProgram = name;
+      dontUnpack = true;
+      installPhase = ''
+        install -D $src $out/bin/${name}
+      '';
+      solutions.default = {
+        scripts = [ "bin/${name}" ];
+        interpreter = "${bash}/bin/bash";
+        inputs = [
+          git
+          direnv
+          perl
+          coreutils
+        ];
+        execer = [
+          "cannot:${getExe git}"
+          "cannot:${getExe direnv}"
+          "cannot:${getExe' perl "shasum"}"
+        ];
+        keep = {
+          "/bin/bash" = true;
+        };
+        fake.external = [
+          "mise"
+          "curl"
+        ];
       };
-      fake.external = [
-        "mise"
-        "curl"
-      ];
     };
+  in
+  writeShellApplication {
+    name = "run-as-admin";
+    # direnv plugins assume these are on the PATH
+    runtimeInputs = [
+      direnv
+      bash
+      coreutils
+    ];
+    text = ''
+      ${getExe script}
+    '';
   }
 ) { }
