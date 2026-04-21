fbf8d15f7ed9a17fc740d3c2b93008419decf388
Biggie <code@bigo.lu>
2026-04-20 22:52:06 -0400

    Sort

--- dotfiles/neovim/colors/bigolu.lua
+++ dotfiles/neovim/colors/bigolu.lua
@@ -22,20 +22,20 @@ if vim.o.background == "dark" then
     [13] = "#B48EAD",
     [14] = "#9BC8C7",
     [15] = "#A5ABBC",
-    bg2 = "#2f333e",
-    pmenu = "#242833",
-    pmenu_thumb = "#3a3e49",
-    error_bg = "#2e2129",
-    warn_bg = "#2a2e29",
-    info_bg = "#1c2935",
-    ok_bg = "#1e2915",
-    inlay_bg = "#292d38",
-    inlay_fg = "#abb4c4",
     accent = "#88C0D0",
+    bg2 = "#2f333e",
+    error_bg = "#2e2129",
     guide = "#373c47",
     inactive_number = "#505c73",
+    info_bg = "#1c2935",
+    inlay_bg = "#292d38",
+    inlay_fg = "#abb4c4",
     notification = "#242833",
+    ok_bg = "#1e2915",
+    pmenu = "#242833",
+    pmenu_thumb = "#3a3e49",
     string = "#9BC8C7",
+    warn_bg = "#2a2e29",
   }
 else
   colors = {
@@ -55,20 +55,20 @@ else
     [13] = "#652d90",
     [14] = "#71101f",
     [15] = "#595959",
-    bg2 = "#e3e5e7",
-    pmenu = "#eef0f2",
-    pmenu_thumb = "#d1d3d5",
-    error_bg = "#fceff0",
-    warn_bg = "#fdf7f1",
-    info_bg = "#edf4fe",
-    ok_bg = "#e8fbee",
-    inlay_bg = "#e3e5e7",
-    inlay_fg = "#666666",
     accent = "#71101f",
+    bg2 = "#e3e5e7",
+    error_bg = "#fceff0",
     guide = "#d3d3d3",
     inactive_number = "#8c8c8c",
+    info_bg = "#edf4fe",
+    inlay_bg = "#e3e5e7",
+    inlay_fg = "#666666",
     notification = "#f6f8fa",
+    ok_bg = "#e8fbee",
+    pmenu = "#eef0f2",
+    pmenu_thumb = "#d1d3d5",
     string = "#71101f",
+    warn_bg = "#fdf7f1",
   }
 end
 
@@ -81,43 +81,43 @@ local groups = {
   -- }}}
 
   -- searching {{{
-  Search = "Visual", -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
   CurSearch = "Search", -- Highlighting a search pattern under the cursor (see 'hlsearch')
   IncSearch = "Search", -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
+  Search = "Visual", -- Last search pattern highlighting (see 'hlsearch'). Also used for similar items that need to stand out.
   -- }}}
 
   -- diagnostics {{{
-  ErrorMsg = { ctermfg = 1, fg = colors[1] }, -- Error messages on the command line
-  WarningMsg = { ctermfg = 3, fg = colors[3] }, -- Warning messages
-  Error = { undercurl = true, ctermfg = 1, sp = colors[1] }, -- Any erroneous construct
-  Warning = { undercurl = true, ctermfg = 3, sp = colors[3] },
-  NvimInternalError = "ErrorMsg",
-  DiagnosticError = "ErrorMsg", -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-  DiagnosticWarn = "WarningMsg", -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-  DiagnosticInfo = { ctermfg = 4, fg = colors[4] }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-  DiagnosticHint = "DiagnosticInfo", -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-  DiagnosticOk = { ctermfg = 2, fg = colors[2] }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-  DiagnosticVirtualTextError = { ctermfg = 1, bold = true, bg = colors.error_bg, fg = colors[1] }, -- Used for "Error" diagnostic virtual text.
-  DiagnosticVirtualTextWarn = { ctermfg = 3, bold = true, bg = colors.warn_bg, fg = colors[3] }, -- Used for "Warn" diagnostic virtual text.
-  DiagnosticVirtualTextInfo = { ctermfg = 4, bold = true, bg = colors.info_bg, fg = colors[4] }, -- Used for "Info" diagnostic virtual text.
-  DiagnosticVirtualTextHint = "DiagnosticVirtualTextInfo", -- Used for "Hint" diagnostic virtual text.
-  DiagnosticVirtualTextOk = { ctermfg = 2, bold = true, bg = colors.ok_bg, fg = colors[2] }, -- Used for "Ok" diagnostic virtual text.
-  DiagnosticUnderlineError = "Error", -- Used to underline "Error" diagnostics.
-  DiagnosticUnderlineWarn = "Warning", -- Used to underline "Warn" diagnostics.
-  DiagnosticUnderlineInfo = { ctermfg = 4, undercurl = true, sp = colors[4] }, -- Used to underline "Info" diagnostics.
-  DiagnosticUnderlineHint = "DiagnosticUnderlineInfo", -- Used to underline "Hint" diagnostics.
-  DiagnosticUnderlineOk = { ctermfg = 2, undercurl = true, sp = colors[2] }, -- Used to underline "Ok" diagnostics.
-  DiagnosticFloatingError = "DiagnosticError", -- Used to color "Error" diagnostic messages in diagnostics float. See |vim.diagnostic.open_float()|
-  DiagnosticFloatingWarn = "DiagnosticWarn", -- Used to color "Warn" diagnostic messages in diagnostics float.
-  DiagnosticFloatingInfo = "DiagnosticInfo", -- Used to color "Info" diagnostic messages in diagnostics float.
-  DiagnosticFloatingHint = "DiagnosticHint", -- Used to color "Hint" diagnostic messages in diagnostics float.
-  DiagnosticFloatingOk = "DiagnosticOk", -- Used to color "Ok" diagnostic messages in diagnostics float.
-  DiagnosticUnnecessary = { ctermfg = 8, undercurl = true, sp = colors[8] },
   DiagnosticDeprecated = "DiagnosticUnnecessary",
+  DiagnosticError = "ErrorMsg", -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+  DiagnosticFloatingError = "DiagnosticError", -- Used to color "Error" diagnostic messages in diagnostics float. See |vim.diagnostic.open_float()|
+  DiagnosticFloatingHint = "DiagnosticHint", -- Used to color "Hint" diagnostic messages in diagnostics float.
+  DiagnosticFloatingInfo = "DiagnosticInfo", -- Used to color "Info" diagnostic messages in diagnostics float.
+  DiagnosticFloatingOk = "DiagnosticOk", -- Used to color "Ok" diagnostic messages in diagnostics float.
+  DiagnosticFloatingWarn = "DiagnosticWarn", -- Used to color "Warn" diagnostic messages in diagnostics float.
+  DiagnosticHint = "DiagnosticInfo", -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+  DiagnosticInfo = { ctermfg = 4, fg = colors[4] }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+  DiagnosticOk = { ctermfg = 2, fg = colors[2] }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
   DiagnosticSignError = "DiagnosticError",
-  DiagnosticSignWarn = "DiagnosticWarn",
-  DiagnosticSignInfo = "DiagnosticInfo",
   DiagnosticSignHint = "DiagnosticHint",
+  DiagnosticSignInfo = "DiagnosticInfo",
+  DiagnosticSignWarn = "DiagnosticWarn",
+  DiagnosticUnderlineError = "Error", -- Used to underline "Error" diagnostics.
+  DiagnosticUnderlineHint = "DiagnosticUnderlineInfo", -- Used to underline "Hint" diagnostics.
+  DiagnosticUnderlineInfo = { ctermfg = 4, undercurl = true, sp = colors[4] }, -- Used to underline "Info" diagnostics.
+  DiagnosticUnderlineOk = { ctermfg = 2, undercurl = true, sp = colors[2] }, -- Used to underline "Ok" diagnostics.
+  DiagnosticUnderlineWarn = "Warning", -- Used to underline "Warn" diagnostics.
+  DiagnosticUnnecessary = { ctermfg = 8, undercurl = true, sp = colors[8] },
+  DiagnosticVirtualTextError = { ctermfg = 1, bold = true, bg = colors.error_bg, fg = colors[1] }, -- Used for "Error" diagnostic virtual text.
+  DiagnosticVirtualTextHint = "DiagnosticVirtualTextInfo", -- Used for "Hint" diagnostic virtual text.
+  DiagnosticVirtualTextInfo = { ctermfg = 4, bold = true, bg = colors.info_bg, fg = colors[4] }, -- Used for "Info" diagnostic virtual text.
+  DiagnosticVirtualTextOk = { ctermfg = 2, bold = true, bg = colors.ok_bg, fg = colors[2] }, -- Used for "Ok" diagnostic virtual text.
+  DiagnosticVirtualTextWarn = { ctermfg = 3, bold = true, bg = colors.warn_bg, fg = colors[3] }, -- Used for "Warn" diagnostic virtual text.
+  DiagnosticWarn = "WarningMsg", -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+  Error = { undercurl = true, ctermfg = 1, sp = colors[1] }, -- Any erroneous construct
+  ErrorMsg = { ctermfg = 1, fg = colors[1] }, -- Error messages on the command line
+  NvimInternalError = "ErrorMsg",
+  Warning = { undercurl = true, ctermfg = 3, sp = colors[3] },
+  WarningMsg = { ctermfg = 3, fg = colors[3] }, -- Warning messages
   -- }}}
 
   -- float {{{
@@ -126,10 +126,9 @@ local groups = {
   -- }}}
 
   -- syntax groups {{{
-  Comment = { ctermfg = 8, fg = colors[8] }, -- Any comment
-
-  String = { ctermfg = 14, fg = colors.string }, --   A string constant: "this is a string"
   Character = "String", --   A character constant: 'c', '\n'
+  Comment = { ctermfg = 8, fg = colors[8] }, -- Any comment
+  String = { ctermfg = 14, fg = colors.string }, --   A string constant: "this is a string"
   -- }}}
 
   -- diffs {{{
@@ -138,8 +137,8 @@ local groups = {
   DiffDelete = { ctermfg = 1, reverse = true, fg = colors[1] }, -- Diff mode: Deleted line |diff.txt|
   DiffText = { ctermfg = 15, reverse = true, fg = colors[15] }, -- Diff mode: Changed text within a changed line |diff.txt|
   diffAdded = "DiffAdd",
-  diffRemoved = "DiffDelete",
   diffChanged = "DiffChange",
+  diffRemoved = "DiffDelete",
   -- }}}
 
   -- line numbers {{{
@@ -150,51 +149,55 @@ local groups = {
 
   -- cursorline {{{
   CursorLine = { underline = true, sp = colors.guide }, -- Screen-line at the cursor, when 'cursorline' is set. Low-priority if foreground (ctermfg OR guifg) is not set.
+  CursorLineFold = "CursorLineNr",
   CursorLineNr = { underline = not vim.o.termguicolors, bg = colors.bg2, bold = true }, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
   CursorLineSign = "CursorLineNr",
-  CursorLineFold = "CursorLineNr",
   -- }}}
 
   -- statusline/tabline {{{
   StatusLine = { ctermfg = 15, reverse = not vim.o.termguicolors, bg = colors.bg2 },
   StatusLineMode = { bold = true, },
-  StatusLineStandoutText = { ctermfg = 3, fg = colors[3], },
   StatusLineRecordingIndicator = { ctermfg = 1, fg = colors[1], },
+  StatusLineStandoutText = { ctermfg = 3, fg = colors[3], },
   TabLine = "StatusLine",
   TabLineFill = "TabLine",
   TabLineSel = "Normal",
   -- }}}
 
   -- LSP {{{
-  LspInfoBorder = "FloatBorder",
-  LspInlayHint = { italic = not vim.o.termguicolors, ctermfg = 15, bg = colors.inlay_bg, fg = colors.inlay_fg },
   LspCodeLens = "LspInlayHint", -- Used to color the virtual text of the codelens. See |nvim_buf_set_extmark()|.
   LspCodeLensSeparator = "LspCodeLens", -- Used to color the separator between two or more code lens.
+  LspInfoBorder = "FloatBorder",
+  LspInlayHint = { italic = not vim.o.termguicolors, ctermfg = 15, bg = colors.inlay_bg, fg = colors.inlay_fg },
   LspReferenceTarget = { ctermfg = "NONE", ctermbg = "NONE", bg = "NONE", fg = colors[7] },
   -- }}}
 
   -- pmenu (autocomplete) {{{
   Pmenu = { ctermbg = 15, ctermfg = 0, bg = colors.pmenu, fg = colors[7] }, -- Popup menu: Normal item.
-  PmenuSel = { ctermbg = 14, ctermfg = 0, bg = colors.bg2, fg = colors[7] }, -- Popup menu: Selected item.
-  PmenuKind = "Pmenu", -- Popup menu: Normal item "kind"
-  PmenuKindSel = "PmenuSel", -- Popup menu: Selected item "kind"
   PmenuExtra = "PmenuKind", -- Popup menu: Normal item "extra text"
   PmenuExtraSel = "PmenuKindSel", -- Popup menu: Selected item "extra text"
-  PmenuSbar = "Pmenu", -- Popup menu: Scrollbar.
-  PmenuThumb = { bg = colors.pmenu_thumb }, -- Popup menu: Thumb of the scrollbar.
+  PmenuKind = "Pmenu", -- Popup menu: Normal item "kind"
+  PmenuKindSel = "PmenuSel", -- Popup menu: Selected item "kind"
   PmenuMatch = { bold = true, fg = colors.accent },
+  PmenuSbar = "Pmenu", -- Popup menu: Scrollbar.
+  PmenuSel = { ctermbg = 14, ctermfg = 0, bg = colors.bg2, fg = colors[7] }, -- Popup menu: Selected item.
+  PmenuThumb = { bg = colors.pmenu_thumb }, -- Popup menu: Thumb of the scrollbar.
   -- }}}
 
   -- Tree-Sitter {{{
+  ["@character"] = "Character",
+  ["@character.printf"] = { ctermfg = 7, fg = colors[7] }, -- e.g. `%s` in `printf("foo: %s")`
+  ["@comment"] = "Comment",
   ["@comment.documentation"] = "Comment",
-  ["@number.comment"] = "@comment.documentation",
-  ["@comment.todo"] = "TODO",
   ["@comment.error"] = "@comment.todo",
   ["@comment.note"] = "@comment.todo",
+  ["@comment.todo"] = "TODO",
   ["@comment.warning"] = "@comment.todo",
+  ["@constant.comment"] = "@comment",
   ["@diff.delta"] = "DiffChange",
   ["@diff.minus"] = "DiffDelete",
   ["@diff.plus"] = "DiffAdd",
+  ["@keyword"] = "Keyword",
   ["@keyword.conditional"] = "Keyword",
   ["@keyword.conditional.ternary"] = "Keyword",
   ["@keyword.coroutine"] = "Keyword",
@@ -203,55 +206,51 @@ local groups = {
   ["@keyword.exception"] = "Keyword",
   ["@keyword.function"] = "Keyword",
   ["@keyword.import"] = "Keyword",
+  ["@keyword.json5"] = { ctermfg = 7, fg = colors[7] },
+  ["@keyword.luadoc"] = "Comment",
   ["@keyword.operator"] = "Keyword",
   ["@keyword.repeat"] = "Keyword",
   ["@keyword.return"] = "Keyword",
   ["@keyword.storage"] = "Keyword",
+  ["@label"] = "Label",
+  ["@label.yaml"] = { ctermfg = 7, fg = colors[7] },
   ["@markup.italic"] = { italic = true },
   ["@markup.link"] = "Underlined",
   ["@markup.link.label"] = "@markup.link",
   ["@markup.link.url"] = "@markup.link",
   ["@markup.strong"] = { bold = true },
   ["@markup.underline"] = "Underlined",
-  ["@comment"] = "Comment",
-  ["@constant.comment"] = "@comment",
+  ["@number.comment"] = "@comment.documentation",
   ["@punctuation.bracket.comment"] = "@comment",
+  ["@punctuation.delimiter.comment"] = "Comment",
+  ["@punctuation.delimiter.luadoc"] = "Comment",
   ["@string"] = "String", -- String
   ["@string.documentation"] = "String",
+  ["@string.documentation.python"] = "Comment",
   ["@string.regexp"] = "String",
   ["@string.special.url"] = "Underlined",
   ["@string.special.url.comment"] = "@string.special.url",
-  ["@string.documentation.python"] = "Comment",
-  ["@character"] = "Character",
-  ["@character.printf"] = { ctermfg = 7, fg = colors[7] }, -- e.g. `%s` in `printf("foo: %s")`
-  ["@label"] = "Label",
-  ["@label.yaml"] = { ctermfg = 7, fg = colors[7] },
-  ["@keyword"] = "Keyword",
-  ["@keyword.json5"] = { ctermfg = 7, fg = colors[7] },
-  ["@punctuation.delimiter.comment"] = "Comment",
-  ["@keyword.luadoc"] = "Comment",
-  ["@punctuation.delimiter.luadoc"] = "Comment",
   -- }}}
 
   -- misc. {{{
+  CodeActionSign = { ctermfg = 3, fg = colors[3] },
   Conceal = { ctermfg = 8, fg = colors[8] }, -- Placeholder characters substituted for concealed text (see 'conceallevel')
   Directory = { ctermfg = 4, fg = colors[4] }, -- Directory names (and other special names in listings)
-  Folded = { italic = not vim.o.termguicolors, bg = colors.pmenu }, -- Line used for closed folds
   FoldColumn = { ctermfg = 8, fg = colors[8] }, -- 'foldcolumn'
-  Substitute = "Search", -- |:substitute| replacement text highlighting
+  Folded = { italic = not vim.o.termguicolors, bg = colors.pmenu }, -- Line used for closed folds
+  Ignore = { ctermfg = 0, fg = colors[0] }, -- Left blank, hidden |hl-Ignore| (May be invisible here in template)
   MsgArea = "StatusLine", -- Area for messages and cmdline
   NonText = { ctermfg = 8, fg = colors[8] }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
   SpellBad = "Error", -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
   SpellCap = "Warning", -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
+  Substitute = "Search", -- |:substitute| replacement text highlighting
+  Todo = "Comment", -- Anything that needs extra attention; mostly the keywords TODO FIXME and XXX
+  Underlined = { underline = true }, -- Text that stands out, HTML links
   Whitespace = { ctermfg = 8, fg = colors[8] }, -- "nbsp", "space", "tab" and "trail" in 'listchars'
   WinSeparator = "Whitespace", -- Separator between window splits. Inherits from |hl-VertSplit| by default, which it will replace eventually.
-  CodeActionSign = { ctermfg = 3, fg = colors[3] },
-  Underlined = { underline = true }, -- Text that stands out, HTML links
-  Ignore = { ctermfg = 0, fg = colors[0] }, -- Left blank, hidden |hl-Ignore| (May be invisible here in template)
-  Todo = "Comment", -- Anything that needs extra attention; mostly the keywords TODO FIXME and XXX
   tutorLink = "Underlined",
-  tutorX = "DiagnosticError",
   tutorOk = "DiagnosticOk",
+  tutorX = "DiagnosticError",
   -- }}}
 
   -- nvim-treesitter-context {{{
@@ -261,31 +260,31 @@ local groups = {
 
   -- vim-matchup {{{
   MatchParen = { ctermfg = 14, bold = true, fg = colors.accent }, -- Character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
-  MatchWord = { ctermfg = 7, fg = colors[7], },
   MatchParenCur = "MatchParen",
+  MatchWord = { ctermfg = 7, fg = colors[7], },
   MatchWordCur = "MatchWord",
   -- }}}
 
   -- mini.nvim {{{
-  MiniIndentscopeSymbol = { ctermfg = 15, fg = colors.guide },
-  MiniCursorword = { underline = not vim.o.termguicolors, bg = colors.bg2 },
-  MiniOperatorsExchangeFrom = "Visual",
   MiniCompletionActiveParameter = { ctermfg = 14, fg = colors.accent, },
+  MiniCursorword = { underline = not vim.o.termguicolors, bg = colors.bg2 },
+  MiniCursorwordCurrent = {},
   MiniDiffSignAdd = { ctermfg = 2, fg = colors[2] },
   MiniDiffSignChange = { ctermfg = 3, fg = colors[3] },
   MiniDiffSignDelete = { ctermfg = 1, fg = colors[1] },
-  MiniPickNormal = { bg = colors.pmenu, },
+  MiniIndentscopeSymbol = { ctermfg = 15, fg = colors.guide },
+  MiniNotifyBorder = { ctermfg = 8, fg = colors[8], bg = colors.notification, },
+  MiniNotifyNormal = { italic = not vim.o.termguicolors, bg = colors.notification },
+  MiniOperatorsExchangeFrom = "Visual",
   MiniPickBorder = { bg = colors.pmenu, fg = colors.pmenu, },
   MiniPickBorderBusy = 'MiniPickBorder',
   MiniPickBorderText = "MiniPickBorder",
+  MiniPickMatchCurrent = {underline = not vim.o.termguicolors, bg = colors.bg2, },
+  MiniPickMatchRanges = { ctermfg = 14, fg = colors.accent, bold = true, },
+  MiniPickNormal = { bg = colors.pmenu, },
   MiniPickPrompt = "MiniPickNormal",
   MiniPickPromptCaret = "MiniPickPrompt",
   MiniPickPromptPrefix = "MiniPickPrompt",
-  MiniPickMatchRanges = { ctermfg = 14, fg = colors.accent, bold = true, },
-  MiniPickMatchCurrent = {underline = not vim.o.termguicolors, bg = colors.bg2, },
-  MiniNotifyBorder = { ctermfg = 8, fg = colors[8], bg = colors.notification, },
-  MiniNotifyNormal = { italic = not vim.o.termguicolors, bg = colors.notification },
-  MiniCursorwordCurrent = {},
   -- }}}
 }
 
