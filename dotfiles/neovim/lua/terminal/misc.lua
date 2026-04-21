7554de3a47db4c7ae4be60fae143f62cc1898d29
Biggie <code@bigo.lu>
2026-04-21 10:29:04 -0400

    Tweak

--- dotfiles/neovim/lua/terminal/misc.lua
+++ dotfiles/neovim/lua/terminal/misc.lua
@@ -11,6 +11,10 @@ vim.keymap.set("n", "<C-->", vim.cmd.split)
 vim.keymap.set("n", "<C-_>", "<C-->", { expr = true })
 vim.keymap.set("n", "<C-[>", vim.cmd.tabprevious)
 vim.keymap.set("n", "<C-]>", vim.cmd.tabnext)
+vim.keymap.set("n", "<M-h>", "<C-w>h")
+vim.keymap.set("n", "<M-j>", "<C-w>j")
+vim.keymap.set("n", "<M-k>", "<C-w>k")
+vim.keymap.set("n", "<M-l>", "<C-w>l")
 -- }}}
 
 -- Settings {{{
