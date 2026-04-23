d1a9bc448f1931cfc9cbacfa08f8239f12ec8e2b
Biggie <code@bigo.lu>
2026-04-23 14:12:10 -0400

    Revert "Reapply "Switch to Iosevka""
    This reverts commit bb898c416e99ad7c4bf43a74bc135fe8af1a76e1.

--- nix/utils/home-modules/common/fonts.nix
+++ nix/utils/home-modules/common/fonts.nix
@@ -17,39 +17,6 @@ let
     paths = with pkgs; [
       nerd-fonts.symbols-only
       jetbrains-mono
-      (iosevka.override {
-        set = "Custom";
-        privateBuildPlan = ''
-          [buildPlans.IosevkaCustom]
-          family = "Iosevka Custom"
-          spacing = "fixed"
-          serifs = "sans"
-          noCvSs = true
-          exportGlyphNames = false
-
-          [buildPlans.IosevkaCustom.weights.Regular]
-          shape = 400
-          menu = 400
-          css = 400
-
-          [buildPlans.IosevkaCustom.weights.Bold]
-          shape = 700
-          menu = 700
-          css = 700
-
-          [buildPlans.IosevkaCustom.slopes.Upright]
-          angle = 0
-          shape = "upright"
-          menu = "upright"
-          css = "normal"
-
-          [buildPlans.IosevkaCustom.slopes.Oblique]
-          angle = 9.4
-          shape = "oblique"
-          menu = "italic"
-          css = "italic"
-        '';
-      })
     ];
   };
 in
