2ab67c1fee5f931ebb7c8aa939912cbfc286f2b4
Biggie <code@bigo.lu>
2026-05-29 15:12:48 -0400

    Only support checking commits in `check` task

--- mise/tasks/check.bash
+++ mise/tasks/check.bash
@@ -2,31 +2,17 @@
 #! nix-shell -i nix-shell-interpreter
 #! nix-shell --packages nix-shell-interpreter git-branchless
 #MISE description="Run jobs to find/fix issues"
-#USAGE long_about "Run jobs to find/fix issues in the code. You can control how the code is checked by passing one of the subcommands: `files`, `commits`, or `rebase`. The default subcommand is `files`."
+#USAGE long_about "Run jobs to find/fix issues in the commits specified."
 #USAGE
-#USAGE cmd "files" {
-#USAGE 	help "Check files with uncommitted changes"
-#USAGE 	long_help "Check any files that have uncommitted changes."
-#USAGE }
+#USAGE arg "<start>" default="head" long_help="The commit to start checking from. Use the special value `head` to check only the `HEAD` commit. Use `not-pushed` to start from the first commit that hasn't been pushed. See [git's documentation for specifying a revision](https://git-scm.com/docs/git-rev-parse#_specifying_revisions). When `head` is used, the current [git worktree](https://git-scm.com/docs/git-worktree) will be checked. Otherwise, a different worktree will be created for each commit being checked, so they can be checked in parallel."
+#USAGE complete "start" run=#" printf '%s\n' not-pushed head "#
 #USAGE
-#USAGE cmd "commits" {
-#USAGE 	help "Check the files/messages of the commits specified"
-#USAGE 	long_help "Check the files and commit message of each of the commits specified."
-#USAGE 	arg "<commits>" long_help="Any revision range that `git log` accepts. Use the special value `head` to check the files in the `HEAD` commit. See [git's documentation for specifying a revision](https://git-scm.com/docs/git-rev-parse#_specifying_revisions)."
-#USAGE 	complete "commits" run=#" printf '%s\n' head "#
-#USAGE }
+#USAGE flag "-a --all-files" help="Check all files" long_help="Check all files instead of only the files changed in the commit being checked."
 #USAGE
-#USAGE cmd "rebase" {
-#USAGE 	help "Check commits using an interactive rebase"
-#USAGE 	long_help "Check commits using an interactive rebase. An `exec` command will be added after every commit which checks the files and message for that commit. If you make a mistake and want to go back to where you were before the rebase, run `git reset --hard refs/project/ir-backup`."
-#USAGE 	arg "<start>" long_help="The revision to start the interactive rebase from. Use the special value `not-pushed` to rebase any commits that haven't been pushed. See [git's documentation for specifying a revision](https://git-scm.com/docs/git-rev-parse#_specifying_revisions)."
-#USAGE 	complete "start" run=#" printf '%s\n' not-pushed "#
-#USAGE }
-#USAGE
-#USAGE flag "-a --all-files" global=#true help="Check all files" long_help="Check all files instead of the files that are normally checked by `files`, `commits`, or `rebase`."
-#USAGE
-#USAGE flag "-j --job <job>" global=#true var=#true help="Job to run" long_help="Job to run. If none are passed then all of them will be run. The list of jobs is in `lefthook.yaml` under the `check` hook."
+#USAGE flag "-j --job <job>" var=#true help="Job to run" long_help="Job to run. If none are passed then all of them will be run. The list of jobs is in `lefthook.yaml` under the `check` hook."
 #USAGE complete "job" run=#" fish -c 'complete --do-complete "lefthook run check --job "' "#
+#USAGE
+#USAGE flag "-r --rebase" help="Use interactive rebase" long_help="Check commits using an interactive rebase. An `exec` command will be added after every commit which checks the files and message for that commit. If you make a mistake and want to go back to where you were before the rebase, run `git reset --hard refs/project/ir-backup`."
 
 set -o errexit
 set -o nounset
@@ -35,35 +21,44 @@ shopt -s nullglob
 shopt -s inherit_errexit
 
 function main {
-	case "${usage_cmd:-files}" in
-		'files')
-			check_files
-			;;
-		'commits')
-			check_commits
-			;;
-		'rebase')
-			check_commits_with_rebase
-			;;
-		*)
-			# It shouldn't ever reach this case
-			return 1
-			;;
-	esac
+	if [[ -n ${usage_rebase:-} ]]; then
+		check_commits_with_rebase
+	else
+		check_commits
+	fi
 }
 
-function check_files {
-	local -a lefthook_command
-	make_lefthook_command lefthook_command
-
-	local files=''
-	if [[ -n ${usage_all_files:-} ]]; then
-		files='all'
+function check_commits_with_rebase {
+	local start=''
+	if [[ ${usage_start:?} == 'not-pushed' ]]; then
+		start="$(git merge-base '@{push}' HEAD)"
 	else
-		files='uncommitted'
+		start="${usage_start}^"
 	fi
 
-	LEFTHOOK_FILES="$files" "${lefthook_command[@]}"
+	# Save a reference to the commit we were on before the rebase started, in case
+	# we want to go back.
+	git update-ref refs/project/ir-backup HEAD
+
+	local -r all_files="${usage_all_files:+ --all-files}"
+
+	local -a job_flags=()
+	make_job_flags job_flags
+	local job_flags_string
+	if ((${#job_flags[@]} > 0)); then
+		job_flags_string=" ${job_flags[*]}"
+	else
+		job_flags_string=''
+	fi
+
+	# The arguments for this task should not be inherited by the one we run in `--exec`
+	# or any other tasks that get run during the interactive rebase.
+	unset "${!usage_@}"
+
+	git rebase \
+		--interactive \
+		--exec "mise run check head$all_files$job_flags_string" \
+		"$start"
 }
 
 function check_commits {
@@ -77,7 +72,7 @@ function check_commits {
 		lefthook_env_variables+=(LEFTHOOK_FILES=head)
 	fi
 
-	if [[ ${usage_commits:?} == 'head' ]]; then
+	if [[ ${usage_start:?} == 'head' ]]; then
 		# When we run this task with the subcommand `rebase`, we check each commit
 		# by running this same task with the arguments `commits head` at each
 		# commit. When we do this, we want any fixes that get made by lefthook to be
@@ -88,7 +83,7 @@ function check_commits {
 	fi
 
 	local hashes
-	hashes="$(git log "$usage_commits" --pretty=%h)"
+	hashes="$(git log "${usage_start}^..HEAD" --pretty=%h)"
 	if [[ -z $hashes ]]; then
 		return 0
 	fi
@@ -123,39 +118,6 @@ function check_commits {
 		--exec "${git_branchless_exec_command[*]@Q}" \
 		--jobs 0 \
 		"${hashes//$'\n'/ | }"
-}
-
-function check_commits_with_rebase {
-	local start=''
-	if [[ ${usage_start:?} == 'not-pushed' ]]; then
-		start="$(git merge-base '@{push}' HEAD)"
-	else
-		start="$usage_start"
-	fi
-
-	# Save a reference to the commit we were on before the rebase started, in case
-	# we want to go back.
-	git update-ref refs/project/ir-backup HEAD
-
-	local -r all_files="${usage_all_files:+ --all-files}"
-
-	local -a job_flags=()
-	make_job_flags job_flags
-	local job_flags_string
-	if ((${#job_flags[@]} > 0)); then
-		job_flags_string=" ${job_flags[*]}"
-	else
-		job_flags_string=''
-	fi
-
-	# The arguments for this task should not be inherited by the one we run in `--exec`
-	# or any other tasks that get run during the interactive rebase.
-	unset "${!usage_@}"
-
-	git rebase \
-		--interactive \
-		--exec "mise run check commits head$all_files$job_flags_string" \
-		"$start"
 }
 
 function make_lefthook_command {
