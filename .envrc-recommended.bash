9e379d7f0942835c1ab8c8fe5c50e9a6ebee3d7f
Biggie <code@bigo.lu>
2026-06-02 20:49:37 -0400

    Extract lychee workflow to a GitHub action

--- .envrc-recommended.bash
+++ .envrc-recommended.bash
@@ -1,6 +1,6 @@
 source_url \
-	'https://raw.githubusercontent.com/bigolu/direnv-devshell/0f1a452b46c183c565b6ac5f710e9ea24bf918bb/src/main.bash' \
-	'sha256-heBPLeA31k9eQvGJBKlxxkuMQXYGOOd4qyceg8aBBYQ='
+	'https://raw.githubusercontent.com/bigolu/direnv-devshell/c063e0605481ec97cfec78abd8dd9ead0746318d/src/main.bash' \
+	'sha256-J1DKZGjz/XiKz430g8pN5CZnqa7kyZ/RIlnV+rTufas='
 DEVSHELL_DIRENV_FALLBACK=true use_devshell --file . devShells.development
 
 source_url \
