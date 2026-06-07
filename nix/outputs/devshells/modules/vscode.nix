c92d62dd7eebdf5e9ffbad51cfc35df3d41c5d5f
Biggie <code@bigo.lu>
2026-06-07 00:36:29 -0400

    Refactor

--- nix/outputs/devshells/modules/vscode.nix
+++ nix/outputs/devshells/modules/vscode.nix
@@ -1,39 +1,27 @@
 { pkgs, ... }:
-let
-  inherit (pkgs) coreutils;
-in
 {
-  imports = [
-    # For extension "maximsmol.vscode-lsp-generic"
-    {
-      devshell.packages = with pkgs; [
-        efm-langserver
-
-        # These are used in the efm-langserver config
-        coreutils
-        jq
-        # efm-langserver launches commands with`sh`
-        dash
-      ];
-    }
-
-    # For extension "rogalmic.bash-debug"
-    {
-      # It needs bash, cat, mkfifo, rm, and pkill
-      devshell.packages = with pkgs; [
-        bash
-        coreutils
-        partialPackages.pkill
-      ];
-    }
-  ];
-
   devshell.packages = with pkgs; [
     # For extension "jnoortheen.nix-ide"
     nixd
+
     # For extension "ndonfris.fish-lsp"
     fish-lsp
+
     # For extension "golang.go"
     gopls
+
+    # For extension "rogalmic.bash-debug".
+    # It needs bash, cat, mkfifo, rm, and pkill
+    bash
+    coreutils
+    partialPackages.pkill
+
+    # For extension "maximsmol.vscode-lsp-generic"
+    efm-langserver
+    # These are used in the efm-langserver config
+    coreutils
+    jq
+    # efm-langserver launches commands with`sh`
+    dash
   ];
 }
