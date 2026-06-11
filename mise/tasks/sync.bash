9b2b13f3ed9a713d90e3975a665ab17e14978fed
Biggie <code@bigo.lu>
2026-06-10 20:22:14 -0400

    Update

--- mise/tasks/sync.bash
+++ mise/tasks/sync.bash
@@ -3,7 +3,7 @@
 #MISE description="Sync your environment with the code"
 #USAGE long_about "Run jobs to sync your environment with the code. For example, running database migrations whenever the schema changes."
 #USAGE
-#USAGE flag "--job <job>" var=#true help="Job to run" long_help="Job to run. If none are passed then all of them will be run. The list of jobs is in `lefthook.yaml` under the `sync` hook."
+#USAGE arg "[job]" var=#true help="Job to run" long_help="Job to run. If none are passed then all of them will be run. The list of jobs is in `lefthook.yaml` under the `sync` hook."
 #USAGE complete "jobs" run=#" fish -c 'complete --do-complete "lefthook run sync --job "' "#
 #USAGE
 #USAGE flag "--ask" help="Show diff and confirm before syncing" long_help="Show a diff of the current state and the new state, and ask for confirmation, before syncing. This is only supported by the `system` job."
@@ -17,7 +17,7 @@ shopt -s inherit_errexit
 command=(lefthook run sync)
 for arg in "$@"; do
 	if [[ $arg != '--ask' ]]; then
-		command+=("$arg")
+		command+=(--job "$arg")
 	fi
 done
 "${command[@]}"
