ff9caa69b26319ec30bb5104a3704916c3cf0c2e
Biggie <code@bigo.lu>
2026-06-14 13:24:43 -0400

    Tweak

--- mise/tasks/check.bash
+++ mise/tasks/check.bash
@@ -7,10 +7,4 @@
 #USAGE flag "--job <job>" var=#true help="Job to run" long_help="Job to run. If none are passed then all of them will be run. The list of jobs is in `lefthook.yaml` under the `check` hook."
 #USAGE complete "job" run=#" fish -c 'complete --do-complete "lefthook run check --job "' "#
 
-set -o errexit
-set -o nounset
-set -o pipefail
-shopt -s nullglob
-shopt -s inherit_errexit
-
 lefthook run check "$@"
