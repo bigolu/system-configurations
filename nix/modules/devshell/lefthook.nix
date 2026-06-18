0e8a5d1cc1d42de4b3495c62d0b72ec771e785b8
Biggie <code@bigo.lu>
2026-06-17 21:38:13 -0400

    Update

--- nix/modules/devshell/lefthook.nix
+++ nix/modules/devshell/lefthook.nix
@@ -1,7 +1,19 @@
 { pkgs, ... }:
 {
   devshell.packages = with pkgs; [
-    lefthook
+    # TODO: Remove when this version reaches nixpkgs-unstable[1].
+    #
+    # [1]: https://github.com/NixOS/nixpkgs/pull/514847
+    (lefthook.overrideAttrs rec {
+      version = "2.1.9";
+      vendorHash = "sha256-7+DzMPE2MqOfXR4G4INLggZhPD2dQmwfOFxBARrdYcI=";
+      src = fetchFromGitHub {
+        owner = "evilmartians";
+        repo = "lefthook";
+        rev = "v${version}";
+        hash = "sha256-QANbvwD1q4UvdmMHdRUrNk1sNqq+hllUKJ1c1t53UD0=";
+      };
+    })
     # TODO: Lefthook won't run unless git is present so maybe nixpkgs should make it
     # a dependency.
     git
