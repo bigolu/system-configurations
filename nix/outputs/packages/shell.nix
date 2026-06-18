4c49c2ee4928f013984573a5644062bcf098aea6
Biggie <code@bigo.lu>
2026-06-18 13:38:20 -0400

    Tweak

--- nix/outputs/packages/shell.nix
+++ nix/outputs/packages/shell.nix
@@ -2,34 +2,29 @@
 let
   moduleRoot = ../../modules/home;
 in
-(pkgs.makePortableHome.override {
-  # The full set of locales is pretty big (~220MB) so I'll only include the one that
-  # will be used.
-  locales = [ "en_US.UTF-8/UTF-8" ];
-})
-  {
-    homeConfig = inputs.home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      extraSpecialArgs = { inherit inputs; };
-      modules = [
-        # This should be added to every Home Manager configuration.
-        # SYNC: hm-base
-        {
-          imports = [ (moduleRoot + "/essentials") ];
-          _module.args = {
-            hasGui = false;
-            hostName = "portable";
-          };
-        }
+(pkgs.makePortableHome.override { locales = [ "en_US.UTF-8/UTF-8" ]; }) {
+  homeConfig = inputs.home-manager.lib.homeManagerConfiguration {
+    inherit pkgs;
+    extraSpecialArgs = { inherit inputs; };
+    modules = [
+      # This should be added to every Home Manager configuration.
+      # SYNC: hm-base
+      {
+        imports = [ (moduleRoot + "/essentials") ];
+        _module.args = {
+          hasGui = false;
+          hostName = "portable";
+        };
+      }
 
-        (import (moduleRoot + "/portable") pkgs)
-      ];
-    };
-
-    shell = "fish";
-
-    activation = [
-      "fzfSetup"
-      "batSetup"
+      (import (moduleRoot + "/portable") pkgs)
     ];
-  }
+  };
+
+  shell = "fish";
+
+  activation = [
+    "fzfSetup"
+    "batSetup"
+  ];
+}
