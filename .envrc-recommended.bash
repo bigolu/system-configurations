47fab9e1c27c3d11591a6c5a4d5c69639956b3d3
Biggie <code@bigo.lu>
2026-06-18 10:18:51 -0400

    Tweak

--- .envrc-recommended.bash
+++ .envrc-recommended.bash
@@ -1,16 +1,8 @@
-path="$(
-	fetchurl \
-		'https://raw.githubusercontent.com/bigolu/direnv-devshell/c063e0605481ec97cfec78abd8dd9ead0746318d/src/main.bash' \
-		'sha256-J1DKZGjz/XiKz430g8pN5CZnqa7kyZ/RIlnV+rTufas='
-)"
-# shellcheck disable=1090
-source "$path"
+source_url \
+	'https://raw.githubusercontent.com/bigolu/direnv-devshell/c063e0605481ec97cfec78abd8dd9ead0746318d/src/main.bash' \
+	'sha256-J1DKZGjz/XiKz430g8pN5CZnqa7kyZ/RIlnV+rTufas='
 DEVSHELL_DIRENV_FALLBACK=true use_devshell --file ./nix/flake-compat.nix outputsForCurrentSystem.devShells.dev
 
-path="$(
-	fetchurl \
-		'https://raw.githubusercontent.com/bigolu/direnv-autocomplete/7505862f80b5501977757686a2219475d8bde3e7/src/main.bash' \
-		'sha256-VeqqJ67/ZRbbhQ9T8ngSziyTh2+B2GwoH7XYwTbxAlI='
-)"
-# shellcheck disable=1090
-source "$path"
+source_url \
+	'https://raw.githubusercontent.com/bigolu/direnv-autocomplete/7505862f80b5501977757686a2219475d8bde3e7/src/main.bash' \
+	'sha256-VeqqJ67/ZRbbhQ9T8ngSziyTh2+B2GwoH7XYwTbxAlI='
