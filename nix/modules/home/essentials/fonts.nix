d1909e445d6322b8999680edb34e2356e3745362
Biggie <code@bigo.lu>
2026-06-19 15:22:21 -0400

    Update

--- nix/modules/home/essentials/fonts.nix
+++ nix/modules/home/essentials/fonts.nix
@@ -6,26 +6,9 @@
 }:
 let
   inherit (pkgs.stdenv.hostPlatform) isLinux;
-  inherit (lib) mkMerge mkIf;
-  inherit (pkgs) symlinkJoin;
-
-  myFonts = symlinkJoin {
-    pname = "my-fonts";
-    version = "0.1.0";
-    paths = with pkgs; [
-      nerd-fonts.symbols-only
-      jetbrains-mono
-    ];
-  };
+  inherit (lib) optional;
 in
-mkMerge [
-  (mkIf hasGui {
-    home.packages = [ myFonts ];
-  })
-  (mkIf (isLinux && hasGui) {
-    fonts.fontconfig.enable = true;
-    # VS Code can't read my fonts from Nix despite them showing up in fontconfig so I
-    # make a symlink to them from a location that VS Code _can_ read.
-    xdg.dataFile."fonts".source = "${myFonts}/share/fonts";
-  })
-]
+{
+  home.packages = optional hasGui pkgs.jetbrains-mono;
+  fonts.fontconfig.enable = isLinux && hasGui;
+}
