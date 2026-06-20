6d77631d239c84bfccc73309909acb1de6106531
Biggie <code@bigo.lu>
2026-06-20 16:49:44 -0400

    Tweak

--- dotfiles/neovim/lua/base/operators.lua
+++ dotfiles/neovim/lua/base/operators.lua
@@ -1,12 +1,19 @@
 -- vim:foldmethod=marker
 
--- for the globals defined by mini.nvim
----@diagnostic disable: undefined-global
-
 -- Copy up to the end of line, not including the newline character
-vim.keymap.set({ "n" }, "Y", "yg_", {
-	desc = "Til end of line, excluding newline",
+vim.keymap.set({ "n" }, "Y", "yg_")
+
+require("treesj").setup({
+	use_default_keymaps = false,
+	max_join_length = 200,
 })
+vim.keymap.set("n", "ss", vim.cmd.TSJToggle)
+vim.keymap.set("n", "sS", function()
+	require("treesj").toggle({
+		join = { recursive = true },
+		split = { recursive = true },
+	})
+end)
 
 -- Extend the types of text that can be incremented/decremented {{{
 local augend = require("dial.augend")
@@ -27,87 +34,44 @@ local function symbols(...)
 	})
 end
 
-local defaults = {
-	-- color: #ffffff
-	--
-	-- If the cursor is over one of the two digits in the red, green, or blue
-	-- value, it only increments that color of the hex. To increment the red,
-	-- green, and blue portions, the cursor must be over the '#'.
-	hex_rgb = augend.hexcolor.new({ case = "prefer_lower" }),
-	-- time: 14:30:00
-	date_hms = augend.date.alias["%H:%M:%S"],
-	-- time: 14:30
-	date_hm = augend.date.alias["%H:%M"],
-	-- decimal integer: 0, 4, -123
-	int = augend.integer.alias.decimal_int,
-	-- hex: 0x00
-	hex = augend.integer.alias.hex,
-	-- binary: 0b0101
-	binary = augend.integer.alias.binary,
-	-- octal: 0o00
-	octal = augend.integer.alias.octal,
-	-- Semantic Versioning: 1.22.1
-	semver = augend.semver.alias.semver,
-	-- uppercase letter: A
-	Alpha = augend.constant.alias.Alpha,
-	-- lowercase letter: a
-	alpha = augend.constant.alias.alpha,
-	logical_word = words("and", "or"),
-	visibility = words("public", "private"),
-	boolean = words("true", "false"),
-	Boolean = words("True", "False"),
-	confirm = words("yes", "no"),
-	logical_symbol = symbols("&&", "||"),
-	equality = symbols("!=", "=="),
-	strict_equality = symbols("!==", "==="),
-	lt_gt = symbols("<", ">"),
-	lte_gte = symbols("<=", ">="),
-	inc_dec = symbols("+=", "-="),
-}
-
-local function make_table(acc, _, item)
-	table.insert(acc, item)
-	return acc
-end
-local function extend_defaults(tweaks)
-	return require("base.utilities").table_concat(
-		vim.iter(defaults)
-			:filter(function(index, _)
-				return not vim.tbl_contains(tweaks.remove or {}, index)
-			end)
-			:fold({}, make_table),
-		tweaks.add or {}
-	)
-end
-
 require("dial.config").augends:register_group({
-	default = vim.tbl_values(defaults),
-})
-
-local augends_for_js_based_languages = extend_defaults({
-	add = {
-		augend.constant.new({ elements = { "let", "const" } }),
+	default = {
+		-- color: #ffffff
+		--
+		-- If the cursor is over one of the two digits in the red, green, or blue
+		-- value, it only increments that color of the hex. To increment the red,
+		-- green, and blue portions, the cursor must be over the '#'.
+		augend.hexcolor.new({ case = "prefer_lower" }),
+		-- time: 14:30:00
+		augend.date.alias["%H:%M:%S"],
+		-- time: 14:30
+		augend.date.alias["%H:%M"],
+		-- decimal integer: 0, 4, -123
+		augend.integer.alias.decimal_int,
+		-- hex: 0x00
+		augend.integer.alias.hex,
+		-- binary: 0b0101
+		augend.integer.alias.binary,
+		-- octal: 0o00
+		augend.integer.alias.octal,
+		-- Semantic Versioning: 1.22.1
+		augend.semver.alias.semver,
+		-- uppercase letter: A
+		augend.constant.alias.Alpha,
+		-- lowercase letter: a
+		augend.constant.alias.alpha,
+		words("and", "or"),
+		words("public", "private"),
+		words("true", "false"),
+		words("True", "False"),
+		words("yes", "no"),
+		symbols("&&", "||"),
+		symbols("!=", "=="),
+		symbols("!==", "==="),
+		symbols("<", ">"),
+		symbols("<=", ">="),
+		symbols("+=", "-="),
 	},
-})
-
-require("dial.config").augends:on_filetype({
-	javascript = augends_for_js_based_languages,
-	javascriptreact = augends_for_js_based_languages,
-	typescript = augends_for_js_based_languages,
-	typescriptreact = augends_for_js_based_languages,
-	lua = extend_defaults({
-		add = {
-			symbols("~=", "=="),
-		},
-		remove = {
-			"equality",
-		},
-	}),
-	markdown = extend_defaults({
-		add = {
-			augend.misc.alias.markdown_header,
-		},
-	}),
 })
 
 local manipulate = require("dial.map").manipulate
@@ -136,15 +100,3 @@ vim.keymap.set("v", "g-", function()
 	manipulate("decrement", "gvisual")
 end)
 -- }}}
-
-require("treesj").setup({
-	use_default_keymaps = false,
-	max_join_length = 200,
-})
-vim.keymap.set("n", "ss", vim.cmd.TSJToggle)
-vim.keymap.set("n", "sS", function()
-	require("treesj").toggle({
-		join = { recursive = true },
-		split = { recursive = true },
-	})
-end)
--- dotfiles/neovim/lua/base/utilities.lua
+++ dotfiles/neovim/lua/base/utilities.lua
@@ -12,11 +12,4 @@ function M.get_char()
 	return vim.fn.nr2char(char_num)
 end
 
-function M.table_concat(t1, t2)
-	for i = 1, #t2 do
-		t1[#t1 + 1] = t2[i]
-	end
-	return t1
-end
-
 return M
