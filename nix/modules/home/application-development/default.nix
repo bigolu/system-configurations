010559d4ec1bdf9ab57232fa11cc3b5db826b495
Biggie <code@bigo.lu>
2026-06-29 21:44:41 -0400

    Update

--- nix/modules/home/application-development/default.nix
+++ nix/modules/home/application-development/default.nix
@@ -47,11 +47,6 @@ in
 
     xdg.configFile = {
       "ipython/profile_default/ipython_config.py".source = "python/ipython/ipython_config.py";
-      "ipython/profile_default/startup" = {
-        source = "python/ipython/startup";
-        # I'm linking recursively because ipython makes files in this directory
-        recursive = true;
-      };
       "direnv/direnv.toml".source = "direnv/direnv.toml";
       # Zed only recognizes the ".json" extension, but it's actually jsonc
       "zed/settings.json".source = "zed/settings.jsonc";
--- program-configs/python/ipython/ipython_config.py
+++ program-configs/python/ipython/ipython_config.py
@@ -1,7 +1,5 @@
 c = get_config()  # noqa: F821
 
-c.InteractiveShellApp.extensions = ["autoreload", "storemagic"]
-c.TerminalIPythonApp.display_banner = False
 c.TerminalInteractiveShell.confirm_exit = False
 c.TerminalInteractiveShell.shortcuts = [
     {"command": "IPython:shortcuts.open_input_in_editor", "new_keys": ["c-e"]}
