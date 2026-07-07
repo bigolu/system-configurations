1821a812a6c5f9518f5a07b91a7b7f665f2da1e2
Biggie <code@bigo.lu>
2026-07-07 09:35:29 -0400

    Try Iosevka

--- nix/modules/home/essentials/fonts.nix
+++ nix/modules/home/essentials/fonts.nix
@@ -9,6 +9,41 @@ let
   inherit (lib) optional;
 in
 {
-  home.packages = optional hasGui pkgs.jetbrains-mono;
+  home.packages = optional hasGui (
+    pkgs.iosevka.override {
+      set = "Custom";
+      privateBuildPlan = ''
+        [buildPlans.IosevkaCustom]
+        family = "Iosevka Custom"
+        spacing = "fixed"
+        serifs = "sans"
+        noCvSs = true
+        exportGlyphNames = false
+
+        [buildPlans.IosevkaCustom.weights.Regular]
+        shape = 400
+        menu = 400
+        css = 400
+
+        [buildPlans.IosevkaCustom.weights.Bold]
+        shape = 700
+        menu = 700
+        css = 700
+
+        [buildPlans.IosevkaCustom.slopes.Upright]
+        angle = 0
+        shape = "upright"
+        menu = "upright"
+        css = "normal"
+
+        [buildPlans.IosevkaCustom.slopes.Oblique]
+        angle = 9.4
+        shape = "oblique"
+        menu = "italic"
+        css = "italic"
+      '';
+    }
+  );
+
   fonts.fontconfig.enable = isLinux && hasGui;
 }
