063dce29d37710ae908a31c9b85afcfe2d934db8
Biggie <code@bigo.lu>
2026-07-08 11:31:46 -0400

    Update

--- nix/modules/home/essentials/terminal/fish.nix
+++ nix/modules/home/essentials/terminal/fish.nix
@@ -1,9 +1,19 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs.fishPlugins; [
-    pkgs.fish
-    async-prompt
-    direnv-shell-hooks
-  ];
+{ pkgs, lib, ... }:
+let
+  inherit (lib) getExe hm;
+in
+{
+  home = {
+    packages = with pkgs.fishPlugins; [
+      pkgs.fish
+      async-prompt
+      direnv-shell-hooks
+    ];
+
+    activation.reloadFish = hm.dag.entryAfter [ "linkGeneration" ] ''
+      ${getExe pkgs.fish} -c fish-reload
+    '';
+  };
 
   fileWrapper.xdg.configFile."fish/conf.d" = {
     source = "fish/conf.d";
