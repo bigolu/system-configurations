411ff73354ebfefbc9e6fec1e061af305618143c
Biggie <code@bigo.lu>
2026-07-13 18:00:41 -0400

    Update

--- nix/modules/home/essentials/terminal/fish.nix
+++ nix/modules/home/essentials/terminal/fish.nix
@@ -1,29 +1,31 @@
 { pkgs, lib, ... }:
 let
   inherit (lib) getExe hm;
+
+  # Remove this override when this commit is included in a release:
+  # https://github.com/fish-shell/fish-shell/commit/2c17c96e5535584f26ea2bcd5a2bebaf1feffdee
+  fish = pkgs.fish.overrideAttrs {
+    version = "4.8.0-next";
+    doCheck = false;
+    doInstallCheck = false;
+    src = pkgs.fetchFromGitHub {
+      owner = "fish-shell";
+      repo = "fish-shell";
+      rev = "2c17c96e5535584f26ea2bcd5a2bebaf1feffdee";
+      hash = "sha256-KtpkSIO2P3oFB2fNDr10EkGI5lO5I6se2sVqiLVQDX8=";
+    };
+  };
 in
 {
   home = {
     packages = with pkgs.fishPlugins; [
-      # Remove this override when this commit is included in a release:
-      # https://github.com/fish-shell/fish-shell/commit/2c17c96e5535584f26ea2bcd5a2bebaf1feffdee
-      (pkgs.fish.overrideAttrs {
-        version = "4.8.0-next";
-        doCheck = false;
-        doInstallCheck = false;
-        src = pkgs.fetchFromGitHub {
-          owner = "fish-shell";
-          repo = "fish-shell";
-          rev = "2c17c96e5535584f26ea2bcd5a2bebaf1feffdee";
-          hash = "sha256-KtpkSIO2P3oFB2fNDr10EkGI5lO5I6se2sVqiLVQDX8=";
-        };
-      })
+      fish
       async-prompt
       direnv-shell-hooks
     ];
 
     activation.reloadFish = hm.dag.entryAfter [ "linkGeneration" ] ''
-      ${getExe pkgs.fish} -c fish-reload
+      ${getExe fish} -c fish-reload
     '';
   };
 
