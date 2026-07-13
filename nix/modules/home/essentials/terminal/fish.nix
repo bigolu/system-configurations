9586e015fbaf940b4b934f64dd7dbaf4c22ff1ef
Biggie <code@bigo.lu>
2026-07-13 17:46:56 -0400

    Update

--- nix/modules/home/essentials/terminal/fish.nix
+++ nix/modules/home/essentials/terminal/fish.nix
@@ -5,7 +5,19 @@ in
 {
   home = {
     packages = with pkgs.fishPlugins; [
-      pkgs.fish
+      # Remove this override when this commit is included in a release:
+      # https://github.com/fish-shell/fish-shell/commit/2c17c96e5535584f26ea2bcd5a2bebaf1feffdee
+      (pkgs.fish.overrideAttrs {
+        version = "4.8.0-next";
+        doCheck = false;
+        doInstallCheck = false;
+        src = pkgs.fetchFromGitHub {
+          owner = "fish-shell";
+          repo = "fish-shell";
+          rev = "2c17c96e5535584f26ea2bcd5a2bebaf1feffdee";
+          hash = "sha256-KtpkSIO2P3oFB2fNDr10EkGI5lO5I6se2sVqiLVQDX8=";
+        };
+      })
       async-prompt
       direnv-shell-hooks
     ];
