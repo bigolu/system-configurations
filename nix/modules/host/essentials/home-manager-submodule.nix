2ea341f03504fb5b51f6e226a2ce6f7f66343e2c
Biggie <code@bigo.lu>
2026-08-03 10:15:59 -0400

    Refactor

--- nix/modules/host/essentials/home-manager-submodule.nix
+++ nix/modules/host/essentials/home-manager-submodule.nix
@@ -1,33 +1,29 @@
-{ _class, inputs, ... }:
-let
-  darwinAndSystem = {
-    imports = [
-      inputs.home-manager."${
-        {
-          "" = "nixos";
-          darwin = "darwin";
-        }
-        .${toString _class}
-      }Modules".home-manager
-    ];
-
-    home-manager = {
-      useGlobalPkgs = true;
-      useUserPackages = true;
-      backupFileExtension = "home-manager-backup";
-      overwriteBackup = true;
-      extraSpecialArgs = { inherit inputs; };
-      sharedModules = [ ./. ];
-    };
-  };
-in
 {
-  "" = {
-    imports = [ darwinAndSystem ];
-  };
+  _class,
+  inputs,
+  lib,
+  ...
+}:
+let
+  inherit (lib) genAttrs const;
+in
+(genAttrs [ "" "darwin" ] (const {
+  imports = [
+    inputs.home-manager."${
+      {
+        "" = "nixos";
+        darwin = "darwin";
+      }
+      .${toString _class}
+    }Modules".home-manager
+  ];
 
-  darwin = {
-    imports = [ darwinAndSystem ];
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    backupFileExtension = "home-manager-backup";
+    overwriteBackup = true;
+    extraSpecialArgs = { inherit inputs; };
+    sharedModules = [ ./. ];
   };
-}
-.${toString _class}
+})).${toString _class}
