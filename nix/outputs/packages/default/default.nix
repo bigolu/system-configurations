5792eba18e727c36229f548167fdff1e076002fe
Biggie <code@bigo.lu>
2026-09-20 19:03:06 -0400

    Update

--- nix/outputs/packages/default/default.nix
+++ nix/outputs/packages/default/default.nix
@@ -2,57 +2,39 @@
 pkgs.callPackage (
   {
     lib,
-    resholve,
-    bash,
+    makeWrapper,
+    runCommand,
+    nushell,
     git,
     direnv,
+    bash,
     coreutils,
-    symlinkJoin,
-    makeWrapper,
   }:
   let
-    inherit (lib) getExe;
-
-    script = resholve.mkDerivation rec {
-      pname = "init-config";
-      version = "0.1.0";
-      src = ./init-config.bash;
-      meta.mainProgram = pname;
-      dontUnpack = true;
-      installPhase = ''
-        install -D $src $out/bin/${pname}
-      '';
-      solutions.default = {
-        scripts = [ "bin/${pname}" ];
-        interpreter = "${bash}/bin/bash";
-        inputs = [
-          git
-          direnv
-        ];
-        execer = [
-          "cannot:${getExe git}"
-          "cannot:${getExe direnv}"
-        ];
-        keep = {
-          "/bin/bash" = true;
-        };
-        fake.external = [
-          "mise"
-          "curl"
-        ];
-      };
-    };
+    inherit (lib) makeBinPath;
+    name = "init-config";
   in
-  symlinkJoin {
-    inherit (script) pname version meta;
-    paths = [ script ];
-    nativeBuildInputs = [ makeWrapper ];
-    postBuild = ''
-      # direnv plugins assume these are on the PATH
-      wrapProgram $out/bin/init-config \
-        --prefix PATH : ${direnv}/bin \
-        --prefix PATH : ${bash}/bin \
-        --prefix PATH : ${coreutils}/bin
-    '';
-  }
+  runCommand name
+    {
+      nativeBuildInputs = [ makeWrapper ];
+      buildInputs = [ nushell ];
+      meta.mainProgram = name;
+    }
+    ''
+      mkdir --parents $out/bin
+      cp ${./init-config.nu} $out/bin/${name}
+      chmod +x $out/bin/${name}
+      patchShebangs $out/bin/${name}
+      wrapProgram $out/bin/${name} \
+        --prefix PATH : ${
+          makeBinPath [
+            nushell
+            git
+            direnv
+            # direnv plugins assume these are on the PATH
+            bash
+            coreutils
+          ]
+        }
+    ''
 ) { }
---
+++ nix/outputs/packages/default/init-config.nu
@@ -0,0 +1,39 @@
+#!/usr/bin/env nu
+
+use std/assert
+
+def main [config] {
+	do --env {
+		let project_dir = $"($nu.home-dir)/code/system-configurations"
+		if not ($project_dir | path exists) {
+			git clone https://github.com/bigolu/system-configurations.git $project_dir
+		}
+		cd $project_dir
+	}
+
+	if DID_LOAD_DIRENV in $env {
+		# Besides passing positional arguments, this is done separately from other syncs
+		# since it may prompt the user and `hk`, which is called by `mise run sync`,
+		# seems to have a bug where it can't take user input despite me enabling the
+		# `interactive` setting.
+		mise run hk:system-sync $config
+		with-env { HK_SKIP_STEPS: 'system' } { mise run sync }
+		exit
+	}
+
+	"source .envrc-recommended.bash" | save --force .envrc
+	direnv allow
+	let nix_config = $"($env.PWD)/program-configs/nix/nix.conf"
+	assert ($nix_config | path exists) 'Nix config does not exist'
+  # Use the caches set in the nix config
+  #
+  # We can't use `NIX_CONFIG` since the config file uses relative paths and lix
+	# forbids their use in `NIX_CONFIG`.
+	with-env {
+		NIX_USER_CONF_FILES: $nix_config
+		DID_LOAD_DIRENV: 'true'
+	} {
+		const self = path self
+		direnv exec . nu $self $config
+	}
+}
